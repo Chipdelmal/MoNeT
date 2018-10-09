3b4d4ee8a5b3d0909d87b427e2765d6213f31dff
chipdelmal <chipdelmal@gmail.com>
2018-10-09 15:06:16 -0700

    Fixing the factorial experiment analysis routine for the AggregateLandscape

--- Dev/HectorSanchez/DataAnalysis/PandasAndPlotlyTests.py
+++ Dev/HectorSanchez/DataAnalysis/PandasAndPlotlyTests.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 import sys
 import matplotlib
+import plotly.plotly as py
+import plotly.graph_objs as go
 from plotly import offline
 offline.init_notebook_mode()
 
@@ -40,3 +42,27 @@ data = [
 ]
 fig = go.Figure(data=data, layout=layout)
 offline.iplot(fig, filename='datetime-heatmap')
+
+
+
+
+
+
+# Define paths for experiment
+pathBase="/Users/sanchez.hmsc/odrive/MGDrivE_Experiments/"
+pathExperiment="MCR/MCRFitted/E_03_02_0001_0010/0001/"
+filename="ADM_Run001_Patch0000.csv"
+
+df=pd.read_csv(pathBase+pathExperiment+filename)
+columnNames=df.columns.values
+columnNames
+
+# Create a trace
+trace = go.Scatter(
+    x = df['Time'],
+    y = df['HH']
+)
+data = [trace]
+offline.iplot(data, filename='basic-line')
+
+df
