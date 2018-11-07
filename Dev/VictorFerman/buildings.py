bbac01b9b077c301df53f828baef9a216cbfcb93
Victor Ferman <victor.ferman@gmail.com>
2018-11-07 11:46:53 -0800

    added a visualization for all of the buildings in a country/island

--- Dev/VictorFerman/buildings.py
+++ Dev/VictorFerman/buildings.py
@@ -3,14 +3,20 @@
 # ╔╦╗┌─┐╔╗╔┌─┐╔╦╗
 # ║║║│ │║║║├┤  ║
 # ╩ ╩└─┘╝╚╝└─┘ ╩
-# Extracts the roads and buildings positions and exports them into
-#   SHP and graphml files
+# Searches the villages and cities of a country on OSM, then for each it
+#  extracts the roads and buildings positions and exports them into
+#  SHP and graphml files and finally calculates the centroids of each building
+#  and exports the buildings' coordinates to a CSV file
 # Data source: https://www.openstreetmap.org/
 # HMSC, vferman
 ################################################################################
 
 #Load package ####################################################
 import fiona
+#import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
 import numpy as np
 import os
 import osmnx as ox, geopandas as gpd
@@ -57,7 +63,8 @@ def main():
             print ('error on '+placeName)
         except:
             print ('I got another exception')
-
+    fullLats = []
+    fullLongs = []
     for place in places:
         filepath = os.path.join(os.getcwd(),"SHP/Buildings/")
         placeShapes = fiona.open(str(filepath)+place+"/"+place+".shp")
@@ -72,9 +79,30 @@ def main():
                     longs.append(float(long))
             meanLat = np.mean(lats)
             meanLong = np.mean(longs)
+            fullLats.append(meanLat)
+            fullLongs.append(meanLong)
             placeCoordCSV.write(str(meanLat)+","+str(meanLong)+"\n")
         placeCoordCSV.close()
         placeShapes.close()
 
+    minLat = min(fullLats)
+    minLong = min(fullLongs)
+    maxLat = max(fullLats)
+    maxLong = max(fullLongs)
+
+    #fig, ax = plt.subplots()
+    m = Basemap(projection='merc',llcrnrlat=minLong-0.03,urcrnrlat=maxLong+0.02,
+            llcrnrlon=minLat-0.02,urcrnrlon=maxLat+0.02,lat_ts=20,resolution='h')
+    m.drawcoastlines(color="black")
+
+    m.scatter(fullLats,fullLongs,latlon=True,s=9)
+    #plt.xlim([minLat+0.02,maxLat+0.02])
+    #plt.ylim([minLong+0.02,maxLong+0.02])
+    plt.savefig("./"+countryCode+".png", dpi=1024, facecolor='w',
+                edgecolor='w', orientation='portrait', papertype=None,
+                format="png", transparent=False, bbox_inches='tight',
+                pad_inches=0.05, frameon=None)
+    plt.show()
+    #plt.close('all')
 
 main()
