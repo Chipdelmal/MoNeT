bb77b8e9e68ac93ca7a5b87574dc5158e688c2b8
chipdelmal <chipdelmal@gmail.com>
2018-11-08 06:19:03 -0800

    Setting up the structure for python package

---
+++ PythonPkg/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MoNeT',
+      version='0.1',
+      description='',
+      url='https://github.com/Chipdelmal/MoNeT',
+      author='Hector M. Sanchez C.',
+      author_email='sanchez.hmsc@berkeley.edu',
+      license='MIT',
+      packages=['MoNeT'],
+      zip_safe=False)
