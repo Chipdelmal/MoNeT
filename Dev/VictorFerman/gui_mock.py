8bf524354c2de0f6fed4e2e396910347c264da58
Victor Ferman <victor.ferman@gmail.com>
2019-01-08 18:06:00 -0800

    added graphics to the gui and created an aunxiliary module

--- Dev/VictorFerman/gui_mock.py
+++ Dev/VictorFerman/gui_mock.py
@@ -1,73 +1,53 @@
 import glob
 import math
 import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import guiAuxFunctions as aux
+import pandas as pd
 
 from matplotlib.backends.qt_compat import QtCore, QtWidgets, QtGui
 from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
 from matplotlib.figure import Figure
-matplotlib.use('QT5Agg')
-#from PyQt5 import QtCore, QtGui, QtWidgets
-#from PyQt5.QtCore import pyqtProperty
 from mpl_toolkits.basemap import Basemap
-
-colors = ['#ff004d','#4d80ff', '#ff00ff', 'red', 'blue', 'green', 'yellow', 'magenta', 'purple', 'black', 'cyan', 'teal']
-def draw_pie(ax,ratios, X=0, Y=0, radius=300):
-    N = len(ratios)
-    xy = []
-    start = 0.
-    for ratio in ratios:
-        x = [0] + np.cos(np.linspace(2*math.pi*start,2*math.pi*(start+ratio), 30)).tolist()
-        y = [0] + np.sin(np.linspace(2*math.pi*start,2*math.pi*(start+ratio), 30)).tolist()
-        xy1 = list(zip(x,y))
-        xy.append(xy1)
-        start += ratio
-
-    for i, xyi in enumerate(xy):
-        ax.scatter([X],[Y] , marker=(xyi,0), s=radius, facecolor=colors[i])
-
-def getCoordinates(fileLocation):
-    patchFile = open(fileLocation,"r")
-    patchesX = []
-    patchesY = []
-    for line in patchFile:
-        coordinates = line.split(',')
-        patchesX.append(float(coordinates[0]))
-        patchesY.append(float(coordinates[1]))
-    patchFile.close()
-    return(patchesX,patchesY)
-
-def getSizes(fileLocation):
-    populationFile = open(fileLocation,"r")
-    radiuses = []
-    for line in populationFile:
-        radiuses.append((math.log(float(line))**2)*0.7)
-    populationFile.close()
-    return radiuses
-
-def getRatios(populations, weights):
-    groupPopulation= []
-    total = 1
-    for i in range(len(weights)):
-        population = 0
-        for j in range(len(weights[i])):
-            population+= (populations[j]*weights[i][j])
-        total+= population
-        groupPopulation.append(population)
-    ratios = [float(pop)/total for pop in groupPopulation]
-    return ratios
-
 
 
 class GraphicsWizard(QtWidgets.QWizard):
     def __init__(self, parent=None):
         super(GraphicsWizard, self).__init__(parent)
-        self.addPage(Page1(self))
-        self.addPage(Page2(self))
-        self.addPage(Page3(self))
+        self.setPage(0,StartP(self))
+        self.setPage(1,Page1(self))
+        self.setPage(2,Page2(self))
+        self.setPage(3,Page3(self))
+        self.setPage(4,Page4(self))
+        self.setPage(5,Page5(self))
+        self.setPage(6,Page6(self))
         self.setWindowTitle('Visualization Wizard')
+
+class StartP(QtWidgets.QWizardPage):
+    def __init__(self,parent=None):
+        super(StartP,self).__init__(parent)
+        comboLabel = QtWidgets.QLabel('Graphic Type:')
+        self.combo = QtWidgets.QComboBox(self)
+        self.combo.addItem('Pies on Map')
+        self.combo.addItem('Grid')
+        self.combo.addItem('Aggregated')
+        self.comboValue = ''
+        self.combo.activated[str].connect(self.onActivated)
+        hbox = QtWidgets.QHBoxLayout()
+        hbox.addWidget(comboLabel)
+        hbox.addWidget(self.combo)
+        self.setLayout(hbox)
+
+    def onActivated(self, text):
+        self.comboValue = str(text)
+
+    def nextId(self):
+        if "Map" in self.comboValue:
+            return 1
+        else:
+            return 4
+
 
 class Page1(QtWidgets.QWizardPage):
     def __init__(self,parent=None):
@@ -186,14 +166,10 @@ class Page3(QtWidgets.QWizardPage):
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
         self.ax = self.figure.add_subplot(111)
-        #self.toolbar = NavigationToolbar(self.canvas, self)
-        #button = QtWidgets.QPushButton('Graph', self)
-        #button.clicked.connect(self.plots)
+        self.setFinalPage(True)
         vbox = QtWidgets.QVBoxLayout()
-        #vbox.addWidget(self.toolbar)
         self._timer = self.canvas.new_timer(500, [(self.plots, (), {})])
         vbox.addWidget(self.canvas)
-        #vbox.addWidget(button)
         self.setLayout(vbox)
         self.patchesX = []
         self.patchesY = []
@@ -209,7 +185,6 @@ class Page3(QtWidgets.QWizardPage):
         self.currentLine= 0
 
 
-
     def plots(self):
         if self.currentLine < self.lineCount:
             ax = self.ax
@@ -220,9 +195,9 @@ class Page3(QtWidgets.QWizardPage):
                 line = next(self.files[patch]).split(',')
                 sizes = [float(elem) for elem in line]
                 sizes = sizes[2:]
-                ratios = getRatios(sizes, self.columnsWeight)
+                ratios = aux.getRatios(sizes, self.columnsWeight)
                 px,py = m(self.patchesX[patch],self.patchesY[patch])
-                draw_pie(ax,ratios,px,py,self.radiuses[patch])
+                aux.draw_pie(ax,ratios,px,py,self.radiuses[patch])
             self.canvas.draw()
             self.currentLine +=1
         else:
@@ -234,12 +209,12 @@ class Page3(QtWidgets.QWizardPage):
         coordFile = str(self.field('coordFile'))
         popFile = str(self.field('popFile'))
         dataFile = str(self.field('dataDir'))+"/AF1_Aggregate_Run1_*.csv"
-        self.patchesX,self.patchesY = getCoordinates(coordFile)
+        self.patchesX,self.patchesY = aux.getCoordinates(coordFile)
         self.minLat = min(self.patchesY)
         self.minLong = min(self.patchesX)
         self.maxLat = max(self.patchesY)
         self.maxLong = max(self.patchesX)
-        self.radiuses = getSizes(popFile)
+        self.radiuses = aux.getSizes(popFile)
         groups = int(self.field('groupCount'))
         alleles = int(self.field('alleleCount'))
         for i in range(groups):
@@ -248,7 +223,6 @@ class Page3(QtWidgets.QWizardPage):
                 weight = int(self.field(str(i)+'-'+str(j)))
                 groupWeights.append(weight)
             self.columnsWeight.append(groupWeights)
-        #
         fileNames = sorted(glob.glob(dataFile))
         self.files =[]
         self.lineCount = len(open(fileNames[0]).readlines())-1
@@ -258,6 +232,137 @@ class Page3(QtWidgets.QWizardPage):
         for f in self.files:
             next(f)
         self._timer.start()
+
+class Page4(QtWidgets.QWizardPage):
+    def __init__(self,parent=None):
+        super(Page4,self).__init__(parent)
+        integerValidator = QtGui.QIntValidator(0,10, self)
+
+        comboLabel = QtWidgets.QLabel('Graphic Type:')
+        dataLabel = QtWidgets.QLabel('Data Directory:')
+        self.dataLine = QtWidgets.QLineEdit()
+        dataBtn = QtWidgets.QPushButton('File', self)
+        dataBtn.clicked.connect(self.getDir)
+        self.registerField('popDir*', self.dataLine)
+
+        allelesLabel = QtWidgets.QLabel('Number of alleles:')
+        self.allelesLine = QtWidgets.QLineEdit()
+        self.allelesLine.setValidator(integerValidator)
+        self.registerField('alleleCount2*', self.allelesLine)
+
+        groupsLabel = QtWidgets.QLabel('Number of Groups:')
+        self.groupsLine = QtWidgets.QLineEdit()
+        self.groupsLine.setValidator(integerValidator)
+        self.registerField('groupCount2*', self.groupsLine)
+
+        self.b1 = QtWidgets.QCheckBox("Females")
+        self.b1.setChecked(False)
+        self.registerField('females', self.b1)
+
+        grid = QtWidgets.QGridLayout()
+        grid.setSpacing(10)
+        grid.addWidget(dataLabel, 1, 0)
+        grid.addWidget(self.dataLine, 1, 1)
+        grid.addWidget(dataBtn, 1, 2)
+        grid.addWidget(allelesLabel, 2, 0)
+        grid.addWidget(self.allelesLine, 2, 1)
+        grid.addWidget(groupsLabel, 3, 0)
+        grid.addWidget(self.groupsLine, 3, 1)
+        grid.addWidget(self.b1, 4, 1)
+
+        self.setLayout(grid)
+
+    def getDir(self):
+        dlg = QtWidgets.QFileDialog(self, "Browse Directory")
+        dlg.setFileMode(dlg.Directory)
+        dlg.setOption(dlg.ShowDirsOnly)
+        fname=str(dlg.getExistingDirectory())
+        self.dataLine.setText(fname)
+
+
+class Page5(QtWidgets.QWizardPage):
+    def __init__(self,parent=None):
+        super(Page5,self).__init__(parent)
+        self.setTitle('Group-Alelle weights')
+        vbox = QtWidgets.QVBoxLayout()
+        self.setLayout(vbox)
+
+    def initializePage(self):
+        groups = int(self.field('groupCount2'))
+        alleles = int(self.field('alleleCount2'))
+        integerValidator = QtGui.QIntValidator(0,10, self)
+        vbox = self.layout()
+        for i in range(groups):
+            hbox1 = QtWidgets.QHBoxLayout()
+            hbox2 = QtWidgets.QHBoxLayout()
+            glabel = QtWidgets.QLabel("group "+str(i+1)+":")
+            gname = QtWidgets.QLineEdit()
+            gname.setText('0')
+            self.registerField('group'+str(i), gname)
+            hbox1.addWidget(glabel)
+            hbox1.addWidget(gname)
+            for j in range(alleles):
+                label = QtWidgets.QLabel(str(j+1)+":")
+                line = QtWidgets.QLineEdit()
+                line.setValidator(integerValidator)
+                line.setText('0')
+                self.registerField(str(i)+'-'+str(j), line)
+                hbox2.addWidget(label)
+                hbox2.addWidget(line)
+            vbox.addLayout(hbox1)
+            vbox.addLayout(hbox2)
+
+    def clearLayout(self, layout):
+        if layout is not None:
+            while layout.count():
+                item = layout.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                    widget.deleteLater()
+                else:
+                    self.clearLayout(item.layout())
+
+    def cleanupPage(self):
+        self.clearLayout(self.layout())
+
+class Page6(QtWidgets.QWizardPage):
+    def __init__(self,parent=None):
+        super(Page6,self).__init__(parent)
+        self.setTitle('Graph')
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.ax = self.figure.add_subplot(111)
+        self.setFinalPage(True)
+        vbox = QtWidgets.QVBoxLayout()
+        vbox.addWidget(self.canvas)
+        self.setLayout(vbox)
+        self.columnsWeight = []
+        self.groupNames = []
+
+
+    def initializePage(self):
+        if self.field('females'):
+            modifier='/AF1*.csv'
+        else:
+            modifier='/ADM*.csv'
+        dataFile = str(self.field('popDir'))+modifier
+        groups = int(self.field('groupCount2'))
+        alleles = int(self.field('alleleCount2'))
+        for i in range(groups):
+            groupWeights = []
+            for j in range(alleles):
+                weight = int(self.field(str(i)+'-'+str(j)))
+                groupWeights.append(weight)
+            self.columnsWeight.append(groupWeights)
+            gname = self.field('group'+str(i))
+            self.groupNames.append(gname)
+        fileNames = sorted(glob.glob(dataFile))
+        counts = aux.allCounts(fileNames, self.columnsWeight, self.groupNames)
+        for i in range(len(counts)):
+            counts[i].plot(ax=self.ax, linewidth = 0.3, legend=False, color = aux.colors[i], alpha = 0.1)
+        self.ax.ylabel("Allele Count")
+        self.canvas.draw()
+
 
 
 
