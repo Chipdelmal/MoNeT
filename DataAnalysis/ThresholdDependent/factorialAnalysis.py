dbbb28130bc541852724b13b1a4f3b3529df9708
chipdelmal <chipdelmal@gmail.com>
2019-01-10 17:14:01 -0800

    Unimportant changes to data analysis routines for threshold-dependent paper.

--- DataAnalysis/ThresholdDependent/factorialAnalysis.py
+++ DataAnalysis/ThresholdDependent/factorialAnalysis.py
@@ -1,21 +1,24 @@
+import fnmatch
+import os
+import csv
+import time
+import plotly.offline as offline
+import plotly.graph_objs as go
+import plotly
+import numpy as np
+import MoNeT_MGDrivE as monet
+from sklearn.externals.joblib import Parallel, delayed
 import warnings
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
-from sklearn.externals.joblib import Parallel, delayed
-import time, csv, os, fnmatch
-import MoNeT_MGDrivE as monet
-import numpy as np
 #import temporaryFunctionsDefinitions as tempFun
-import plotly
-import plotly.graph_objs as go
-import plotly.offline as offline
 offline.init_notebook_mode(connected=True)
 
 ###############################################################################
 # Factorial Experiment Example
 ###############################################################################
 dataType = float
-filename = "2018_10_10_ANALYZED"
+filename = "TRSA_200"
 path = "/Users/sanchez.hmsc/Desktop/" + filename + "/"
 wildsList = [1, 1, 2, 2, 3, 3, 4, 5, 6, 1, 1, 2, 4, 4, 5, 7, 7, 8]
 homingList = [4, 5, 6, 7, 7, 8, 8, 9, 9, 2, 3, 3, 5, 6, 6, 8, 9, 9]
@@ -57,37 +60,37 @@ Parallel(n_jobs=4)(delayed(
     for folder in experimentFolders
 )
 end = time.time()
-print(end - start)
+print((end - start)/60)
 ###############################################################################
 # Load and Compile CSVs into one
 ###############################################################################
-outFilename = "A_" + filename + ".csv"
+outFilename = filename + ".csv"
 monet.compileFactorialCSVFromFiles(path, outFilename)
 
 ###############################################################################
 # Load compiled CSV and analyze the output
 ###############################################################################
-path = "/Users/sanchez.hmsc/Desktop/ThresholdDependent/"
-centralFile = "50TX_1x_2018_09_01.csv"
+path = "/Volumes/Pusheen/xjego-khuq3/ThresholdDependent/Datasets/"
+centralFile = "50TR_1x_2018_09_11.csv"
 sensitivitySelector = 1
 if(sensitivitySelector == 1):
     # 001 larval life decrease
     # 002 larval life increase
     title = "Larval Lifespan"
-    testFileA = "TSA_001.csv"
-    testFileB = "TSA_002.csv"
+    testFileA = "TRSA_001.csv"
+    testFileB = "TRSA_002.csv"
 if(sensitivitySelector == 2):
     # 010 adult life increase
     # 020 adult life decrease
     title = "Adult Mortality"
-    testFileA = "TSA_010.csv"
-    testFileB = "TSA_020.csv"
+    testFileA = "TRSA_010.csv"
+    testFileB = "TRSA_020.csv"
 if(sensitivitySelector == 3):
     # 100 fitness cost 10% reduction
     # 200 fitness cost 20% reduction
     title = "Fitness Cost"
-    testFileA = "TSA_100.csv"
-    testFileB = "TSA_200.csv"
+    testFileA = "UXSA_100.csv"
+    testFileB = "UXSA_200.csv"
 centralData = monet.loadAndHashFactorialCSV(path + centralFile)
 probeDataA = monet.loadAndHashFactorialCSV(path + testFileA)
 probeDataB = monet.loadAndHashFactorialCSV(path + testFileB)
@@ -107,8 +110,14 @@ errorsB = differencesHashB.values()
 deHashedA = monet.deHashFactorial(differencesHashA)
 deHashedB = monet.deHashFactorial(differencesHashB)
 
-np.savetxt(path + "deHashedA.csv", deHashedA, fmt='%2.6f', delimiter=",")
-np.savetxt(path + "deHashedB.csv", deHashedB, fmt='%2.6f', delimiter=",")
+np.savetxt(
+    path + testFileA.split(".")
+    [0] + "Diff.csv", deHashedA, fmt='%2.6f', delimiter=","
+)
+np.savetxt(
+    path + testFileB.split(".")
+    [0] + "Diff.csv", deHashedB, fmt='%2.6f', delimiter=","
+)
 
 ###############################################################################
 # Plotting the results
