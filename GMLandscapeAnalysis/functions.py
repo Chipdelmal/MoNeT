8ab507f9dfdf0ce2582f2ecbd3a063690647b1b0
Biyonka Liang <Biyonka@calvisitor-10-105-149-229.calvisitor.1918.berkeley.edu>
2019-02-08 17:10:58 -0800

    updated functions and started clustering coefficient function

---
+++ GMLandscapeAnalysis/functions.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+
+
+def population_split(pop_size, n, C=0.0):
+    """ Function to split population size into n groups.
+        pop_size: int, total population size
+        n: int, number of groups to split pop_size
+        C: int or float, can take on range of values from 0 (uniform) to 1 (random).
+    """
+    uniform_pop_size = ((pop_size * (1.0 - C)) // n) * n
+    random_pop_size = pop_size - uniform_pop_size
+    populations = [uniform_pop_size/n] * n
+    random_pop = np.random.choice(range(n), int(random_pop_size))
+    for i in random_pop:
+        populations[i] += 1
+    base = pop_size / n
+    populations.insert(0, base)
+    populations.append(base)
+    return populations
+
+
+def array_creation(dist, pop_size, n, C=0.0):
+    """
+    dist: int or float, step size in x-coordinate
+    pop_size: int, total population size
+    n: int, number of groups to split pop_size
+    C: int or float, can take on range of values from 0 (uniform) to 1 (random).
+    """
+    pop = np.array(population_split(pop_size, n, C))
+    x_coords = np.linspace(0, dist * (n + 1), n + 2)
+    y_coords = np.zeros(n + 2)
+    return np.array([x_coords, y_coords, pop])
---
+++ GMLandscapeAnalysis/main_biyonka.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+import copy
+import pandas as pd
+from functions import array_creation
+from sklearn.metrics.pairwise import euclidean_distances
+import networkx as nx
+
+
+def gaussian_cluster(n, p, v):
+    """
+    n: int, number of total points
+    p: int, number of points to perturb
+    v: float or int, variance of noise term, N(0, v)
+    """
+    y = array_creation(1, n, n-1)[1]
+    base = array_creation(1, n, n-1)[0]
+    if v == 0:
+        return [base, y]
+    # randomly sample from base to pick p points to perturb
+    index = np.sort(np.random.choice(base.shape[0], p, replace=False))
+    perturb = base[index]
+    # perturb p points
+    noise = stats.norm.rvs(loc=0, scale=v, size=len(perturb), random_state=21)
+    # modulo the noise to keep within range
+    perturbed = (perturb + noise) % 10
+    # add new points to base to make new pointset
+    new_points = copy.deepcopy(base)
+    new_points[index] = perturbed
+    return [new_points, y]
+
+# array_creation(1, 10, 10-1)[1]
+# nodes = gaussian_cluster(10, 5,  3)
+# xy_points = zip(nodes[0], nodes[1])
+# adj_matrix = euclidean_distances(xy_points)
+# inv = 1/(np.matrix(adj_matrix) + 0.00001)
+# G=nx.from_numpy_matrix(adj_matrix)
+# nx.average_clustering(G)
+
+def clustering_coefficient(n, p, v):
+    nodes = gaussian_cluster(n, p, v)
+    xy_points = zip(nodes[0], nodes[1])
+    distances = euclidean_distances(xy_points)
+    adj_matrix = np.matrix(distances)
+    inv_distances = 1/(np.matrix(adj_matrix) + 0.00001)
+    # pd.DataFrame(
+    # {'lst1Title': nodes[0],
+    #  'lst2Title': nodes[1],
+    # })
+    G=nx.from_numpy_matrix(inv_distances)
+    return average_clustering(G)
+
+
+# plotting
+test_base = gaussian_cluster(40, 25, 0)
+plt.scatter(test_base, np.zeros((len(test_base))), alpha=0.5)
+
+test_cluster = gaussian_cluster(50, 25, 5)
+plt.scatter(test_cluster, np.zeros((len(test_cluster))), alpha=0.5)
