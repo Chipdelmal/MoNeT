4bb07054a389bed4b90123f5fcb30a6fe12a52b9
Biyonka Liang <Biyonka@biyonka.local>
2019-02-19 17:15:11 -0800

    finished clustering coefficient function

--- GMLandscapeAnalysis/main_biyonka.py
+++ GMLandscapeAnalysis/main_biyonka.py
@@ -8,6 +8,7 @@ from sklearn.metrics.pairwise import euclidean_distances
 import networkx as nx
 from networkx import average_clustering
 import itertools
+from sklearn.cluster import KMeans
 
 
 def gaussian_cluster(array, p, v):
@@ -49,54 +50,97 @@ def clustering_coefficient(nodes):
     Returns
     clustering_coefficient: float
     """
-    xy_points = zip(nodes[0], nodes[1])
-    distances = euclidean_distances(xy_points)
-    adj_matrix = np.matrix(distances)
-    inv_distances = 1/(np.matrix(adj_matrix) + 0.00001)
-    G=nx.from_numpy_matrix(inv_distances)
-    return average_clustering(G)
+    #xy_points = zip(nodes[0], nodes[1])
+    #distances = euclidean_distances(xy_points)
+    # adj_matrix = np.matrix(distances)
+    # inv_distances = 1/(np.matrix(adj_matrix) + 0.00001)
+    # G=nx.from_numpy_matrix(inv_distances)
+    # return average_clustering(G)
 
 
-def write_csv(n, p, path, v=1, cluster = False):
+def kmeans_clustering_coefficient(nodes, num_clusters):
+    """
+    Performs kmeans on a pointset, and caluclates the average maximum distance
+    between any two points in each cluster
+
+    Inputs
+    nodes:array containing array of x-points and y-points.
+           e.g. the output of gaussian_cluster(n, p, v)
+    num_clusters: number of clusters to create in the kmeans
+
+    Returns:
+    clustering coefficient: float, represents degree of clustering in a point set
+    """
+    x = nodes[0]
+    y = nodes[1]
+    X = np.array(zip(x, y))
+    kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(X)
+    clusters = []
+    for label in np.unique(kmeans.labels_):
+        clusters.append([node for i,node in enumerate(X) if kmeans.labels_[i] == label])
+    coeff = 0
+    for cluster in clusters:
+        coeff += np.max(euclidean_distances(cluster))
+    return coeff/len(clusters)
+
+
+def write_csv(nodes, path, num_clusters, v=1, cluster = False):
     """
     Input
-    n: int, number of total points for array creation
+    nodes: pointset to write, like output of array_creation
     cluster: bool, whether we add clustering to the habitats
     path_to_output: string, the path to output folder
 
     Returns
     df: Pandas DataFrame
     """
-    nodes = array_creation(1, n, n-1)
-    if cluster:
-        nodes = gaussian_cluster(nodes, p, v)
+    # nodes = array_creation(1, n, n-1)
+    # if cluster:
+    #     nodes = gaussian_cluster(nodes, p, v)
     x_points = nodes[0]
     y_points = nodes[1]
-    clustering = clustering_coefficient(nodes)
+    clustering = kmeans_clustering_coefficient(nodes, num_clusters)
     df = pd.DataFrame({
         "x": x_points,
         "y": y_points,
-        "n": n,
+        "n": len(x_points),
         "clustering_coeff": clustering
     })
     df = df[['x', 'y', 'n', 'clustering_coeff']]
     df.to_csv(path_or_buf  = path + r'output.csv', index = False)
     return df
-    #add column of x and y coordinates, number of total points and
-    #clustering coefficients
 
+
+#=========================================
+#Generate deterministic pointsets
+#=========================================
 path_to_output = r'~/Desktop/Output/spaHet/'
-w = write_csv(n=22, p=15, path = path_to_output, cluster = True)
 
+#base case
+nodes = array_creation(1, 9, 9-1)
+w = write_csv(nodes, num_clusters = 1, v=0, path = path_to_output)
 
-# plotting
-array = array_creation(1, 40, 40-1)
-test_base = gaussian_cluster(array, 25, 0)
-plt.scatter(test_base[0], test_base[1], alpha=0.5)
+#two uneven clusters
+x = np.concatenate((np.arange(0, 4, 0.5), np.arange(9, 10, 0.5)))
+y = np.zeros(len(x))
+nodes = np.array((x, y))
+w = write_csv(nodes, num_clusters = 2, v=0, path = path_to_output)
+plt.scatter(x, y, alpha=0.5)
 
+#two tighter clusters
+x = np.concatenate((np.linspace(0, 1, 5), np.linspace(9, 10, 5)))
+y = np.zeros(len(x))
+nodes = np.array((x, y))
+kmeans_clustering_coefficient(nodes, 2)
+w = write_csv(nodes, num_clusters = 2, v=0, path = path_to_output)
+plt.scatter(x, y, alpha=0.5)
 
-test_cluster = gaussian_cluster(array, 25, 10)
-plt.scatter(test_cluster[0], test_cluster[1], alpha=0.5)
+#two looser clusters
+x = np.concatenate((np.linspace(0, 3, 5), np.linspace(7, 10, 5)))
+y = np.zeros(len(x))
+nodes = np.array((x, y))
+w = write_csv(nodes, num_clusters = 2, v=0, path = path_to_output)
+plt.scatter(x, y, alpha=0.5)
 
 # array_creation(1, 10, 1)[1]
 # nodes = gaussian_cluster(5, 5,  0)
