ca63338e6733b4ea64ac6c9fb1dad928c88ad073
chipdelmal <chipdelmal@gmail.com>
2019-02-20 17:11:21 -0800

    Traces and stacked plots now work

--- DataAnalysis/PythonDemos/demo_MoNeT.py
+++ DataAnalysis/PythonDemos/demo_MoNeT.py
@@ -18,28 +18,54 @@ def plotMeanGenotypeTrace(aggData, style):
     final = [df[['Time']] for _ in range(len(groups))]
     local=pd.DataFrame(pops, columns=groups)
     fig, ax = plt.subplots()
-    ax.set_aspect(aspect=0.5)
+    ax.set_aspect(aspect=style["aspect"])
     #plt.xticks([])
     #plt.yticks([])
     for j in range(len(groups)):
         final[j].insert(1, groups[j] + str(1), (local[groups[j]]).copy())
         final[j] = final[j].set_index('Time')
-
     for i in range(len(final)):
-        final[i].plot(ax = ax, linewidth = style["width"], legend=False, color = colors[i], alpha =style["alpha"])
-
-
+        final[i].plot(
+            ax = ax, linewidth = style["width"], legend=False, 
+            color = style["colors"][i], alpha =style["alpha"]
+        )
     legends=[]
     for i in range(len(groups)):
         legends.append(
-            mpatches.Patch(color=colors[i], label=groups[i])
+            mpatches.Patch(color=style["colors"][i], label=groups[i])
         )
     #plt.legend(handles=legends)
     #ax.xaxis.set_label_text("")
     #ax.yaxis.set_label_text("")
     plt.ylabel("Allele Count")
 
-    return plt
+    return fig
+
+
+def plotMeanGenotypeStack(aggData, style):
+    groups = aggData['genotypes']
+    pops = aggData['population']
+    time = np.arange(len(pops))
+    df=pd.DataFrame(time , columns=['Time'])
+    final = [df[['Time']] for _ in range(len(groups))]
+    local=pd.DataFrame(pops, columns=groups)
+    fig, ax2 = plt.subplots()
+    ax2.set_aspect(aspect=style["aspect"])
+    allele_dict = {}
+    for j in range(len(groups)):
+        final[j].insert(1, groups[j] + str(1), (local[groups[j]]).copy())
+        final[j] = final[j].set_index('Time')
+    patchList=[]
+    for i in range(len(groups)):
+        patchList.append(mpatches.Patch(color=style["colors"][i], label=groups[i]))
+    for i in range(len(groups)):
+        allele_dict[groups[i]] = final[i].T.sum()
+    res = pd.DataFrame(allele_dict)
+    res.plot(kind = 'area', ax =ax2, legend=False, color=style["colors"])
+    plt.ylabel("Allele Count")
+    return fig
+
+
 
 ###############################################################################
 # MCR Construct
@@ -64,15 +90,6 @@ aggregationDictionary = monet.generateAggregationDictionary(
 )
 filenames = monet.readExperimentFilenames(path + experimentString)
 
-# To analyze a single node ...................................................
-# Load a single node (auxiliary function just for demonstration)
-# nodeIndex = 0
-# nodeData = monet.loadNodeData(
-#     filenames.get("male")[nodeIndex],
-#     filenames.get("female")[nodeIndex],
-#     dataType=float
-# )
-
 # To analyze the sum of the whole landscape ..................................
 # Sum landscape into one array ("in place" memory-wise)
 landscapeSumData = monet.sumLandscapePopulationsFromFiles(
@@ -88,99 +105,37 @@ aggData = monet.aggregateGenotypesInNode(
     aggregationDictionary
 )
 
-# To analyze the landscape without sum .......................................
-# Load the population dynamics data of the whole landscape
-# landscapeData = monet.loadLandscapeData(filenames, dataType=float)
-# aggregatedNodesData = monet.aggregateGenotypesInLandscape(
-#     landscapeData,
-#     aggregationDictionary
-# )
-# aggregatedNodesData["landscape"]
-
 #------------------------------------------------------------------------------
 # Plotting
 #------------------------------------------------------------------------------
 
 style={
-    "width":2,
-    "alpha":1,
-    "dpi":1024,
-    "legend":True,
+    "width":2, "alpha":1,
+    "dpi":1024, "legend":True,
+    "aspect":.5,
     "colors": [
         '#5cf64a', '#6e44ff','#e56399','#f1866d', '#861657',
         'yellow', 'magenta', 'purple', 'black', 'cyan', 'teal'
     ]
 }
-plt=plotMeanGenotypeTrace(aggData,style)
+fig=plotMeanGenotypeTrace(aggData,style)
 plt.savefig("./images/demotraces.png",
             dpi=1024, facecolor='w',
             edgecolor='w', orientation='portrait', papertype=None,
-            format="png", transparent=False, bbox_inches='tight',
+            format="png", transparent=True, bbox_inches='tight',
             pad_inches=0, frameon=None)
 plt.close(fig)
-plt.close('all')
 
+
+fig=plotMeanGenotypeStack(aggData, style)
+plt.savefig("./images/demostacks.png",
+            dpi=1024, facecolor='w',
+            edgecolor='w', orientation='portrait', papertype=None,
+            format="png", transparent=True, bbox_inches='tight',
+            pad_inches=0, frameon=None)
+plt.close(fig)
 
 labels = aggData["genotypes"]
 colors = ["rgb(25,128,255)", "rgb(255,25,128)",
           "rgb(128,0,255)", "rgb(255,0,255)"]
 inData = aggData["population"]
-
-# Plot allele frequency dynamics .............................................
-tracesList = []
-for i in range(0, len(labels)):
-    trace = dict(
-        x=range(0, len(inData)),
-        y=inData[:, i],
-        stackgroup='one',
-        mode='lines',
-        line=dict(width=3, color=colors[i]),
-        name=labels[i]
-    )
-    tracesList.append(trace)
-layout = go.Layout(
-    title='Genotypes Breakdown',
-    xaxis=dict(
-        title='Time [days]',
-        titlefont=dict(size=20, color='#7f7f7f')
-    ),
-    yaxis=dict(
-        title='Allele Frequency',
-        titlefont=dict(size=20, color='#7f7f7f')
-    ),
-    width=1500,
-    height=400
-)
-fig = go.Figure(data=go.Data(tracesList), layout=layout)
-py.iplot(fig,filename='stacked-area-plot-hover',validate=False)
-plotly.offline.plot(fig, filename='alleleFrequency.html')
-
-
-# Plot allele ratio dynamics ..................................................
-tracesList = []
-for i in range(0, len(labels)):
-    trace = dict(
-        x=range(0, len(inData)),
-        y=inData[:, i],
-        stackgroup='one',
-        groupnorm='fraction',
-        mode='lines',
-        line=dict(width=3, color=colors[i]),
-        name=labels[i]
-    )
-    tracesList.append(trace)
-layout = go.Layout(
-    title='Genotypes Breakdown',
-    xaxis=dict(
-        title='Time [days]',
-        titlefont=dict(size=20, color='#7f7f7f')
-    ),
-    yaxis=dict(
-        title='Allele Ratio',
-        titlefont=dict(size=20, color='#7f7f7f')
-    ),
-    width=1500,
-    height=400
-)
-fig = dict(data=tracesList, layout=layout)
-plotly.offline.plot(fig, filename='allelePercentage.html')
