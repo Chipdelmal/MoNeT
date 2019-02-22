42385c985ac6ef121c0d9c25d51217a0249c912b
chipdelmal <chipdelmal@gmail.com>
2019-02-22 11:30:11 -0800

    Minor update to the demo_MoNeT example so that it does not require library update.

--- GMLandscapeAnalysis/demo_MoNeT.py
+++ GMLandscapeAnalysis/demo_MoNeT.py
@@ -1,7 +1,44 @@
 import matplotlib
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import MoNeT_MGDrivE as monet
+
+###############################################################################
+# Temporary function definition (will move to a separate file when time allows it)
+###############################################################################
+
+def plotMeanGenotypeTrace(aggData, style):
+    groups = aggData['genotypes']
+    pops = aggData['population']
+    time = np.arange(len(pops))
+    df = pd.DataFrame(time, columns=['Time'])
+    final = [df[['Time']] for _ in range(len(groups))]
+    local = pd.DataFrame(pops, columns=groups)
+    fig, ax = plt.subplots()
+    ax.set_aspect(aspect=style["aspect"])
+    # plt.xticks([])
+    # plt.yticks([])
+    for j in range(len(groups)):
+        final[j].insert(1, groups[j] + str(1), (local[groups[j]]).copy())
+        final[j] = final[j].set_index('Time')
+    for i in range(len(final)):
+        final[i].plot(
+            ax=ax, linewidth=style["width"], legend=False,
+            color=style["colors"][i], alpha=style["alpha"]
+        )
+    legends = []
+    for i in range(len(groups)):
+        legends.append(
+            mpatches.Patch(color=style["colors"][i], label=groups[i])
+        )
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0.)
+    ax.xaxis.set_label_text("")
+    ax.yaxis.set_label_text("")
+    plt.ylabel("Allele Count")
+    return fig
 
 ###############################################################################
 # MCR Construct
@@ -70,5 +107,5 @@ style={
         'yellow', 'magenta', 'purple', 'black', 'cyan', 'teal'
     ]
 }
-fig=monet.plotMeanGenotypeTrace(aggData, style)
+fig=plotMeanGenotypeTrace(aggData, style)
 fig
