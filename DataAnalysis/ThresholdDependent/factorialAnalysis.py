8567285723cd30bb8c5cace02dffa06bd7359f91
chipdelmal <chipdelmal@gmail.com>
2019-02-25 10:24:23 -0800

    Splitting the "factorialAnalysis" script in two because it makes more sense to have: "Analysis" and "Differences" separated.

--- DataAnalysis/ThresholdDependent/factorialAnalysis.py
+++ DataAnalysis/ThresholdDependent/factorialAnalysis.py
@@ -5,16 +5,11 @@ import fnmatch
 import os
 import csv
 import time
-import plotly.offline as offline
-import plotly.graph_objs as go
-import plotly
 import numpy as np
 import MoNeT_MGDrivE as monet
 from sklearn.externals.joblib import Parallel, delayed
-#import temporaryFunctionsDefinitions as tempFun
-offline.init_notebook_mode(connected=True)
 import inspect
-print inspect.getsource(monet.loadAndHashFactorialCSV)
+# print inspect.getsource(monet.loadAndHashFactorialCSV)
 
 ###############################################################################
 # Factorial Experiment Example
@@ -22,7 +17,7 @@ print inspect.getsource(monet.loadAndHashFactorialCSV)
 dataType = float
 filename = "UDSA"
 #path = "/Users/sanchez.hmsc/Desktop/" + filename + "/"
-path = "/Volumes/marshallShare/Batch_SA/Translocations_Batch_001/2019_02_07_ANALYZED/"
+path = "/Volumes/marshallShare/Batch_SA/UDMel_Batch_010/2019_02_12_ANALYZED/"
 wildsList = [1, 1, 2, 2, 3, 3, 4, 5, 6, 1, 1, 2, 4, 4, 5, 7, 7, 8]
 homingList = [4, 5, 6, 7, 7, 8, 8, 9, 9, 2, 3, 3, 5, 6, 6, 8, 9, 9]
 aggregationDictionary = monet.generateAggregationDictionary(
@@ -69,84 +64,3 @@ print((end - start)/60)
 ###############################################################################
 outFilename = filename + ".csv"
 monet.compileFactorialCSVFromFiles(path, outFilename)
-
-###############################################################################
-# Load compiled CSV and analyze the output
-###############################################################################
-path = "/Volumes/marshallShare/MGDrivE_Datasets/ThresholdDependent/Datasets/"
-centralFile = "TB.csv"
-coverageRescale = 10
-sensitivitySelector = 1
-if(sensitivitySelector == 1):
-    # 001 larval life decrease
-    # 002 larval life increase
-    title = "Larval Lifespan"
-    testFileA = "TBSA_001.csv"
-    testFileB = "TBSA_002.csv"
-if(sensitivitySelector == 2):
-    # 010 adult life increase
-    # 020 adult life decrease
-    title = "Adult Mortality"
-    testFileA = "TBSA_010.csv"
-    testFileB = "TBSA_020.csv"
-if(sensitivitySelector == 3):
-    # 100 fitness cost 10% reduction
-    # 200 fitness cost 20% reduction
-    title = "Fitness Cost"
-    testFileA = "TBSA_100.csv"
-    testFileB = "TBSA_200.csv"
-centralData = monet.loadAndHashFactorialCSV(
-    path + centralFile, floatMultiplier=coverageRescale
-)
-probeDataA = monet.loadAndHashFactorialCSV(
-    path + testFileA, floatMultiplier=coverageRescale
-)
-probeDataB = monet.loadAndHashFactorialCSV(
-    path + testFileB, floatMultiplier=coverageRescale
-)
-differencesHashA = monet.calculateFactorialHashError(
-    probeDataA,
-    centralData,
-    monet.sampleDifference
-)
-differencesHashB = monet.calculateFactorialHashError(
-    probeDataB,
-    centralData,
-    monet.sampleDifference
-)
-errorsA = differencesHashA.values()
-errorsB = differencesHashB.values()
-
-deHashedA = monet.deHashFactorial(differencesHashA)
-deHashedB = monet.deHashFactorial(differencesHashB)
-
-np.savetxt(
-    path + testFileA.split(".")
-    [0] + "Diff.csv", deHashedA, fmt='%2.6f', delimiter=","
-)
-np.savetxt(
-    path + testFileB.split(".")
-    [0] + "Diff.csv", deHashedB, fmt='%2.6f', delimiter=","
-)
-
-###############################################################################
-# Plotting the results
-###############################################################################
-binsDict = dict(start=0, end=1, size=0.05)
-trace1 = go.Histogram(
-    x=errorsA, histnorm='percent',
-    name='Increase', xbins=binsDict,
-    marker=dict(color='#B9C1DB'), opacity=0.75
-)
-trace2 = go.Histogram(
-    x=errorsB, histnorm='percent',
-    name='Decrease', xbins=binsDict,
-    marker=dict(color='#FF7373'), opacity=0.75
-)
-layout = go.Layout(
-    title=title, xaxis=dict(title='Difference'),
-    yaxis=dict(title='Count'), bargap=0.125, bargroupgap=0.05
-)
-data = [trace1, trace2]
-fig = go.Figure(data=data, layout=layout)
-plotly.offline.iplot(fig, filename='normalized histogram')
