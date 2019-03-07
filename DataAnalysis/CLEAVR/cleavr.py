f91e82d8b613099b4396d1ffd712171bad8b63d0
chipdelmal <chipdelmal@gmail.com>
2019-03-06 16:43:09 -0800

    Final pushes of the day.

--- DataAnalysis/CLEAVR/cleavr.py
+++ DataAnalysis/CLEAVR/cleavr.py
@@ -7,7 +7,7 @@ import auxiliaryFunctions as aux
 ###############################################################################
 BATCH = True
 DRIVE = 4
-EXPORT_TO_DRIVE = False
+EXPORT_TO_DRIVE = True
 PROBED = "E_85_50_20_50"
 # Key: E_(Cutting rate)_(H allele cost)_(releases)_(mosquitoes per release)
 ###############################################################################
@@ -21,11 +21,11 @@ else:
     output = "./images/"+id+"/"
 directories = monet.listDirectoriesWithPathWithinAPath(path)
 # Style .......................................................................
-colors = ['#232ed1', '#ff009d', '#9f00cc', '#ec0b43', '#94d4ff']
+colors = ["#090446", "#ed0091", "#7fff3a", "#7692ff", "#29339b", "#c6d8ff"]
 if (DRIVE == 1) or (DRIVE == 2):
-    aspect, yrange = [.03, 25000]
+    aspect, yrange = [.0175, 50000]
 elif (DRIVE == 3) or (DRIVE == 4):
-    aspect, yrange = [.03, 25000]
+    aspect, yrange = [.0175, 50000]
 styleTrace = {
     "width": 2, "alpha": 1, "dpi": 512, "legend": True,
     "aspect": aspect, "colors": colors, "xrange": 3600, "yrange": yrange
