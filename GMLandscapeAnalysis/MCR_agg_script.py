de743facf0fc5333f6db87dae0630f91e4923355
Biyonka Liang <Biyonka@biyonka.local>
2019-03-06 18:55:30 -0800

    got drive metrics for all contrived datasets

--- GMLandscapeAnalysis/MCR_agg_script.py
+++ GMLandscapeAnalysis/MCR_agg_script.py
@@ -3,6 +3,8 @@ import plotly.graph_objs as go
 import plotly.offline as offline
 import MoNeT_MGDrivE as monet
 import plotly.plotly as py
+import numpy as np
+import pandas as pd
 offline.init_notebook_mode(connected=True)
 
 
@@ -10,48 +12,38 @@ offline.init_notebook_mode(connected=True)
 # MCR Construct
 ###############################################################################
 #------------------------------------------------------------------------------
-# Data Handling
+# Analyzing different metrics on drive behavior
 #------------------------------------------------------------------------------
-
-
-ITERATIONS = 10
-time_to_thres = []
-for i in range(1, ITERATIONS+1):
-    #print(i)
+experiments = ['BASE', 'LOOSE', 'TIGHT', 'UNEVEN']
+time_to_thres, intersection, prop_homing_end = [], [], []
+for i in experiments:
     # Define the experiment's path, aggregation dictionary, and read filenames
     type = float
-    formatted_num = '%03d' % i
-    #experimentString = "E_095_075_006_015"
-    #path = "/Users/sanchez.hmsc/odrive/sanchez.hmsc@berkeley.edu/GMLandscape/ParserDataset/"
-    experimentString = "E_099_000_000_"+formatted_num
-
-    path = "/Users/Biyonka/Desktop/Output/2019_02_27_ANALYZED/"
+    experimentString = "E_099_000_000_001"
+    path = "/Users/Biyonka/Desktop/Output/contrived_landscapes/" + i + "_2019_02_27_ANALYZED/"
     aggregationDictionary = monet.generateAggregationDictionary(
         ["W", "H", "R", "B"],
         [
-            [0, 0, 1, 2, 3],    # Wild
-            [1, 4, 4, 5, 6],    # Homing
-            [2, 5, 7, 7, 8],    # Resistant
-            [3, 6, 8, 9, 9]     # Broken
+            [0, 0, 1, 2, 3],
+            [1, 4, 4, 5, 6],
+            [2, 5, 7, 7, 8],
+            [3, 6, 8, 9, 9]
         ]
     )
     filenames = monet.readExperimentFilenames(path + experimentString)
+
     # To analyze a single node ...................................................
     # Load a single node (auxiliary function just for demonstration)
-    # nodeIndex = 0
-    # nodeData = monet.loadNodeData(
-    #     filenames.get("male")[nodeIndex],
-    #     filenames.get("female")[nodeIndex],
-    #     dataType=float
-    # )
+    nodeIndex = 0
+    nodeData = monet.loadNodeData(
+        filenames.get("male")[nodeIndex], filenames.get("female")[nodeIndex],
+        dataType=float
+    )
 
     # To analyze the sum of the whole landscape ..................................
     # Sum landscape into one array ("in place" memory-wise)
     landscapeSumData = monet.sumLandscapePopulationsFromFiles(
-        filenames,
-        male=True,
-        female=True,
-        dataType=float
+        filenames, male=True, female=True, dataType=float
     )
 
     # Aggregate genotypes (node or landscape) ....................................
@@ -59,23 +51,38 @@ for i in range(1, ITERATIONS+1):
         landscapeSumData,
         aggregationDictionary
     )
-
-    # To analyze the landscape without sum .......................................
-    # Load the population dynamics data of the whole landscape
-    # landscapeData = monet.loadLandscapeData(filenames, dataType=float)
-    # aggregatedNodesData = monet.aggregateGenotypesInLandscape(
-    #     landscapeData,
-    #     aggregationDictionary
-    # )
-    # aggregatedNodesData["landscape"]
-
-    #get wild type proportion at end
-    wbh = aggData['population'][:, 0] + aggData['population'][:, 1] + aggData['population'][:, 3]
+    wbh = np.sum(aggData['population'], axis =1)
     ratio_homing = aggData['population'][:, 1]/(wbh)
     #get time at which homing ratio exceeds threshold
-    time = (list(ratio_homing>0.5)).index(True)
+    if any(ratio_homing >= 0.5):
+        #get first time that ratio exceeds 0.5
+        time = (list(ratio_homing> 0.5)).index(True)
+    else:
+        # if threshold of homing never eceeds 0.5, set time to
+        #total length of experiment (i.e. 3 years)
+        time = aggData['population'].shape[0]
     time_to_thres.append(time)
-{'iteration': range(1, ITERATIONS+1, 1), 'time_to_thres': time_to_thres}
+
+    #get proportion of homing at end of 3 years
+    prop_homing = aggData['population'][:, 1][-1]/wbh[-1]
+    prop_homing_end.append(prop_homing)
+
+    #get the time of intersection between wild and homing
+    diff = aggData['population'][:, 1] - aggData['population'][:, 0]
+    if i == "TIGHT":
+        #no intersection occurs in the tight experiment
+        inter = aggData['population'].shape[0]
+    else:
+        #get index where the wild and homing arrays are the closest
+        inter = list(np.abs(diff)).index(min(np.abs(diff)))
+    intersection.append(inter)
+
+metrics_df = pd.DataFrame({'experiment': experiments,
+'time_to_thres': time_to_thres,
+'intersection': intersection,
+'prop_homing_end': prop_homing_end})
+metrics_df['num_inflection'] = [1, 2, 1, 2]
+metrics_df.to_csv("metrics.csv", header = True, index = False)
 #------------------------------------------------------------------------------
 # Plotting
 #------------------------------------------------------------------------------
