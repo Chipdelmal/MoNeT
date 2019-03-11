0150e1c0b7e70d4802ff968affd0c2c560733580
chipdelmal <chipdelmal@gmail.com>
2019-03-11 14:45:49 -0700

    Trying to fix installing error with MoNeTv0.4.3

--- PythonPkg/setup.py
+++ PythonPkg/setup.py
@@ -1,23 +1,23 @@
-from setuptools import setup
+import setuptools
 
-with open('README.md') as f:
-    long_description = f.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-
-setup(
-    name='MoNeT_MGDrivE',
-    version='0.4.3',
-    url='https://chipdelmal.github.io/MoNeT/',
-    author='Hector M. Sanchez C.',
-    author_email='sanchez.hmsc@berkeley.edu',
-    description="MoNeT python package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=['numpy', 'scipy', 'matplotlib',
-                      'ipython', 'jupyter', 'pandas', 'sympy'],
-    license='MIT',
-    classifiers=[
+setuptools.setup(
+	  name='MoNeT_MGDrivE',
+      version='0.4.4',
+      url='https://chipdelmal.github.io/MoNeT/',
+      author='Hector M. Sanchez C.',
+      author_email='sanchez.hmsc@berkeley.edu',
+      description="MoNeT python package",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=setuptools.find_packages(),
+      install_requires=['numpy', 'scipy', 'matplotlib',
+                        'ipython', 'jupyter', 'pandas', 'sympy'],
+      license='MIT',
+      classifiers=[
         "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
-    ],
+      ],
  )
