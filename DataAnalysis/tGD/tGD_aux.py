f205ed7cc18d95f5cbee7c042f260a7e0af2b557
chipdelmal <chipdelmal@gmail.com>
2019-03-12 14:44:59 -0700

    Attempted parallel plot export and did not work. Libraries clashes.

--- DataAnalysis/tGD/tGD_aux.py
+++ DataAnalysis/tGD/tGD_aux.py
@@ -1,6 +1,9 @@
 import numpy as np
 import os as os
 import csv as csv
+import MoNeT_MGDrivE as monet
+import tGD_plots as plots
+import matplotlib.pyplot as plt
 
 
 def reachedSteadtStateAtDay(
@@ -95,3 +98,61 @@ def getRatiosAtEnd(aggData, groupingsList, finalFrame):
         ratios = total / sum(finalFramePop)
         outList[i] = [ratios] # [total, ratios]
     return flatten(outList)
+
+
+def exportStackPlotFromPath(
+    pathSample,
+    pathRoot,
+    DRIVE,
+    aggregationDictionary,
+    styleS,
+    xRange,
+    yRange
+):
+        experimentString = pathSample.split("/")[-1]
+        filenames = monet.readExperimentFilenames(pathSample)
+        #####################################################################
+        landscapeSumData = monet.sumLandscapePopulationsFromFiles(
+            filenames, male=True, female=True, dataType=float
+        )
+        aggData = monet.aggregateGenotypesInNode(
+            landscapeSumData,
+            aggregationDictionary
+        )
+        #####################################################################
+        if (DRIVE == 1 or DRIVE == 2):
+            groupingsList = [[2]]
+            ratiosAtEnd = getRatiosAtEnd(aggData, groupingsList, -1)
+            ffString = "r(R): " + format(ratiosAtEnd[0], '.3f')
+        else:
+            groupingsList = [[2], [3], [2, 3]]
+            ratiosAtEnd = getRatiosAtEnd(aggData, groupingsList, -1)
+            ffString = "r(R1): " + format(ratiosAtEnd[0], '.3f') + ", r(R2): " + format(ratiosAtEnd[1], '.3f') + ", r(R1+R2): " + format(ratiosAtEnd[2], '.3f')
+        ssDay = reachedSteadtStateAtDay(aggData, .01)
+        if not (DRIVE == 1 or DRIVE == 2):
+            aggData = {
+                "genotypes": aggData["genotypes"],
+                "population": aggData["population"]/2
+            }
+        #####################################################################
+        # figA = plots.plotMeanGenotypeTrace(aggData, styleT, ssDay, 2 * yRange)
+        # figA.get_axes()[0].set_xlim(0, xRange)
+        # figA.get_axes()[0].set_ylim(0, 2 * yRange)
+        figB = plots.plotMeanGenotypeStack(aggData, styleS, ssDay, 2 * yRange)
+        figB.get_axes()[0].set_xlim(0, xRange)
+        figB.get_axes()[0].set_ylim(0, 2 * yRange)
+        figB.get_axes()[0].set_title(
+            "[tSS: " + str(ssDay) + ", " + ffString + "]",
+            fontsize=6
+        )
+        # monet.quickSaveFigure(
+        #     figA,
+        #     "./images/" + str(DRIVE).rjust(2, "0") + "T_" +
+        #     experimentString + ".png"
+        # )
+        monet.quickSaveFigure(
+            figB,
+            pathRoot + "/images/stacks/" + str(DRIVE).rjust(2, "0") + "S_" +
+            experimentString + ".png"
+        )
+        plt.close()
