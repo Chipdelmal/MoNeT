eec8b3e78b66892f7a59630039ae92c66d2a73ef
chipdelmal <chipdelmal@gmail.com>
2019-03-14 17:45:56 -0700

    Minor change to the aggregation script.

--- DataAnalysis/Heterogeneity/aggregation_main.py
+++ DataAnalysis/Heterogeneity/aggregation_main.py
@@ -42,90 +42,90 @@ aggregationDictionary = monet.generateAggregationDictionary(
 )
 styleS = {
     "width": 0, "alpha": .9, "dpi": 1024, "legend": False,
-    "aspect": .025, "colors": colors, "xRange": [0, 1800], "yRange": [0, 11000],
+    "aspect": .01, "colors": colors, "xRange": [0, 1000], "yRange": [0, 11000],
     "vLinesWidth": .5
 }
 styleT = {
     "width": .1, "alpha": .25, "dpi": 1024, "legend": True,
-    "aspect": 2, "colors": colors, "xRange": [0, 1800], "yRange": [0, 11000],
+    "aspect": 2, "colors": colors, "xRange": [0, 1000], "yRange": [0, 11000],
     "vLinesWidth": .5
 }
 ##############################################################################
 # Paths
 ##############################################################################
 folderNames = monet.listDirectoriesInPath(pathRoot + pathExperiments)
-#for nameExp in folderNames[2:]:
-pathFull = pathRoot + pathExperiments + nameExp
-##########################################################################
-# Stack
-##########################################################################
-if STACK is True:
-    filenames = monet.readExperimentFilenames(pathFull + "/ANALYZED/")
-    landscapeSumData = monet.sumLandscapePopulationsFromFiles(
-        filenames, male=True, female=True, dataType=float
-    )
-    aggData = monet.aggregateGenotypesInNode(
-        landscapeSumData, aggregationDictionary
-    )
-    ssDay = monet.reachedSteadtStateAtDay(aggData, .01)
-    figB = monet.plotMeanGenotypeStack(
-        aggData,
-        styleS,
-        vLinesCoords=[RELEASE_DAY, ssDay]
-    )
-    figB.get_axes()[0].set_xlim(styleS["xRange"][0], styleS["xRange"][1])
-    figB.get_axes()[0].set_ylim(styleS["yRange"][0], styleS["yRange"][1])
-    figB.get_axes()[0].set_title(
-        "[tSS: " + str(ssDay-RELEASE_DAY) + "]",
-        fontsize=4
-    )
-    monet.quickSaveFigure(
-        figB, pathPlots + "S_" + nameExp + ".png"
-    )
-    plt.close()
-##########################################################################
-# Spread Plot (Heatmaps)
-##########################################################################
-if SPREAD is True:
-    filenames = monet.readExperimentFilenames(pathFull + "/ANALYZED/")
-    landscapeData = monet.loadLandscapeData(filenames, dataType=float)
-    aggregatedNodesData = monet.aggregateGenotypesInLandscape(
-        landscapeData, aggregationDictionary
-    )
-    geneSpatiotemporals = monet.getGenotypeArraysFromLandscape(
-        aggregatedNodesData
-    )
-    overlay = monet.plotGenotypeOverlayFromLandscape(
-        geneSpatiotemporals, style={"aspect": 12, "cmap": cmaps}
-    )
-    monet.quickSaveFigure(
-        overlay, pathPlots + "O_" + nameExp + ".png"
-    )
-    plt.close()
-##########################################################################
-# Garbage (Traces)
-##########################################################################
-if TRACES is True:
-    paths = monet.listDirectoriesWithPathWithinAPath(pathFull + "/GARBAGE/")
-    landscapeReps = monet.loadAndAggregateLandscapeDataRepetitions(
-        paths, aggregationDictionary,
-        male=False, female=True, dataType=float
-    )
-    yRange = aux.calculateMaxPopulationInLandscape(landscapeReps)
-    aspect = styleT["xRange"][1] / yRange
-    styleT["aspect"] = .15 * aspect
-    figsArray = monet.plotLandscapeDataRepetitions(landscapeReps, styleT)
-    for i in range(0, len(figsArray)):
-        figsArray[i].get_axes()[0].set_xlim(
-            styleT["xRange"][0], styleT["xRange"][1]
+for nameExp in folderNames[0:]:
+    pathFull = pathRoot + pathExperiments + nameExp
+    ##########################################################################
+    # Stack
+    ##########################################################################
+    if STACK is True:
+        filenames = monet.readExperimentFilenames(pathFull + "/ANALYZED/")
+        landscapeSumData = monet.sumLandscapePopulationsFromFiles(
+            filenames, male=True, female=True, dataType=float
         )
-        figsArray[i].get_axes()[0].set_ylim(
-            styleT["yRange"][0],
-            1.5 * aux.calculateMaxPopulationInLandscape(landscapeReps)
+        aggData = monet.aggregateGenotypesInNode(
+            landscapeSumData, aggregationDictionary
+        )
+        ssDay = monet.reachedSteadtStateAtDay(aggData, .01)
+        figB = monet.plotMeanGenotypeStack(
+            aggData,
+            styleS,
+            vLinesCoords=[RELEASE_DAY, ssDay]
+        )
+        figB.get_axes()[0].set_xlim(styleS["xRange"][0], styleS["xRange"][1])
+        figB.get_axes()[0].set_ylim(styleS["yRange"][0], styleS["yRange"][1])
+        figB.get_axes()[0].set_title(
+            "[tSS: " + str(ssDay-RELEASE_DAY) + "]",
+            fontsize=4
         )
         monet.quickSaveFigure(
-            figsArray[i],
-            pathRoot + "images/Garbage/" + nameExp + "_" +
-            str(i).rjust(3, "0") + ".png"
+            figB, pathPlots + "S_" + nameExp + ".png"
         )
         plt.close()
+    ##########################################################################
+    # Spread Plot (Heatmaps)
+    ##########################################################################
+    if SPREAD is True:
+        filenames = monet.readExperimentFilenames(pathFull + "/ANALYZED/")
+        landscapeData = monet.loadLandscapeData(filenames, dataType=float)
+        aggregatedNodesData = monet.aggregateGenotypesInLandscape(
+            landscapeData, aggregationDictionary
+        )
+        geneSpatiotemporals = monet.getGenotypeArraysFromLandscape(
+            aggregatedNodesData
+        )
+        overlay = monet.plotGenotypeOverlayFromLandscape(
+            geneSpatiotemporals, style={"aspect": 12, "cmap": cmaps}
+        )
+        monet.quickSaveFigure(
+            overlay, pathPlots + "O_" + nameExp + ".png"
+        )
+        plt.close()
+    ##########################################################################
+    # Garbage (Traces)
+    ##########################################################################
+    if TRACES is True:
+        paths = monet.listDirectoriesWithPathWithinAPath(pathFull + "/GARBAGE/")
+        landscapeReps = monet.loadAndAggregateLandscapeDataRepetitions(
+            paths, aggregationDictionary,
+            male=False, female=True, dataType=float
+        )
+        yRange = aux.calculateMaxPopulationInLandscape(landscapeReps)
+        aspect = styleT["xRange"][1] / yRange
+        styleT["aspect"] = .15 * aspect
+        figsArray = monet.plotLandscapeDataRepetitions(landscapeReps, styleT)
+        for i in range(0, len(figsArray)):
+            figsArray[i].get_axes()[0].set_xlim(
+                styleT["xRange"][0], styleT["xRange"][1]
+            )
+            figsArray[i].get_axes()[0].set_ylim(
+                styleT["yRange"][0],
+                1.5 * aux.calculateMaxPopulationInLandscape(landscapeReps)
+            )
+            monet.quickSaveFigure(
+                figsArray[i],
+                pathRoot + "images/Garbage/" + nameExp + "_" +
+                str(i).rjust(3, "0") + ".png"
+            )
+            plt.close()
