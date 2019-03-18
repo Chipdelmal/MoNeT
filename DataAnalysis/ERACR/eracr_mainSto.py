c822d23b422e4b6843ca71715abe0fd56bd50aee
chipdelmal <chipdelmal@gmail.com>
2019-03-18 16:42:31 -0700

    Minor plot style corrections

--- DataAnalysis/ERACR/eracr_mainSto.py
+++ DataAnalysis/ERACR/eracr_mainSto.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 plt.rcParams.update({'figure.max_open_warning': 0})
 
+
 def calculateMaxPopulationInLandscape(landscapeReps):
     landscapes = landscapeReps["landscapes"]
     list = [None] * len(landscapeReps["landscapes"][0])
@@ -14,15 +15,18 @@ def calculateMaxPopulationInLandscape(landscapeReps):
             list[i] = sum(landscapes[0][i][0])
     return max(list)
 
+
+STACK = True
+GARBAGE = True
+HEAT = True
 ##############################################################################
 # Setup
 ##############################################################################
 # nameExp = "E_0125_02_00028"
 pathRoot = "/Volumes/marshallShare/vic/"
-pathSet = pathRoot + "eRACR34/" # + "eRACR29"
+pathSet = pathRoot + "eRACR34/"  # + "eRACR29"
 pathOut = pathSet + "images"
 foldersList = glob.glob(pathSet + "*ANALYZED")
-garbage = True
 
 for j in range(len(foldersList)):
     id = foldersList[j].split("/")[-1].split("_")[0]
@@ -32,53 +36,67 @@ for j in range(len(foldersList)):
         filenames = monet.readExperimentFilenames(pathFull)
         filenames
         if (len(filenames["male"]) > 0) or (len(filenames["female"]) > 0):
-            #######################################################################
+            ###################################################################
             colors = [
-                "#090446", "#f20060", "#c6d8ff", "#ff28d4", "#7fff3a", "#7692ff"
+                "#090446", "#f20060", "#6898ff",
+                "#ff28d4", "#7fff3a", "#c6d8ff"
             ]
             cmaps = monet.generateAlphaColorMapFromColorArray(colors)
             styleS = {
                 "width": 0, "alpha": .85, "dpi": 1024, "legend": True,
-                "aspect": .075,#.25,
+                "aspect": .075,  # .25,
                 "colors": colors,
-                "xRange": [0, 5400], "yRange": [0, 12000]#2500]
+                "xRange": [0, 5400], "yRange": [0, 3000]  # 2500]
             }
             styleT = {
-                "width": 0.1, "alpha": .25, "dpi": 1024, "legend": True,
-                "aspect": .075,#.25,
+                "width": 0.1, "alpha": .175, "dpi": 1024, "legend": True,
+                "aspect": 1.75,  # .25,
                 "colors": colors,
-                "xRange": [0, 5400], "yRange": [0, 12000]#2500]
+                "xRange": [0, 2500], "yRange": [0, 300]  # 2500]
             }
-            #######################################################################
+            ###################################################################
             # Population breakdown analysis
-            #######################################################################
-            landscapeSumData = monet.sumLandscapePopulationsFromFiles(
-                        filenames, male=True, female=False, dataType=float
-                    )
-            genotypes = landscapeSumData["genotypes"]
-            aggregationDictionary = monet.autoGenerateGenotypesDictionary(
-                ["W", "H", "E", "R", "B"],
-                genotypes
-            )
-            aggData = monet.aggregateGenotypesInNode(
-                landscapeSumData,
-                aggregationDictionary
-            )
-            #######################################################################
-            figB = monet.plotMeanGenotypeStack(aggData, styleS)
-            figB.get_axes()[0].set_xlim(styleS["xRange"][0], styleS["xRange"][1])
-            figB.get_axes()[0].set_ylim(styleS["yRange"][0], styleS["yRange"][1])
-            monet.quickSaveFigure(
-                figB,
-                pathOut + "/stack/" + id + "-" + nameExp.split("/")[-1] + "_S.png",
-                dpi=500
-            )
-            plt.close()
-
+            ###################################################################
+            if STACK:
+                landscapeSumData = monet.sumLandscapePopulationsFromFiles(
+                    filenames, male=True, female=False, dataType=float
+                )
+                genotypes = landscapeSumData["genotypes"]
+                aggregationDictionary = monet.autoGenerateGenotypesDictionary(
+                    ["W", "H", "E", "R", "B"],
+                    genotypes
+                )
+                aggData = monet.aggregateGenotypesInNode(
+                    landscapeSumData,
+                    aggregationDictionary
+                )
+                ###############################################################
+                figB = monet.plotMeanGenotypeStack(aggData, styleS)
+                figB.get_axes()[0].set_xlim(
+                    styleS["xRange"][0], styleS["xRange"][1])
+                figB.get_axes()[0].set_ylim(
+                    styleS["yRange"][0], styleS["yRange"][1])
+                monet.quickSaveFigure(
+                    figB,
+                    pathOut + "/stack/" + id + "-" +
+                    nameExp.split("/")[-1] + "_S.png",
+                    dpi=500
+                )
+                plt.close()
+            ###################################################################
             # Garbage (Traces)
-            #########################################################################
-            if garbage:
-                garbargePath = nameExp.replace('ANALYZED','GARBAGE')+'/'
+            ###################################################################
+            if GARBAGE:
+                landscapeData = monet.loadLandscapeData(
+                    filenames,
+                    dataType=float
+                )
+                genotypes = landscapeData["genotypes"]
+                aggregationDictionary = monet.autoGenerateGenotypesDictionary(
+                    ["W", "H", "E", "R", "B"],
+                    genotypes
+                )
+                garbargePath = nameExp.replace('ANALYZED', 'GARBAGE')+'/'
                 paths = monet.listDirectoriesWithPathWithinAPath(garbargePath)
                 landscapeReps = monet.loadAndAggregateLandscapeDataRepetitions(
                     paths, aggregationDictionary,
@@ -87,46 +105,54 @@ for j in range(len(foldersList)):
                 for landRepetition in landscapeReps['landscapes']:
                     nodeAggregation = np.sum(landRepetition)
                     landRepetition = nodeAggregation
-
-                yRange = styleT["yRange"][1]
-                aspect =  yRange/styleT["xRange"][1]
-                styleT["aspect"] = aspect
-                figsArray = monet.plotLandscapeDataRepetitions(landscapeReps, styleT)
+                figsArray = monet.plotLandscapeDataRepetitions(
+                    landscapeReps, styleT
+                )
                 for i in range(0, len(figsArray)):
                     figsArray[i].get_axes()[0].set_xlim(
                         styleT["xRange"][0], styleT["xRange"][1]
                     )
                     figsArray[i].get_axes()[0].set_ylim(
-                        styleT["yRange"][0],
-                        1.5 * calculateMaxPopulationInLandscape(landscapeReps)
+                        styleT["yRange"][0], styleT["yRange"][1]
                     )
                     monet.quickSaveFigure(
                         figsArray[i],
-                        pathOut+ "/Garbage/"+ id + "-" + nameExp.split("/")[-1] + "_" +
-                        str(i).rjust(3, "0") + ".png"
+                        pathOut + "/garbage/" + id + "-" +
+                        nameExp.split("/")[-1] + "_" +
+                        str(i).rjust(3, "0") + ".png",
+                        dpi=500
                     )
                     plt.close()
-
-            #######################################################################
-            # Spatial analysis
-            #######################################################################
-            # landscapeData = monet.loadLandscapeData(filenames, dataType=float)
-            # aggregatedNodesData = monet.aggregateGenotypesInLandscape(
-            #     landscapeData,
-            #     aggregationDictionary
-            # )
-            # geneSpatiotemporals = monet.getGenotypeArraysFromLandscape(
-            #     aggregatedNodesData
-            # )
-            # #######################################################################
-            # overlay = monet.plotGenotypeOverlayFromLandscape(
-            #     geneSpatiotemporals,
-            #     style={"aspect": 75, "cmap": cmaps}
-            # )
-            # monet.quickSaveFigure(
-            #     overlay,
-            #     pathOut + "/heat/" + id + "-" + nameExp.split("/")[-1] + "F_L.png",
-            #     dpi=500
-            # )
-            # plt.close()
-            ##########################################################################
+            if HEAT:
+                ###############################################################
+                # Spatial analysis
+                ###############################################################
+                landscapeData = monet.loadLandscapeData(
+                    filenames,
+                    dataType=float
+                )
+                genotypes = landscapeData["genotypes"]
+                aggregationDictionary = monet.autoGenerateGenotypesDictionary(
+                    ["W", "H", "E", "R", "B"],
+                    genotypes
+                )
+                aggregatedNodesData = monet.aggregateGenotypesInLandscape(
+                    landscapeData,
+                    aggregationDictionary
+                )
+                geneSpatiotemporals = monet.getGenotypeArraysFromLandscape(
+                    aggregatedNodesData
+                )
+                ###############################################################
+                overlay = monet.plotGenotypeOverlayFromLandscape(
+                 geneSpatiotemporals,
+                 style={"aspect": 50, "cmap": cmaps}
+                )
+                monet.quickSaveFigure(
+                    overlay,
+                    pathOut + "/heat/" + id + "-" +
+                        nameExp.split("/")[-1] + "F_L.png",
+                    dpi=500
+                )
+                plt.close()
+                ###############################################################
