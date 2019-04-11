430148e312a3c8b4b93dcc35409dffed987968fc
Gillian <gillichu@berkeley.edu>
2019-04-11 14:41:55 -0700

    updating and finally regenerating coord data properly

--- GMLandscapeAnalysis/aggregation.py
+++ GMLandscapeAnalysis/aggregation.py
@@ -3,6 +3,7 @@ import operator
 import numpy as np
 from numpy import genfromtxt
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics.pairwise import euclidean_distances
 import csv
 
 import sys
@@ -64,13 +65,21 @@ def agglom_clustering(points, pop, total_num_nodes, levels_of_agg, filename_stri
 			these assignments.
 	"""
 	std_pop = pop[0]
-	std_distance = points[0] - points[1]
+	std_distance = points[0][0], points[1][0]
+	# std_distance = points[0] - points[1]
 
 	list_of_centroids = []
+
+	# print("total num nodes is ", total_num_nodes)
+	# print("levels of agg ", levels_of_agg)
+
 	for i in range(1, total_num_nodes, int(total_num_nodes/levels_of_agg)):
+		print("here is i ", i)
+		# print("num of points", len(points))
 		clustering = AgglomerativeClustering(n_clusters=i).fit(points)
 		grouping = dict()
 		clustering = clustering.labels_
+
 		for j in range(len(clustering)):
 			group = clustering[j]
 			if group in grouping.keys():
@@ -78,6 +87,8 @@ def agglom_clustering(points, pop, total_num_nodes, levels_of_agg, filename_stri
 			else:
 				grouping[group] = [clustering[j]]
 		
+		print("here is grouping", grouping)
+		# print("num groups is", len(grouping.keys()))
 		#calculate centroids from the points returned in clustering
 		centroids = []
 		for g in grouping:
@@ -89,12 +100,15 @@ def agglom_clustering(points, pop, total_num_nodes, levels_of_agg, filename_stri
 					centroid_pop += pop[k] 
 			#before I append the centroids, add beginning and ending 
 			centroids.append([centroid(nodes_in_centroid), centroid_pop])
-		centroids.insert(0, [std_distance, std_pop])
-		centroids.append([centroids[-1][0][-1]+abs(std_distance), std_pop])
+		# centroids.insert(0, [std_distance, std_pop])
+		# centroids.append([centroids[-1][0][-1]+(abs(std_distance[0]), std_distance[1]), std_pop])
 
+		print("here are centroids", centroids)
+		centroids = sorted(centroids, key=lambda x: x[0])
 
 		with open(filename_string + str(i) + '.csv', mode='w') as destination:
 			writer = csv.writer(destination, delimiter=',')
+			writer.writerow(('x', 'y', 'n'))
 			for c in centroids:
 				writer.writerow((c[0][0], c[0][1], c[1]))
 		#EXPORT in different csv's
--- GMLandscapeAnalysis/main_gillian.py
+++ GMLandscapeAnalysis/main_gillian.py
@@ -1,24 +1,26 @@
 from aggregation import agglom_clustering
 # from spaHeterogeneity import gaussian_cluster
 import numpy as np
+from functions import * 
 
 n = 100
 dist = 15
 stdpop = 50
+totalpop = stdpop * n
 
-L = b.LineGraph(n, dist)
-L.createLineGraph(random=True)
-L.allVertices 
-points = np.array(L.allVerticesCoord())
-sizesLists = np.array([stdpop for i in range(len(points))])
-k = agglom_clustering(points, sizesLists, 100, 10, "Outputs/pusheen")
+# L = b.LineGraph(n, dist)
+# L.createLineGraph(random=True)
+# L.allVertices 
+# points = np.array(L.allVerticesCoord())
+# sizesLists = np.array([stdpop for i in range(len(points))])
+# k = agglom_clustering(points, sizesLists, 100, 10, "Outputs/pusheen")
 
-
-""" Notes:
-Currently the error I get when running gaussian_cluster is that the points returned 
-yield: ValueError: Expected 2D array, got 1D array instead.
-
-"""
+points = array_creation(dist, totalpop, 100, C=0.0)
+# print("here are coords", coords)
+sizesLists = points[2]
+coords = zip(points[0], points[1], sizesLists)
+# sizesLists = population_split(totalpop, n, C=0.0)
+k = agglom_clustering(coords, sizesLists, 100, 20, "Outputs/pusheen")
 
 # points = gaussian_cluster(n, n, 5)
 # sizesLists = np.array([stdpop for i in range(len(points))])
