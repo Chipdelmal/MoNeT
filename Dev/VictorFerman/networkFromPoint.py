5d177de8f3f17b396a97b34a22ccfc9321a8a8ee
Victor Ferman <victor.ferman@gmail.com>
2019-04-15 14:19:10 -0700

    renamed to reflect function, now we also export paths

--- Dev/VictorFerman/networkFromPoint.py
+++ Dev/VictorFerman/networkFromPoint.py
@@ -4,12 +4,15 @@ ox.config(log_file=True,log_console=True,use_cache=True)
 
 def exportBuildingCoordinates(point, name):
     # point must be a tuple (lat,long)
-    buildings=ox.buildings_from_point(point=point,distance=5000)
+    buildings=ox.buildings_from_point(point=point,distance=1800)
     centroidList = buildings.centroid
     placeCoordCSV = open(name+"_LatLongs.csv","w")
     for i in centroidList:
         placeCoordCSV.write(str(i.y)+','+str(i.x)+'\n')
     placeCoordCSV.close()
+    network = ox.graph_from_point(point,distance=1800)
+    ox.save_graphml(network,filename=name+'.grpahml',folder='network')
+    ox.save_graph_shapefile(network, filename=name,folder='network')
 
 
-exportBuildingCoordinates((35.32, -119.01),'calif')
+exportBuildingCoordinates((35.3532, -119.1148),'barkersfiel3')
