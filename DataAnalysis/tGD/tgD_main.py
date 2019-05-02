18d6504a9d3c7bdc567f4891ce38f044b8e24f7a
Sarafina Smith <sarafina98@gmail.com>
2019-05-02 11:04:53 -0700

    Stack animation tgd

--- DataAnalysis/tGD/tgD_main.py
+++ DataAnalysis/tGD/tgD_main.py
@@ -31,10 +31,11 @@ def quickSaveFigure(
 #   5: tGDCross
 #   6: tGDXCross
 ##############################################################################
-DRIVE = 2
+DRIVE = 3
 TRACES = False
-TRACE_ANIMATION = True
+TRACE_ANIMATION = False
 STACK = False
+STACK_ANIMATION = True
 SUMMARIES_DATA = False
 TRACES_DATA = False
 FORMAT = ".png"
@@ -116,6 +117,59 @@ def plotAndSaveLandscapeReps(landscapeReps, ssDay, path):
             dpi=750
         )
         plt.close()
+
+def getFFStrings(aggData):
+    if (DRIVE == 1 or DRIVE == 2):
+        groupingsList = [[2]]
+        groupingsListH = [[1]]
+        ratiosAtEnd = aux.getRatiosAtEnd(aggData, groupingsList, -1)
+        ratiosAtEndH = aux.getRatiosAtEnd(aggData, groupingsListH, -1)
+        ffString = "p(R): " + format(ratiosAtEnd[0], '.3f')
+        ffStringH = "p(H): " + format(ratiosAtEndH[0], '.3f')
+    else:
+        groupingsList = [[2], [3], [2, 3]]
+        groupingsListH = [[1], [4], [1, 4]]
+        ratiosAtEnd = aux.getRatiosAtEnd(aggData, groupingsList, -1)
+        ratiosAtEndH = aux.getRatiosAtEnd(aggData, groupingsListH, -1)
+        intersection = 4 * (ratiosAtEnd[0] * ratiosAtEnd[1])
+        intersectionH = 4 * (ratiosAtEndH[0] * ratiosAtEndH[1])
+        union = 2*ratiosAtEnd[0] + 2*ratiosAtEnd[1] - 4*(ratiosAtEnd[0] * ratiosAtEnd[1])
+        unionH = 2*ratiosAtEndH[0] + 2*ratiosAtEndH[1] - 4*(ratiosAtEndH[0] * ratiosAtEndH[1])
+        ffString = "p(R1): " + format(ratiosAtEnd[0]*2, '.3f') + ", p(R2): " + format(
+            ratiosAtEnd[1]*2, '.3f') + ", p(R1&R2): " + format(intersection,
+            '.3f') + ", p(R1|R2): " + format(union, '.3f')
+        ffStringH = "p(H): " + format(ratiosAtEndH[0]*2, '.3f') + ", p(G): " + format(
+            ratiosAtEndH[1]*2, '.3f') + ", p(H&G): " + format(intersectionH,
+            '.3f') + ", p(H|G): " + format(unionH, '.3f')
+    return ffString, ffStringH
+
+def adjustAggDataForDrive(aggData):
+    if not (DRIVE == 1 or DRIVE == 2):
+            aggData = {
+                "genotypes": aggData["genotypes"],
+                "population": aggData["population"]/2
+            }
+    return aggData
+
+def plotAndSaveStack(aggData, ssDay, ffString, ffStringH, path):
+    figB = plots.plotMeanGenotypeStack(aggData, styleS, ssDay, 2 * yRange)
+    figB.get_axes()[0].set_xlim(0, xRange)
+    figB.get_axes()[0].set_ylim(0, 2 * yRange)
+    figB.get_axes()[0].set_title(
+        "[tSS: " + str(ssDay) + "] :: [" + ffString + "] :: [" + ffStringH + "]",
+        fontsize=5
+    )
+    # monet.quickSaveFigure(
+    #     figA,
+    #     "./images/" + str(DRIVE).rjust(2, "0") + "T_" +
+    #     experimentString + ".png"
+    # )
+    quickSaveFigure(
+        figB,
+        path,
+        dpi=1024
+    )
+    plt.close()
 ##############################################################################
 ##############################################################################        
 if TRACES is True:
@@ -140,11 +194,9 @@ if TRACE_ANIMATION is True:
                 os.mkdir(pathFolder)
             except OSError:  
                 print ("Creation of the directory %s failed" % pathFolder)
-        else:
-            for i in range(1, len(landscapeReps["landscapes"])):
-                print(i)
-                landscapeSublist = {"genotypes": landscapeReps["genotypes"], "landscapes": landscapeReps["landscapes"][0:i]}
-                plotAndSaveLandscapeReps(landscapeSublist, ssDay, pathFolder + "/" + str(i).rjust(6, "0") + ".png")
+        for i in range(1, len(landscapeReps["landscapes"])):
+            landscapeSublist = {"genotypes": landscapeReps["genotypes"], "landscapes": landscapeReps["landscapes"][0:i]}
+            plotAndSaveLandscapeReps(landscapeSublist, ssDay, pathFolder + "/" + str(i).rjust(6, "0") + ".png")
 ##############################################################################
 ##############################################################################
 if STACK is True:
@@ -155,56 +207,38 @@ if STACK is True:
         #####################################################################
         aggData, ssDay, experimentString = getAggDataSSDay(pathsRoot, i)
         #####################################################################
-        if (DRIVE == 1 or DRIVE == 2):
-            groupingsList = [[2]]
-            groupingsListH = [[1]]
-            ratiosAtEnd = aux.getRatiosAtEnd(aggData, groupingsList, -1)
-            ratiosAtEndH = aux.getRatiosAtEnd(aggData, groupingsListH, -1)
-            ffString = "p(R): " + format(ratiosAtEnd[0], '.3f')
-            ffStringH = "p(H): " + format(ratiosAtEndH[0], '.3f')
-        else:
-            groupingsList = [[2], [3], [2, 3]]
-            groupingsListH = [[1], [4], [1, 4]]
-            ratiosAtEnd = aux.getRatiosAtEnd(aggData, groupingsList, -1)
-            ratiosAtEndH = aux.getRatiosAtEnd(aggData, groupingsListH, -1)
-            intersection = 4 * (ratiosAtEnd[0] * ratiosAtEnd[1])
-            intersectionH = 4 * (ratiosAtEndH[0] * ratiosAtEndH[1])
-            union = 2*ratiosAtEnd[0] + 2*ratiosAtEnd[1] - 4*(ratiosAtEnd[0] * ratiosAtEnd[1])
-            unionH = 2*ratiosAtEndH[0] + 2*ratiosAtEndH[1] - 4*(ratiosAtEndH[0] * ratiosAtEndH[1])
-            ffString = "p(R1): " + format(ratiosAtEnd[0]*2, '.3f') + ", p(R2): " + format(
-                ratiosAtEnd[1]*2, '.3f') + ", p(R1&R2): " + format(intersection,
-                '.3f') + ", p(R1|R2): " + format(union, '.3f')
-            ffStringH = "p(H): " + format(ratiosAtEndH[0]*2, '.3f') + ", p(G): " + format(
-                ratiosAtEndH[1]*2, '.3f') + ", p(H&G): " + format(intersectionH,
-                '.3f') + ", p(H|G): " + format(unionH, '.3f')
-        if not (DRIVE == 1 or DRIVE == 2):
-            aggData = {
-                "genotypes": aggData["genotypes"],
-                "population": aggData["population"]/2
-            }
+        ffString, ffStringH = getFFStrings(aggData)
+        aggData = adjustAggDataForDrive(aggData)
         #####################################################################
         # figA = plots.plotMeanGenotypeTrace(aggData, styleT, ssDay, 2 * yRange)
         # figA.get_axes()[0].set_xlim(0, xRange)
         # figA.get_axes()[0].set_ylim(0, 2 * yRange)
-        figB = plots.plotMeanGenotypeStack(aggData, styleS, ssDay, 2 * yRange)
-        figB.get_axes()[0].set_xlim(0, xRange)
-        figB.get_axes()[0].set_ylim(0, 2 * yRange)
-        figB.get_axes()[0].set_title(
-            "[tSS: " + str(ssDay) + "] :: [" + ffString + "] :: [" + ffStringH + "]",
-            fontsize=5
-        )
-        # monet.quickSaveFigure(
-        #     figA,
-        #     "./images/" + str(DRIVE).rjust(2, "0") + "T_" +
-        #     experimentString + ".png"
-        # )
-        quickSaveFigure(
-            figB,
-            pathRoot + "/images/stacks/" + str(DRIVE).rjust(2, "0") + "S_" +
-            experimentString + FORMAT,
-            dpi=1024
-        )
-        plt.close()
+
+        plotAndSaveStack(aggData, ssDay, ffString, ffStringH, pathRoot + "/images/stacks/" + str(DRIVE).rjust(2, "0") + "S_" + experimentString + FORMAT)
+##############################################################################
+##############################################################################
+if STACK_ANIMATION is True:
+    pathsRoot = monet.listDirectoriesWithPathWithinAPath(
+        pathRoot + pathExt + "ANALYZED/"
+    )
+    for i in range(0, len(pathsRoot)):
+        #####################################################################
+        aggData, ssDay, experimentString = getAggDataSSDay(pathsRoot, i)
+        #####################################################################
+        ffString, ffStringH = getFFStrings(aggData)
+        aggData = adjustAggDataForDrive(aggData)
+        #####################################################################
+
+        # make folder in /images/stack_animations/ with name of experiment
+        pathFolder = pathRoot + "images/stack_animations/" + str(DRIVE).rjust(2, "0") + "R_" + experimentString 
+        if not os.path.isdir(pathFolder):
+            try:  
+                os.mkdir(pathFolder)
+            except OSError:  
+                print ("Creation of the directory %s failed" % pathFolder)
+        for i in range(1, aggData["population"].shape[0]):
+            aggSublist = {"genotypes": aggData["genotypes"], "population": aggData["population"][:i, :]}
+            plotAndSaveStack(aggSublist, ssDay, ffString, ffStringH, pathFolder+ "/" + str(i).rjust(6, "0") + ".png")       
 ##############################################################################
 ##############################################################################
 if SUMMARIES_DATA is True:
