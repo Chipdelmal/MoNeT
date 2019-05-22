b0d8e327124f7f796506d0677510ad421723abc4
Gillian <gillichu@berkeley.edu>
2019-05-22 00:41:49 -0700

    Added code to randomly drop, trying to aggregate bakersfield with 186k nodes but hier agg seems to be failing due to too many nodes.

--- GMLandscapeAnalysis/main_gillian.py
+++ GMLandscapeAnalysis/main_gillian.py
@@ -35,19 +35,46 @@ totalpop = stdpop * n
 # sizesLists = np.array([stdpop for i in range(len(points))])
 # k = agglom_clustering(points, sizesLists, 100, 10, "Outputs/pusheen")
 
+from numpy import genfromtxt
+bakersfield = genfromtxt('barkersfielFull_LatLongs.csv', delimiter=',')
+n = len(bakersfield[:,0])
+print("we have ", n, " nodes in bakersfield")
+sizesLists = np.array(population_split(20*n, n, C=0.0))
+smaller_baker = []
+
+for i in range(n):
+    b = np.random.uniform(0, 1)
+    if b > 0.80:
+        smaller_baker.append(bakersfield[i])
+k = len(smaller_baker)
+print("we have ", k, " nodes in smaller bakersfield")
+k = agglom_clustering(smaller_baker, sizesLists, k, 5, "bakeoutputs/pusheen")
+
+
+# points = array_creation2d(dist, totalpop, 100, C=0.0)
+# print(points)
+# sizesLists = points[2]
+# # print(len(points[0]))
+# coords = zip(points[0], points[1], sizesLists)
+# # sizesLists = population_split(totalpop, n, C=0.0)
+# k = agglom_clustering(coords, sizesLists, 100, 20, "2d_outputs/pusheen")
+# points = gaussian_cluster(n, n, 5)
+# sizesLists = np.array([stdpop for i in range(len(points))])
+# k = agglom_clustering(points, sizesLists, 100, 10, "Outputs/pusheen")
+
 import csv
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-with open('2d_outputs/pusheen11.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    colors = cm.rainbow(np.linspace(0, 1, 11))
-    print(colors)
-    for row in readCSV:
-        if row[0] == 'x':
-            continue
-        print(row)
-        c = colors[int(row[3])]
-        plt.plot(int(row[0]), int(row[1]), marker='o', markersize=3, color=c)
+# with open('2d_outputs/pusheen11.csv') as csvfile:
+#     readCSV = csv.reader(csvfile, delimiter=',')
+#     colors = cm.rainbow(np.linspace(0, 1, 11))
+#     print(colors)
+#     for row in readCSV:
+#         if row[0] == 'x':
+#             continue
+#         print(row)
+#         c = colors[int(row[3])]
+#         plt.plot(int(row[0]), int(row[1]), marker='o', markersize=3, color=c)
 
-    plt.show()
+#     plt.show()
