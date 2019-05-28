ab91bb8fb1f495f5dc12c4e88776cd20f204e653
chipdelmal <chipdelmal@gmail.com>
2019-05-28 12:44:09 -0700

    Minor aesthetic updates to the ffmpegTimelapse script.

--- ffmpeg/ffmpegTimelapse.py
+++ ffmpeg/ffmpegTimelapse.py
@@ -6,16 +6,21 @@ id = "Networks"
 outName = "outVideo"
 rate = 24
 
-outPath = path + outName + '.mp4'
 inPath = path + id + '%06d.png" '
-
-fullCmd = optsA + inPath + optsB + optsC + outPath
-subprocess.Popen(['ffmpeg', '-start_number', '1','-r', str(rate), '-f', 'image2', '-s', '1920x1080', '-i', inPath, '-vf', 'scale=trunc(iw/2)*2:trunc(ih/2)*2', '-vcodec', 'libx264', '-preset', 'veryslow','-crf', '15', '-pix_fmt', 'yuv420p',outPath])
+outPath = path + outName + '.mp4'
 
 
-# ffmpegCmd = 'ffmpeg -start_number 1 -r 24 -f image2 -s 1920x1080 -i Networks%06d.png -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -vcodec libx264 -preset veryslow -crf 15 -pix_fmt yuv420p 00VD.mp'
-# video = subprocess.Popen(['ffmpeg', '-r', '24', '-f', 'image2', '-s', '1920x1080', '-i', subfolder+experiment+'/%05d.png', '-vcodec',
-#                           'libx264', '-crf', '25', '-vf', 'pad=ceil(iw/2)*2:ceil(ih/2)*2', '-pix_fmt', 'yuv420p', folder+'videos/'+vname+'.mp4'])
-# 'ffmpeg -start_number 1 -r 24 -f image2 -s 1920x1080 -i Networks%06d.png -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -vcodec libx264 -preset veryslow -crf 15 -pix_fmt yuv420p 00VD.mp'
-#
-# print(os.system("pwd"))
+subprocess.Popen([
+    'ffmpeg',
+    '-start_number', '1',
+    '-r', str(rate),
+    '-f', 'image2',
+    '-s', '1920x1080',
+    '-i', inPath,
+    '-vf', 'scale=trunc(iw/2)*2:trunc(ih/2)*2',
+    '-vcodec', 'libx264',
+    '-preset', 'veryslow',
+    '-crf', '15',
+    '-pix_fmt', 'yuv420p',
+    outPath
+])
