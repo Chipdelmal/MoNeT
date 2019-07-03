58df2a8024a94ced6e67a5c87109fe35a4ff9bd4
chipdelmal <chipdelmal@gmail.com>
2019-07-03 12:58:49 -0700

    Minor updates to the clustering routines (NEEDS UPDATE OF MONET)

--- DataAnalysis/ERACR/Fowler.py
+++ DataAnalysis/ERACR/Fowler.py
@@ -1,15 +1,21 @@
-import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import FowlerAux as aux
+import vincenty as vn
+import MoNeT_MGDrivE as monet
 from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import SpectralClustering
 
 ##############################################################################
-(seed, clustersNo, CLST_METHOD) = (10, 1000, 1)
+# Parameters Setup
+##############################################################################
+(seed, clustersNo, CLST_METHOD) = (42, 1000, 3)
 PATH = "/Volumes/marshallShare/ERACR/Fowler/"
 LATLONGS = "fowler_centroids.csv"
 latlongs = np.genfromtxt(PATH + LATLONGS, delimiter=',')
+##############################################################################
+# Clustering Algorithms
 ##############################################################################
 if CLST_METHOD == 1:
     clObj = KMeans(
@@ -21,22 +27,37 @@ elif CLST_METHOD == 2:
         n_clusters=clustersNo,
         affinity='euclidean', compute_full_tree='auto'
     )
+elif CLST_METHOD == 3:
+    clObj = SpectralClustering(
+        n_clusters=clustersNo,
+        random_state=seed
+    )
 clusters = clObj.fit_predict(latlongs)
+##############################################################################
+# Clustering the pointset
 ##############################################################################
 cLatlongs = aux.appendClustersToLatlongs(latlongs, clusters)
 csvPath = PATH + "Fowler" + str(CLST_METHOD) + ".csv"
 aux.exportListToCSV(csvPath, cLatlongs)
 ##############################################################################
+# Plotting
+##############################################################################
 plt.figure(figsize=(20, 20))
 plt.scatter(
     latlongs[:, 0], latlongs[:, 1], c=clusters,
-    alpha=.25, cmap='hsv', s=150
+    alpha=.25, cmap='hsv', s=150, linewidth=4
 )
 plt.tight_layout()
 plt.xlim(min(latlongs[:, 0]) - .0005, max(latlongs[:, 0]) + .0005)
 plt.ylim(min(latlongs[:, 1]) - .0005, max(latlongs[:, 1]) + .0005)
 plt.savefig(
-    "./images/fowler" + str(CLST_METHOD) + ".png",
+    PATH + "fowler" + str(CLST_METHOD) + ".png",
     dpi=250
 )
 plt.close()
+##############################################################################
+# Export distance matrix
+##############################################################################
+distPath = PATH + "FowlerDistanceMatrix.csv"
+distMat = monet.calculateDistanceMatrix(latlongs, distFun=vn.vincenty) * 1000
+np.savetxt(distPath, distMat.astype(int), fmt='%i', delimiter=',')
