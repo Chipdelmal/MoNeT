e26ec70697cdfe3a0df8a026b2b6f9d34ccacb4b
chipdelmal <chipdelmal@gmail.com>
2019-07-02 17:06:13 -0700

    Clustering tests underwayyyyy

--- DataAnalysis/ERACR/Fowler.py
+++ DataAnalysis/ERACR/Fowler.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
-%matplotlib inline
 
-(seed, clustersNo, CLST_METHOD) = (10, 1000, 1)
+
+(seed, clustersNo, CLST_METHOD) = (10, 1000, 2)
 PATH = "/Volumes/marshallShare/ERACR/Fowler/"
 LATLONGS = "fowler_centroids.csv"
 latlongs = np.genfromtxt(PATH + LATLONGS, delimiter=',')
@@ -14,7 +14,7 @@ if CLST_METHOD == 1:
         n_clusters=clustersNo,
         random_state=seed
     )
-elif CLST_METHOD ==2:
+elif CLST_METHOD == 2:
     clObj = AgglomerativeClustering(
         n_clusters=clustersNo,
         affinity='euclidean', compute_full_tree='auto'
@@ -24,8 +24,13 @@ clusters = clObj.fit_predict(latlongs)
 
 plt.figure(figsize=(20, 20))
 plt.scatter(
-    latlongs[:, 0],
-    latlongs[:, 1],
-    c=clusters,
-    alpha=.5, cmap='RdBu'
+    latlongs[:, 0], latlongs[:, 1], c=clusters,
+    alpha=.25, cmap='hsv', s=150
+)
+plt.tight_layout()
+plt.xlim(min(latlongs[:, 0]) - .0005, max(latlongs[:, 0]) + .0005)
+plt.ylim(min(latlongs[:, 1]) - .0005, max(latlongs[:, 1]) + .0005)
+plt.savefig(
+    "./images/fowler" + str(CLST_METHOD) + ".png",
+    dpi=250
 )
