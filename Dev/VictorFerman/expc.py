bbc70f9c4ad43adcece82c72fa1a356083c88c50
Victor Ferman <victor.ferman@gmail.com>
2019-07-04 13:05:01 -0700

    code for the truncated exponential

---
+++ Dev/VictorFerman/expc.py
@@ -0,0 +1,29 @@
+import scipy.stats as stats
+import numpy as np
+
+def approx_equals(a, b, margin):
+    dif = np.fabs(a-b)
+    if dif <= margin:
+        return True
+    else:
+        return False
+    return False
+
+def trunc_exp(x, rate, a ,b):
+    if(a>b):
+        return None
+
+    scale = 1.0/rate
+
+    gA = stats.expon.cdf(a,scale=scale)
+    gB = stats.expon.cdf(b,scale=scale)
+    if approx_equals(gA,gB, 0.000001):
+        return None
+
+    density = stats.expon.pdf(x,scale=scale)/ (stats.expon.cdf(b,scale=scale)-stats.expon.cdf(a,scale=scale))
+
+    return round(density, 10)
+
+
+
+print(trunc_exp(1023.1,1/800.0,50,5e3))
