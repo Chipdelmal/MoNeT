eb7c79c55592e979476119823c2a1ace172fb8db
Maya Shen <mayashen@berkeley.edu>
2019-07-10 21:11:24 -0700

    Swap file updates

--- GMLandscapeAnalysis/popHeterogeneity_SwapAggregation.py
+++ GMLandscapeAnalysis/popHeterogeneity_SwapAggregation.py
@@ -5,6 +5,7 @@ import pandas as pd
 import math
 import MoNeT_MGDrivE as monet
 import matplotlib.pyplot as plt
+import glob
 
 pathRoot = "/Users/mayashen/Desktop/Marshall_Simulations/test/"
 
@@ -29,29 +30,42 @@ for swap in range(NUM_SWAP + 1):
                 invalid_raise=False
             )
 
-np.zeros(5)
-for nameExp in folderNames[0:]:
-    pathFull = pathRoot + nameExp
-    filenames = monet.readExperimentFilenames(
-            pathFull + "/ANALYZED/" + "0001/"
-    )
-    filenames['male'][0]
-a        landscapeSumData = monet.sumLandscapePopulationsFromFiles(
-            filenames, male=True, female=True, dataType=float
-        )
-        landscapeSumData['genotypes']
-        hello = np.genfromtxt(
-            filenames['male'][0],
-            dtype=float,
-            skip_header=1,
-            delimiter=",",
-            invalid_raise=False
-        )
-        hello
-        df = pd.DataFrame(data=hello[0:, 1:], columns=landscapeSumData['genotypes'])
-        df
-        df[]
-        df.to_csv("foo.csv")
-        # aggData = monet.aggregateGenotypesInNode(
-        #     landscapeSumData, aggregationDictionary
-        # )
+aggregationDictionary = monet.generateAggregationDictionary(
+    ["W", "H", "R", "B"],
+    [
+        [0, 0, 1, 2, 3],
+        [1, 4, 4, 5, 6],
+        [2, 5, 7, 7, 8],
+        [3, 6, 8, 9, 9]
+    ]
+)
+
+monet.readExperimentFilenames("/Users/mayashen/Desktop/swap_down_0_0/ANALYZED")
+sexFilenameIdentifiers={"male": "ADM", "female": "AF1"}
+"Users/mayashen/Desktop/swap_down_0_0/ANALYZED" + "/" + sexFilenameIdentifiers["male"] + "*.csv"
+
+pd.read_csv("/Users/mayashen/Desktop/swap_down_0_0/ANALYZED/ADM_Mean_Patch0000.csv")
+glob.glob("/Users/mayashen/Desktop/swap_down_0_0/ANALYZED/ADM_Mean_Patch0000.csv")
+
+glob.glob("/Users/mayashen/Desktop/swap_down_0_0/ANALYZED/ADM*.csv")
+loadLandscapeData(
+    filenames, male=male, female=female, dataType=dataType
+)
+root = "/Users/mayashen/Desktop/repetitions_test/"
+paths = [root+"swap_down_0_"+str(i)+"/ANALYZED/0001" for i in range(5)]
+paths
+test2 = monet.loadAndAggregateLandscapeDataRepetitions(paths, aggregationDictionary, True, True)
+test.keys()
+len(test["landscapes"])
+len(test['landscapes'][0])
+len(test['landscapes'][0][0])
+len(test['landscapes'][0][0][0])
+test['landscapes'][0][0][0]
+test['genotypes']
+
+test2.keys()
+len(test2["landscapes"])
+len(test2['landscapes'][0])
+len(test2['landscapes'][0][0])
+len(test2['landscapes'][0][0][0])
+test2['landscapes'][0][0][0]
