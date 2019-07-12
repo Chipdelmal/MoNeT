a7c11fbd453f748a12ec63cbea14d74cced5dc01
Maya Shen <mayashen@berkeley.edu>
2019-07-12 13:35:55 -0700

    Aggregation edit update

--- GMLandscapeAnalysis/popHeterogeneity_SwapAggregation.py
+++ GMLandscapeAnalysis/popHeterogeneity_SwapAggregation.py
@@ -16,20 +16,6 @@ TIME = 1459
 folderNames = monet.listDirectoriesInPath(pathRoot)
 folderNames[0].split("_")
 
-for swap in range(NUM_SWAP + 1):
-    outputpath = "/Users/mayashen/Desktop/Marshall_Simulations/swap_aggregated/swap_down_"+str(swap)+"/ANALYZED/0001/"
-    for node in range(NUM_NODES):
-        node_data = np.zeros(TIME)
-        for iter in range(NUM_ITER):
-            pathExp = "swap_down_"+str(swap)+"_"+str(iter)
-            np.genfromtxt(
-                pathRoot+pathExp,
-                dtype=float,
-                skip_header=1,
-                delimiter=",",
-                invalid_raise=False
-            )
-
 aggregationDictionary = monet.generateAggregationDictionary(
     ["W", "H", "R", "B"],
     [
@@ -40,32 +26,28 @@ aggregationDictionary = monet.generateAggregationDictionary(
     ]
 )
 
-monet.readExperimentFilenames("/Users/mayashen/Desktop/swap_down_0_0/ANALYZED")
-sexFilenameIdentifiers={"male": "ADM", "female": "AF1"}
-"Users/mayashen/Desktop/swap_down_0_0/ANALYZED" + "/" + sexFilenameIdentifiers["male"] + "*.csv"
-
-pd.read_csv("/Users/mayashen/Desktop/swap_down_0_0/ANALYZED/ADM_Mean_Patch0000.csv")
-glob.glob("/Users/mayashen/Desktop/swap_down_0_0/ANALYZED/ADM_Mean_Patch0000.csv")
-
-glob.glob("/Users/mayashen/Desktop/swap_down_0_0/ANALYZED/ADM*.csv")
-loadLandscapeData(
-    filenames, male=male, female=female, dataType=dataType
-)
 root = "/Users/mayashen/Desktop/repetitions_test/"
 paths = [root+"swap_down_0_"+str(i)+"/ANALYZED/0001" for i in range(5)]
 paths
-test2 = monet.loadAndAggregateLandscapeDataRepetitions(paths, aggregationDictionary, True, True)
+test = monet.loadAndAggregateLandscapeDataRepetitions(paths, aggregationDictionary, True, True)
 test.keys()
 len(test["landscapes"])
 len(test['landscapes'][0])
 len(test['landscapes'][0][0])
 len(test['landscapes'][0][0][0])
 test['landscapes'][0][0][0]
-test['genotypes']
+test['genotypes'][3]
 
-test2.keys()
-len(test2["landscapes"])
-len(test2['landscapes'][0])
-len(test2['landscapes'][0][0])
-len(test2['landscapes'][0][0][0])
-test2['landscapes'][0][0][0]
+comp_data = []
+for patch in range(52):
+    comp_patch = []
+    for row in range(1459):
+        comp_row = np.zeros(4)
+        for rep in range(5):
+            comp_row += test['landscapes'][rep][patch][row]
+        comp_patch.append(comp_row/5)
+    comp_data.append(comp_patch)
+
+comp_data = np.array(comp_data)
+
+pd.DataFrame({'Time':range(1, 1460), 'W': comp_data[0][:,0], 'H': comp_data[0][:,1], 'R': comp_data[0][:,2], 'B': comp_data[0][:,3]}).to_csv("~/Desktop/test.csv", header=["Time", "W", "H", "R", "B"], index=None)
