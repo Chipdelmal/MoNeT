d5ec2c8a4909e8fa48eae0b35aad3113e23ca616
Victor Ferman <victor.ferman@gmail.com>
2019-07-19 14:04:39 -0700

    Aggregation Done

--- DataAnalysis/ERACR/FowlerAux.py
+++ DataAnalysis/ERACR/FowlerAux.py
@@ -45,6 +45,9 @@ if __name__ == "__main__":
     clObj = KMeans(n_clusters=clustersNo, random_state=seed)
     clusters = clObj.fit_predict(latlongs)
     cLatlongs = appendClustersToLatlongs(latlongs, clusters)
+    vicClusterObj = clObj.fit(latlongs)
+    vicCentroids = vicClusterObj.cluster_centers_
+    vicLabels = vicClusterObj.labels_
     ###########################################################################
     # Distance Matrix
     distMat = monet.calculateDistanceMatrix(latlongs, distFun=vn.vincenty) * 1000
@@ -58,18 +61,36 @@ if __name__ == "__main__":
     )
     ###########################################################################
     # Aggregation
-    sitesNum = len(latlongs)
-    clustersID = sorted(set(clusters))
-    clustersNum = len(clustersID)
-    aggrdMat = np.zeros([clustersNum, clustersNum], dtype=float)
-    grpdClust = [list(np.where(clusters == id)[0]) for id in clustersID]
-    grpdLens = [len(clst) for clst in grpdClust]
+    # sitesNum = len(latlongs)
+    # clustersID = sorted(set(clusters))
+    # clustersNum = len(clustersID)
+    # aggrdMat = np.zeros([clustersNum, clustersNum], dtype=float)
+    # grpdClust = [list(np.where(clusters == id)[0]) for id in clustersID]
+    # grpdLens = [len(clst) for clst in grpdClust]
+    #
+    # for row in range(clustersNum):
+    #     rowGroup = grpdClust[row]
+    #     for col in range(clustersNum):
+    #         aggrdMat[row, col] = sum(migrMat[rowGroup][grpdClust[col]])
+    #     aggrdMat[row] = aggrdMat[row] / grpdLens[row]
+    #
+    # sum(aggrdMat[1])
+    # monet.testMarkovMatrix(migrMat)
 
-    for row in range(clustersNum):
-        rowGroup = grpdClust[row]
-        for col in range(clustersNum):
-            aggrdMat[row, col] = sum(migrMat[rowGroup][grpdClust[col]])
-        aggrdMat[row] = aggrdMat[row] / grpdLens[row]
+    num_clusters = len(vicCentroids)
+    aggr_matrix = np.zeros([num_clusters, num_clusters], dtype=float)
+    aggr_latlongs = [[] for x in range(num_clusters)]
+    for idx, label in enumerate(vicLabels):
+        aggr_latlongs[label].append(idx)
+    for row in range(num_clusters):
+        row_ids = aggr_latlongs[row]
+        for colum in range(num_clusters):
+            colum_ids  =aggr_latlongs[colum]
+            res = 0
+            for rid in row_ids:
+                for cid in colum_ids:
+                    res += migrMat[rid][cid]
+            aggr_matrix[row][colum] = res/len(row_ids)
 
-    sum(aggrdMat[1])
-    monet.testMarkovMatrix(migrMat)
+    print(sum(aggr_matrix[1]))
+    monet.testMarkovMatrix(aggr_matrix)
