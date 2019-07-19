6694198d401273997e768b3cd4cbe6779e44993c
chipdelmal <chipdelmal@gmail.com>
2019-07-19 15:17:30 -0700

    Vic's aggregation script is now in a function

--- DataAnalysis/ERACR/FowlerAux.py
+++ DataAnalysis/ERACR/FowlerAux.py
@@ -18,7 +18,21 @@ def exportListToCSV(path, listToExport):
 
 
 def aggregateLandscape(migrationMatrix, clusters):
-    return True
+    num_clusters = len(set(clusters))
+    aggr_matrix = np.zeros([num_clusters, num_clusters], dtype=float)
+    aggr_latlongs = [[] for x in range(num_clusters)]
+    for idx, label in enumerate(clusters):
+        aggr_latlongs[label].append(idx)
+    for row in range(num_clusters):
+        row_ids = aggr_latlongs[row]
+        for colum in range(num_clusters):
+            colum_ids = aggr_latlongs[colum]
+            res = 0
+            for rid in row_ids:
+                for cid in colum_ids:
+                    res += migrationMatrix[rid][cid]
+            aggr_matrix[row][colum] = res/len(row_ids)
+    return aggr_matrix
 
 
 if __name__ == "__main__":
@@ -43,17 +57,15 @@ if __name__ == "__main__":
     ###########################################################################
     # Clustering Algorithms
     clObj = KMeans(n_clusters=clustersNo, random_state=seed)
-    clusters = clObj.fit_predict(latlongs)
+    clustersObj = clObj.fit(latlongs)
+    (clusters, centroids) = (clustersObj.labels_, clustersObj.cluster_centers_)
     cLatlongs = appendClustersToLatlongs(latlongs, clusters)
-    vicClusterObj = clObj.fit(latlongs)
-    vicCentroids = vicClusterObj.cluster_centers_
-    vicLabels = vicClusterObj.labels_
     ###########################################################################
     # Distance Matrix
     distMat = monet.calculateDistanceMatrix(latlongs, distFun=vn.vincenty) * 1000
     ###########################################################################
     # Migration Matrix
-    zeroInflation = .72 ** .09
+    zeroInflation = pow(.72, .09)
     migrMat = monet.zeroInflatedExponentialMigrationKernel(
         distMat,
         params=monet.AEDES_EXP_PARAMS,
@@ -61,36 +73,5 @@ if __name__ == "__main__":
     )
     ###########################################################################
     # Aggregation
-    # sitesNum = len(latlongs)
-    # clustersID = sorted(set(clusters))
-    # clustersNum = len(clustersID)
-    # aggrdMat = np.zeros([clustersNum, clustersNum], dtype=float)
-    # grpdClust = [list(np.where(clusters == id)[0]) for id in clustersID]
-    # grpdLens = [len(clst) for clst in grpdClust]
-    #
-    # for row in range(clustersNum):
-    #     rowGroup = grpdClust[row]
-    #     for col in range(clustersNum):
-    #         aggrdMat[row, col] = sum(migrMat[rowGroup][grpdClust[col]])
-    #     aggrdMat[row] = aggrdMat[row] / grpdLens[row]
-    #
-    # sum(aggrdMat[1])
-    # monet.testMarkovMatrix(migrMat)
-
-    num_clusters = len(vicCentroids)
-    aggr_matrix = np.zeros([num_clusters, num_clusters], dtype=float)
-    aggr_latlongs = [[] for x in range(num_clusters)]
-    for idx, label in enumerate(vicLabels):
-        aggr_latlongs[label].append(idx)
-    for row in range(num_clusters):
-        row_ids = aggr_latlongs[row]
-        for colum in range(num_clusters):
-            colum_ids  =aggr_latlongs[colum]
-            res = 0
-            for rid in row_ids:
-                for cid in colum_ids:
-                    res += migrMat[rid][cid]
-            aggr_matrix[row][colum] = res/len(row_ids)
-
-    print(sum(aggr_matrix[1]))
-    monet.testMarkovMatrix(aggr_matrix)
+    aggregatedMatrix = aggregateLandscape(migrMat, clusters)
+    monet.testMarkovMatrix(aggregatedMatrix)
