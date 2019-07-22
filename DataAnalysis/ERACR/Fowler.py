8b0f61835ea37bdf1279eb11acdf6886b1044505
chipdelmal <chipdelmal@gmail.com>
2019-07-22 12:04:24 -0700

    Iterating over different aggregation levels on Fowler

--- DataAnalysis/ERACR/Fowler.py
+++ DataAnalysis/ERACR/Fowler.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -9,112 +10,97 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import SpectralClustering
 
 
-# def aggregateLandscape(migrationMatrix, clusters):
-#     num_clusters = len(set(clusters))
-#     aggr_matrix = np.zeros([num_clusters, num_clusters], dtype=float)
-#     aggr_latlongs = [[] for x in range(num_clusters)]
-#     for idx, label in enumerate(clusters):
-#         aggr_latlongs[label].append(idx)
-#     for row in range(num_clusters):
-#         row_ids = aggr_latlongs[row]
-#         for colum in range(num_clusters):
-#             colum_ids = aggr_latlongs[colum]
-#             res = 0
-#             for rid in row_ids:
-#                 for cid in colum_ids:
-#                     res += migrationMatrix[rid][cid]
-#             aggr_matrix[row][colum] = res/len(row_ids)
-#     return aggr_matrix
-
-##############################################################################
-# Parameters Setup
-##############################################################################
-(seed, clustersNo, CLST_METHOD) = (42, 500, 1)
-(lifeStayProb, adultMortality) = (.72, .09)
-# PLACE = "BakersfieldRiver"
-# PATH = "/Volumes/marshallShare/ERACR/Bakersfield/Riverside/clean/"
-# LATLONGS = "full2.csv"
-PLACE = "Fowler"
-PATH = "/Volumes/marshallShare/ERACR/Fowler/"
-LATLONGS = "fowler_centroids_ordered2.csv"
-# PLACE = "Gordonvale"
-# PATH = "/Volumes/marshallShare/MGDrivE_Datasets/ThresholdDependent/GeoLocations/Curated/"
-# LATLONGS = "YorkeysKnob_03.csv"
-# PLACE = "Fowler"
-# PATH = "/Volumes/marshallShare/ERACR/Fowler/"
-# LATLONGS = "fowler_centroids.csv"
-latlongs = np.genfromtxt(PATH + LATLONGS, delimiter=',')
-##############################################################################
-# Clustering Algorithms
-##############################################################################
-if CLST_METHOD == 1:
-    clObj = KMeans(
-        n_clusters=clustersNo,
-        random_state=seed
+for clsts in [1, 100, 250, 500, 750, 1000, 1250, 1500, 2000]:
+    ##############################################################################
+    # Parameters Setup
+    ##############################################################################
+    (seed, clustersNo, CLST_METHOD) = (int(time.time()), clsts, 1)
+    (lifeStayProb, adultMortality) = (.72, .09)
+    # PLACE = "BakersfieldRiver"
+    # PATH = "/Volumes/marshallShare/ERACR/Bakersfield/Riverside/clean/"
+    # LATLONGS = "full2.csv"
+    PLACE = "Fowler"
+    PATH = "/Volumes/marshallShare/ERACR/Fowler/Clustered/"
+    LATLONGS = "fowler_centroids_ordered2.csv"
+    # PLACE = "Gordonvale"
+    # PATH = "/Volumes/marshallShare/MGDrivE_Datasets/ThresholdDependent/GeoLocations/Curated/"
+    # LATLONGS = "YorkeysKnob_03.csv"
+    # PLACE = "Fowler"
+    # PATH = "/Volumes/marshallShare/ERACR/Fowler/"
+    # LATLONGS = "fowler_centroids.csv"
+    namePad = str(clustersNo).rjust(5, '0')
+    latlongs = np.genfromtxt(PATH + LATLONGS, delimiter=',')
+    ##############################################################################
+    # Clustering Algorithms
+    ##############################################################################
+    if CLST_METHOD == 1:
+        clObj = KMeans(
+            n_clusters=clustersNo,
+            random_state=seed
+        )
+    elif CLST_METHOD == 2:
+        clObj = AgglomerativeClustering(
+            n_clusters=clustersNo,
+            affinity='euclidean',
+            compute_full_tree='auto'
+        )
+    elif CLST_METHOD == 3:
+        clObj = SpectralClustering(
+            n_clusters=clustersNo,
+            random_state=seed
+        )
+    clustersObj = clObj.fit(latlongs)
+    (clusters, centroids) = (clustersObj.labels_, clustersObj.cluster_centers_)
+    ##############################################################################
+    # Clustering the pointset
+    ##############################################################################
+    cLatlongs = aux.appendClustersToLatlongs(latlongs, clusters)
+    csvPath = PATH + PLACE + "_CLS_" + str(CLST_METHOD) + "_" + namePad + ".csv"
+    aux.exportListToCSV(csvPath, cLatlongs)
+    ##############################################################################
+    # Plotting
+    ##############################################################################
+    plt.figure(figsize=(20, 20))
+    plt.scatter(
+        latlongs[:, 0], latlongs[:, 1], c=clusters,
+        alpha=.25, cmap='hsv', s=150, linewidth=4
     )
-elif CLST_METHOD == 2:
-    clObj = AgglomerativeClustering(
-        n_clusters=clustersNo,
-        affinity='euclidean',
-        compute_full_tree='auto'
+    plt.tight_layout()
+    plt.xlim(min(latlongs[:, 0]) - .0005, max(latlongs[:, 0]) + .0005)
+    plt.ylim(min(latlongs[:, 1]) - .0005, max(latlongs[:, 1]) + .0005)
+    plt.savefig(
+        PATH + PLACE + "_MAP_" + str(CLST_METHOD) + "_" + namePad + ".png",
+        dpi=250
     )
-elif CLST_METHOD == 3:
-    clObj = SpectralClustering(
-        n_clusters=clustersNo,
-        random_state=seed
+    plt.close()
+    ##############################################################################
+    # Export distance matrix
+    ##############################################################################
+    distPath = PATH + PLACE + "_DST_" + str(CLST_METHOD) + "_" + namePad + ".csv"
+    distMat = monet.calculateDistanceMatrix(latlongs, distFun=vn.vincenty) * 1000
+    # np.savetxt(distPath, distMat.astype(int), fmt='%i', delimiter=',')
+    # heat = sns.heatmap(distMat, annot=False)
+    # heat.get_figure().savefig(PATH + PLACE + "DIST.png", dpi=500)
+    ##############################################################################
+    # Export migration matrix
+    ##############################################################################
+    migrPath = PATH + PLACE + "_MIG_" + str(CLST_METHOD) + "_" + namePad + ".csv"
+    zeroInflation = pow(lifeStayProb, adultMortality)
+    migrMat = monet.zeroInflatedExponentialMigrationKernel(
+        distMat,
+        params=monet.AEDES_EXP_PARAMS,
+        zeroInflation=zeroInflation
     )
-clustersObj = clObj.fit(latlongs)
-(clusters, centroids) = (clustersObj.labels_, clustersObj.cluster_centers_)
-##############################################################################
-# Clustering the pointset
-##############################################################################
-cLatlongs = aux.appendClustersToLatlongs(latlongs, clusters)
-csvPath = PATH + PLACE + "CLS" + str(CLST_METHOD) + ".csv"
-# aux.exportListToCSV(csvPath, cLatlongs)
-##############################################################################
-# Plotting
-##############################################################################
-# plt.figure(figsize=(20, 20))
-# plt.scatter(
-#     latlongs[:, 0], latlongs[:, 1], c=clusters,
-#     alpha=.25, cmap='hsv', s=150, linewidth=4
-# )
-# plt.tight_layout()
-# plt.xlim(min(latlongs[:, 0]) - .0005, max(latlongs[:, 0]) + .0005)
-# plt.ylim(min(latlongs[:, 1]) - .0005, max(latlongs[:, 1]) + .0005)
-# plt.savefig(
-#     PATH + PLACE + str(CLST_METHOD) + ".png",
-#     dpi=250
-# )
-# plt.close()
-##############################################################################
-# Export distance matrix
-##############################################################################
-distPath = PATH + PLACE + "DIST.csv"
-distMat = monet.calculateDistanceMatrix(latlongs, distFun=vn.vincenty) * 1000
-# np.savetxt(distPath, distMat.astype(int), fmt='%i', delimiter=',')
-# heat = sns.heatmap(distMat, annot=False)
-# heat.get_figure().savefig(PATH + PLACE + "DIST.png", dpi=500)
-##############################################################################
-# Export migration matrix
-##############################################################################
-migrPath = PATH + PLACE + "MIGR.csv"
-zeroInflation = pow(lifeStayProb, adultMortality)
-migrMat = monet.zeroInflatedExponentialMigrationKernel(
-    distMat,
-    params=monet.AEDES_EXP_PARAMS,
-    zeroInflation=zeroInflation
-)
-monet.testMarkovMatrix(migrMat)
-# np.savetxt(migrPath, migrMat, delimiter=',')
-# heat = sns.heatmap(migrMat, annot=False)
-# heat.get_figure().savefig(PATH + "MIGR.png", dpi=500)
-##############################################################################
-# Export aggregated migration matrix
-##############################################################################
-aggrPath = PATH + PLACE + "AGGR.csv"
-aggrCPath = PATH + PLACE + "AGGR_C.csv"
-aggrMat = monet.aggregateLandscape(migrMat, clusters)
-monet.testMarkovMatrix(aggrMat)
-np.savetxt(aggrPath, aggrMat, delimiter=',')
-np.savetxt(aggrCPath, centroids, delimiter=',')
+    monet.testMarkovMatrix(migrMat)
+    # np.savetxt(migrPath, migrMat, delimiter=',')
+    # heat = sns.heatmap(migrMat, annot=False)
+    # heat.get_figure().savefig(PATH + "MIGR.png", dpi=500)
+    ##############################################################################
+    # Export aggregated migration matrix
+    ##############################################################################
+    aggrPath = PATH + PLACE + "_AGG_" + str(CLST_METHOD) + "_" + namePad + ".csv"
+    aggrCPath = PATH + PLACE + "_AGC_" + namePad + ".csv"
+    aggrMat = monet.aggregateLandscape(migrMat, clusters)
+    monet.testMarkovMatrix(aggrMat)
+    np.savetxt(aggrPath, aggrMat, delimiter=',')
+    np.savetxt(aggrCPath, centroids, delimiter=',')
