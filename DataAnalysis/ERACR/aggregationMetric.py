89515b970dc2c51cb4d045e96e69ab89b9af50f8
chipdelmal <chipdelmal@gmail.com>
2019-07-25 09:50:08 -0700

    Baseline level is imported correctly

--- DataAnalysis/ERACR/aggregationMetric.py
+++ DataAnalysis/ERACR/aggregationMetric.py
@@ -9,9 +9,25 @@ import matplotlib.pyplot as plt
 import aggregationMetricAux as aux
 
 
+# ############################################################################
+# User-defined experiment input
+# ############################################################################
 truthExperiment = "Fowler_AGG_1_01500"
 pathRoot = "/Volumes/marshallShare/ERACR/Fowler/Experiment/"
-
-truthExpPath = pathRoot + truthExperiment + "/"
+# ############################################################################
+# Setting up the experiments paths
+# ############################################################################
+truthExpPath = pathRoot + truthExperiment + "/ANALYZED/*"
 pathSet = pathRoot + "Fowler_AGG_1*/"
 foldersList = sorted(glob.glob(pathSet + "*ANALYZED"))
+
+# ############################################################################
+# Calculating the baseline level (unaggregated)
+# ############################################################################
+filenames = monet.readExperimentFilenames(glob.glob(truthExpPath)[0] + "/")
+landscapeSumData = monet.sumLandscapePopulationsFromFiles(
+    filenames, male=True, female=False, dataType=float
+)
+
+
+landscapeSumData["population"]
