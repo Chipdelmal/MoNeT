8e346eb7b55464972db948fabde5cce555f3e5ab
chipdelmal <chipdelmal@gmail.com>
2019-07-25 13:41:42 -0700

    RMSE calculated

--- DataAnalysis/ERACR/aggregationMetric.py
+++ DataAnalysis/ERACR/aggregationMetric.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
 import glob
 import numpy as np
 import MoNeT_MGDrivE as monet
@@ -9,25 +8,60 @@ import matplotlib.pyplot as plt
 import aggregationMetricAux as aux
 
 
-# ############################################################################
-# User-defined experiment input
-# ############################################################################
+# #############################################################################
+# User-defined experiment input
+# #############################################################################
 truthExperiment = "Fowler_AGG_1_01500"
 pathRoot = "/Volumes/marshallShare/ERACR/Fowler/Experiment/"
-# ############################################################################
-# Setting up the experiments paths
-# ############################################################################
-truthExpPath = pathRoot + truthExperiment + "/ANALYZED/*"
 pathSet = pathRoot + "Fowler_AGG_1*/"
+# #############################################################################
+# Setting up the experiments paths
+# #############################################################################
 foldersList = sorted(glob.glob(pathSet + "*ANALYZED"))
-
-# ############################################################################
+truthExpPath = glob.glob(pathRoot + truthExperiment + "/ANALYZED/*")[0] + "/"
+# #############################################################################
 # Calculating the baseline level (unaggregated)
-# ############################################################################
-filenames = monet.readExperimentFilenames(glob.glob(truthExpPath)[0] + "/")
+# #############################################################################
+filenames = monet.readExperimentFilenames(truthExpPath)
 landscapeSumData = monet.sumLandscapePopulationsFromFiles(
-    filenames, male=True, female=False, dataType=float
+    filenames, male=True, female=True
+)
+basePopDyns = monet.aggregateGenotypesInNode(landscapeSumData, aux.genAggDict)
+# #############################################################################
+# Calculating the error metric
+# #############################################################################
+refExperiment = "Fowler_AGG_1_00750"
+refExpPath = glob.glob(pathRoot + refExperiment + "/ANALYZED/*")[0] + "/"
+filenames = monet.readExperimentFilenames(refExpPath)
+landscapeSumData = monet.sumLandscapePopulationsFromFiles(
+    filenames, male=False, female=True, dataType=float
+)
+refPopDyns = monet.aggregateGenotypesInNode(landscapeSumData, aux.genAggDict)
+# #############################################################################
+# Calculating the error metric
+# #############################################################################
+# Pre-analyses numbers
+initPop = sum(basePopDyns['population'][0])
+simTime = len(basePopDyns['population'])
+# Metrics
+error = (basePopDyns['population'] - refPopDyns['population'])
+rmse = abs(error)
+rmseNrm = rmse / initPop
+rmseAcc = np.cumsum(rmse, axis=0) / simTime
+# #############################################################################
+# Export Plot
+# #############################################################################
+fig = plt.figure()
+for i in range(len(rmse[0])):
+    plt.plot(rmseAcc[:,i], color=aux.colors[i], linewidth=0.5)
+fig.get_axes()[0].set_ylim(0, 1)
+monet.quickSaveFigure(
+    fig,
+    pathRoot + "RMSE_" +
+    refExperiment + ".png",
+    dpi=aux.styleS["dpi"],
+    format="png"
 )
 
 
-landscapeSumData["population"]
+monet.getClusters
--- DataAnalysis/ERACR/aggregationMetricAux.py
+++ DataAnalysis/ERACR/aggregationMetricAux.py
@@ -1,4 +1,5 @@
 import MoNeT_MGDrivE as monet
+import matplotlib.pyplot as plt
 
 colors = [
     "#090446", "#f20060", "#7fff3a",
@@ -11,10 +12,17 @@ styleS = {
     "colors": colors, "format": "png",
     "xRange": [0, 5000], "yRange": [0, 440000]  # 2500]
 }
-aggregationDictionary = monet.autoGenerateGenotypesDictionary(
+genAggDict = monet.autoGenerateGenotypesDictionary(
     ["W", "H", "E", "R", "B"],
     [
         'WW', 'WH', 'WE', 'WR', 'WB', 'HH', 'HE', 'HR',
         'HB', 'EE', 'ER', 'EB', 'RR', 'RB', 'BB'
     ]
 )
+
+def plotTimeError(data, path):
+    fig = plt.figure()
+    for i in range(len(data[0])):
+        plt.plot(data[:,i], color=colors[i], linewidth=0.5)
+    fig.get_axes()[0].set_ylim(0, 1)
+    return fig
