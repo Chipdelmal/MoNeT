d6e1cd554f0ab06fea971c094a113151580cb822
chipdelmal <chipdelmal@gmail.com>
2019-08-02 13:09:30 -0700

    Software paper plots run

--- DataAnalysis/SoftwarePaper/PaperPlots.py
+++ DataAnalysis/SoftwarePaper/PaperPlots.py
@@ -1,15 +1,19 @@
 import MoNeT_MGDrivE as monet
 
-colors = ['#9f00cc', '#ec0b43', '#232ed1', '#ff009d']
+#############################################################################
+colors = ['#9f00cc', '#ec0b43', '#232ed1', '#ff009d', '#000000']
 style = {
-    "width": 1, "alpha": 1, "dpi": 1024, "legend": True,
-    "aspect": .1, "colors": colors
+    "width": .01, "alpha": .05, "dpi": 2*1024, "legend": True,
+    "aspect": 20, "colors": colors
+}
+styleS = {
+    "width": 0, "alpha": .85, "dpi": 2*1024, "legend": True,
+    "aspect": .175, "colors": colors
 }
 cmaps = monet.generateAlphaColorMapFromColorArray(colors)
-
 #############################################################################
-basePath = "/Volumes/marshallShare/MGDrivE_Datasets/SoftwarePaper/"
-experimentString = "ReplacementDeterministic/0001/"
+basePath = "/Users/sanchez.hmsc/Desktop/SoftwarePaper/"
+experimentString = "ReplacementStochastic"
 aggregationDictionary = monet.generateAggregationDictionary(
     ["W", "H", "R", "B"],
     [
@@ -19,22 +23,58 @@ aggregationDictionary = monet.generateAggregationDictionary(
         [3, 6, 8, 9, 9]
     ]
 )
-filenames = monet.readExperimentFilenames(
-    basePath + experimentString,
-    sexFilenameIdentifiers={"male": "ADM", "female": "AF1_Aggregate"}
-)
 #############################################################################
+repsFolders = monet.listDirectoriesWithPathWithinAPath(
+    basePath + experimentString + "/"
+)
+# landscapeReps = monet.loadAndAggregateLandscapeDataRepetitions(
+#     repsFolders, aggregationDictionary,
+#     male=False, female=True
+# )
+# tracesPlot = monet.plotAllTraces(landscapeReps, style)
+# monet.quickSaveFigure(
+#     tracesPlot,
+#     basePath + experimentString + "_Trace.png", format="png"
+# )
+#############################################################################
+filenames = monet.readExperimentFilenames(
+    basePath + experimentString + "_Analyzed/"
+)
 landscapeSumData = monet.sumLandscapePopulationsFromFiles(
-    filenames, male=True, female=True
+    filenames,
+    male=True, female=True
 )
 aggData = monet.aggregateGenotypesInNode(
     landscapeSumData,
     aggregationDictionary
 )
+stack = monet.plotMeanGenotypeStack(aggData, styleS)
+monet.quickSaveFigure(
+    stack,
+    basePath + experimentString + "_Stack.png", format="png"
+)
+#############################################################################
 landscapeData = monet.loadLandscapeData(filenames)
 aggregatedNodesData = monet.aggregateGenotypesInLandscape(
     landscapeData,
     aggregationDictionary
 )
 geneSpatiotemporals = monet.getGenotypeArraysFromLandscape(aggregatedNodesData)
-figA = monet.plotMeanGenotypeStack(aggData, style)
+genes = geneSpatiotemporals["genotypes"]
+plotsArray = monet.plotGenotypeArrayFromLandscape(
+    geneSpatiotemporals,
+    style={"aspect": 12, "cmap": cmaps}
+)
+for i in range(0, len(genes)):
+    geneIndex = i
+    fig = plotsArray["plots"][geneIndex]
+    monet.quickSaveFigure(
+        fig,
+        basePath + experimentString + "_Heat" + genes[i] +  ".png"
+    )
+
+overlay = monet.plotGenotypeOverlayFromLandscape(
+    geneSpatiotemporals,
+    style={"aspect": 10, "cmap": cmaps}
+)
+monet.quickSaveFigure(overlay, basePath + experimentString + "HeatO.png")
