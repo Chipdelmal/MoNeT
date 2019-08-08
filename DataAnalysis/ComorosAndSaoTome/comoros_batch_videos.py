d07fb3c52be602f223d56da75b7428f0071887fd
Victor Ferman <victor.ferman@gmail.com>
2019-08-08 12:08:49 -0700

    fowler/yorkeys and comoros batch export

--- DataAnalysis/ComorosAndSaoTome/comoros_batch_videos.py
+++ DataAnalysis/ComorosAndSaoTome/comoros_batch_videos.py
@@ -9,25 +9,20 @@ colors = ["#090446", "#f20060", "#ff28d4", "#7fff3a", "#c6d8ff", '#6e44ff','#e56
 
 groups = ["W", "H", "R", "B", "E"]
 
-folder = '/Volumes/marshallShare/ERACR/Fowler3/Experiment/'
+folder = '/Volumes/marshallShare/Comoros_STP/Comoros/output/'
 extras = '/Volumes/marshallShare/ERACR/Fowler/Clustered/'
 patchFilePattern = {'males':'/M_*', 'females':'/F_*'}
 imagePattern = '/c_%06d.png'
+bgName  = '/Volumes/marshallShare/Comoros_STP/Comoros/all_sites2.png'
+clusterName = '/Volumes/marshallShare/Comoros_STP/Comoros/all_sites2.csv'
 
 
-for expFolder in sorted(glob.glob(folder+'*_AGG_*')):
+for expFolder in sorted(glob.glob(folder+'*')):
     expBaseName = expFolder.split('/')[-1]
     print(expBaseName)
-    clusteringNum = int(expBaseName.split('_')[-1])
-    if clusteringNum < 400 or clusteringNum >2000:
-        continue
     for expPath in sorted(glob.glob(expFolder+'/ANALYZED/E_*')):
         vname = expPath.replace('ANALYZED','videos')+'_cdots.mp4'
-        bgName  = expBaseName.replace('_AGG_','_VBG_')
-        clusterName = bgName.replace('VBG_','AGCV_')
-        background = extras+bgName+'.png'
-        coordinates = monet.getClusters(extras+clusterName+'.csv')
-
+        coordinates = monet.getClusters(clusterName)
 
         imageLocation = expPath.replace('ANALYZED','images/clustercharts')
         subprocess.Popen(['mkdir', imageLocation])
@@ -38,7 +33,7 @@ for expFolder in sorted(glob.glob(folder+'*_AGG_*')):
         genotypes = monet.getGenotypes(clusters[0]['male'][0])
         aggDict = monet.autoGenerateGenotypesDictionary(groups, genotypes)
         aggList = monet.aggregateClusters(clusters, aggDict)
-        monet.generateClusterGraphs(aggList, coordinates, imageLocation, colors, 0.001, 512)
-        video = monet.generateVideo(vname,background, imageLocation, imagePattern)
+        monet.generateClusterGraphs(aggList, coordinates, imageLocation, colors, 0.1, 512)
+        video = monet.generateVideo(vname,bgname, imageLocation, imagePattern)
 
 video.wait()
