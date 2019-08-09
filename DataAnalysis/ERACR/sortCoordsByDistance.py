c81ebeed88d9946bbe650ccd9280206650f004c1
chipdelmal <chipdelmal@gmail.com>
2019-08-09 14:12:29 -0700

    Distances to POI script is live (requires MoNeT_MGDrivE update 0.5.5.1).

--- DataAnalysis/ERACR/sortCoordsByDistance.py
+++ DataAnalysis/ERACR/sortCoordsByDistance.py
@@ -1,7 +1,6 @@
 import numpy as np
 import MoNeT_MGDrivE as monet
 import vincenty as vn
-import matplotlib.pyplot as plt
 
 PLACE = "Fowler"
 PATH = "/Volumes/marshallShare/ERACR/Fowler4/Clustered/"
@@ -9,40 +8,11 @@ LATLONGS = "fowler_centroids_ordered2.csv"
 
 POI = (36.617, -119.690)
 latlongs = np.genfromtxt(PATH + LATLONGS, delimiter=',')
+monet.sortByDistanceToPOI(POI, latlongs, distFun=vn.vincenty)
 
-distances = [vn.vincenty(POI, (row[1], row[0])) for row in latlongs]
-inds = np.asarray(distances).argsort()
-sortedByDistance = latlongs[inds]
-
-np.savetxt(
-    PATH + "SORTED_TEST_HMSC.csv",
-    sortedByDistance,
-    fmt='%.10f',
-    delimiter=","
-)
-
-distArray = np.asarray([distances])
-latlongDist = np.concatenate((latlongs, distArray.T), axis=1)
-latlongDistSort = latlongDist[inds]
 np.savetxt(
     PATH + "SORTED_TEST_HMSC.csv",
     latlongDistSort,
     fmt='%.10f',
     delimiter=","
-)
-
-
-plt.figure(figsize=(20, 20))
-plt.scatter(
-    latlongs[:, 0], latlongs[:, 1], c=distances,
-    alpha=.25, cmap='hsv', s=150, linewidth=4
-)
-plt.tight_layout()
-plt.xlim(min(latlongs[:, 0]) - .0005, max(latlongs[:, 0]) + .0005)
-plt.ylim(min(latlongs[:, 1]) - .0005, max(latlongs[:, 1]) + .0005)
-
-
-plt.savefig(
-    PATH + PLACE + "_MAP_" + str(CLST_METHOD) + "_" + namePad + ".png",
-    dpi=250
 )
