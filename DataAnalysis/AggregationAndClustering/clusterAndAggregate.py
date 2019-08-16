e448cc8f3e52720ac94b2e4e08d7a0aa4dfac9e7
chipdelmal <chipdelmal@gmail.com>
2019-08-16 14:17:16 -0700

    Loop over number of clusters

--- DataAnalysis/AggregationAndClustering/clusterAndAggregate.py
+++ DataAnalysis/AggregationAndClustering/clusterAndAggregate.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import csv
 import aux
 import time
 import numpy as np
@@ -18,7 +17,10 @@ from sklearn.cluster import KMeans
     "LandSorted/Yorkeys01_M.csv",
     "LandAggregated/"
 )
-(CLUSTERS_NO, REPS, JOBS) = (500, 25, 1)
+(CLUSTERS_NO, REPS, JOBS) = (
+    [1, 25, 50, 250, 500, 750, 1000, 1250, 1500, 2000, 2195],
+    10, 1
+)
 
 ###############################################################################
 # Read latlongs and migration matrix
@@ -33,29 +35,33 @@ coordsList = aux.readCoordsCSV(PATH + LATLONGS)
 ###############################################################################
 # Cluster and Export
 ###############################################################################
-for rep in range(REPS):
-    ###########################################################################
-    # Cluster and Aggregate
-    ###########################################################################
-    clObj = KMeans(
-        n_clusters=CLUSTERS_NO,
-        random_state= int(time.time()),
-        n_jobs=JOBS
-    )
-    clustersObj = clObj.fit(latlongs)
-    (clusters, centroids) = (clustersObj.labels_, clustersObj.cluster_centers_)
-    aggrMat = monet.aggregateLandscape(migrMat, clusters)
-    ###########################################################################
-    # Export
-    ###########################################################################
-    # Define filenames
-    placeName = LATLONGS.split("/")[1].split(".")[0].split("_")[0]
-    outRepPath = PATH + OUT + "C" + str(CLUSTERS_NO).rjust(6, '0')
-    filenames = outRepPath + "/" + placeName + "_" + str(rep).rjust(4, '0')
-    # Export files
+for clst in CLUSTERS_NO:
+    outRepPath = PATH + OUT + "C" + str(clst).rjust(6, '0')
     aux.createFolder(outRepPath)
-    np.savetxt(filenames + "_A.csv", aggrMat, delimiter=',')
-    aux.writeLatLongsClustersWithID(
-        coordsList, clusters, centroids,
-        filenames + "_I.csv"
-    )
+    for rep in range(REPS):
+        #######################################################################
+        # Cluster and Aggregate
+        #######################################################################
+        clObj = KMeans(
+            n_clusters=clst,
+            random_state=int(time.time()),
+            n_jobs=JOBS
+        )
+        clustersObj = clObj.fit(latlongs)
+        (clusters, centroids) = (
+            clustersObj.labels_,
+            clustersObj.cluster_centers_
+        )
+        aggrMat = monet.aggregateLandscape(migrMat, clusters)
+        #######################################################################
+        # Export
+        #######################################################################
+        # Define filenames
+        placeName = LATLONGS.split("/")[1].split(".")[0].split("_")[0]
+        filenames = outRepPath + "/" + placeName + "_" + str(rep).rjust(4, '0')
+        # Export files
+        np.savetxt(filenames + "_A.csv", aggrMat, delimiter=',')
+        aux.writeLatLongsClustersWithID(
+            coordsList, clusters, centroids,
+            filenames + "_I.csv"
+        )
