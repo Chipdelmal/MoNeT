7da67893b5021864b26c313ced70adcd1c673482
Victor Ferman <victor.ferman@gmail.com>
2019-08-19 17:01:45 -0700

    New script to generate the backgrounds of the new landscape files

---
+++ Dev/VictorFerman/eracer_new_landscapes.py
@@ -0,0 +1,65 @@
+import os
+import glob
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.mplot3d import Axes3D
+
+pathRoot = "/Volumes/marshallShare/ERACR/Yorkeys_DEMO/LandAggregated"
+
+for clusterFile in glob.glob(pathRoot+'/*/Yorkeys*I.csv'):
+    minGroup = 99999999
+    maxGroup = 0
+    minLat = 0
+    minlong  = 0
+    maxLat = 0
+    maxLong = 0
+    lats = []
+    longs = []
+    clusters = []
+    centroidSet = {}
+    expName = (clusterFile.split("/")[-1]).replace('_I.csv','_VBG_.png')
+    clusterData = open(clusterFile,'r')
+    next(clusterData)
+    for line in clusterData:
+        tokens = line.split(',')
+        lat = float(tokens[1])
+        long = float(tokens[2])
+        cluster = int(tokens[3])
+        lats.append(lat)
+        longs.append(long)
+        clusters.append(cluster)
+
+    minLat = min(lats)
+    minLong  = min(longs)
+    minCluster = min(clusters)
+    maxLat = max(lats)
+    maxLong = max(longs)
+    maxCluster = max(clusters)
+
+    fig = plt.figure(figsize=(5, 5))
+    ax=fig.add_subplot(111, label="1")
+    m = Basemap(projection='merc',llcrnrlat=minLat-0.001,urcrnrlat=maxLat+0.001,llcrnrlon=minLong-0.001,urcrnrlon=maxLong+0.001,lat_ts=20,resolution='i', ax=ax)
+    m.drawcounties(linewidth=0.3)
+    m.drawcoastlines(linewidth=0.3)
+    m.drawcountries(linewidth=0.3)
+    m.scatter(longs, lats, latlon=True, alpha=.2, marker='o', s=3, cmap=plt.get_cmap('rainbow'), c=clusters, vmin=minCluster, vmax=maxCluster)
+    # tLon,tLat = m(longs,lats)
+    # for i in range(len(longs)):
+    #     plt.text(tLon[i],tLat[i], str(i), fontsize=1)
+    ax.tick_params(
+    axis='both',          # changes apply to the both
+    which='both',      # both major and minor ticks are affected
+    bottom=False,      # ticks along the bottom edge are off
+    top=False,         # ticks along the top edge are off
+    left=False,
+    right=False,
+    labelbottom=False, # labels along the bottom edge are off
+    labelleft=False)
+    plt.savefig(pathRoot+'/'+expName, dpi=512,
+                facecolor='w', edgecolor='w', orientation='portrait',
+                papertype=None, format="png", transparent=False,
+                bbox_inches='tight', pad_inches=0.05, frameon=None)
+    plt.close(fig)
+    plt.close('all')
