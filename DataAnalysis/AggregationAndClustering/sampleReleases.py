7efa33b910869a4f9060d31bfb37728e18bfd376
Victor Ferman <victor.ferman@gmail.com>
2019-08-23 14:06:10 -0700

    start of the release sample

--- DataAnalysis/AggregationAndClustering/sampleReleases.py
+++ DataAnalysis/AggregationAndClustering/sampleReleases.py
@@ -2,10 +2,45 @@
 # -*- coding: utf-8 -*-
 
 from random import sample
+import numpy as np
+import glob
 
 coverage = .5
-nodesIDPool = range(0, 1000)
+
+clusterDirs = "/Volumes/marshallShare/ERACR/Yorkeys_DEMO/LandAggregated/C*"
+nodeFileName = "/Volumes/marshallShare/ERACR/Yorkeys_DEMO/LandAggregated/yorkeys_ids.csv"
+nodeFile = open(nodeFileName, 'r')
+next(nodeFile)
+
+nodesIDPool = []
+for line in nodeFile:
+    nodesIDPool.append(int(line.split(',')[0]))
+nodeFile.close()
 
 
 sampledNumber = round(len(nodesIDPool) * coverage)
-sample(nodesIDPool, sampledNumber)
+sampledNodes = sample(nodesIDPool, k=sampledNumber)
+
+for clusterDir in sorted(glob.glob(clusterDirs)):
+
+    clusterNum = int(clusterDir.split('/')[-1][1:])
+    clusterFileNames = sorted(glob.glob(clusterDir+'/*_I.csv'))
+
+    if clusterNum == 1:
+        clusterFileNames = clusterFileNames[:1]
+
+    for clusterFileName in clusterFileNames:
+
+        clusterFile = open(clusterFileName, 'r')
+        header = next(clusterFile)
+        coordList = clusterFile.readlines()
+
+        resultList = [coordList[x] for x in sampledNodes]
+        resultFileName = clusterFileName.replace('_I.', '_S.')
+        resultFile = open(resultFileName,'w')
+        resultFile.write(header)
+        for line in resultList:
+            resultFile.write(line)
+
+        clusterFile.close()
+        resultFile.close()
