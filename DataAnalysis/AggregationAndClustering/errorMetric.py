281541fb635ad6a8abc93a6053c373b6328a0c24
chipdelmal <chipdelmal@gmail.com>
2019-08-26 11:09:43 -0700

    Summary stats are working. Need to generalize the code.

--- DataAnalysis/AggregationAndClustering/errorMetric.py
+++ DataAnalysis/AggregationAndClustering/errorMetric.py
@@ -7,36 +7,29 @@ import statistics as stat
 import MoNeT_MGDrivE as monet
 import matplotlib.pyplot as plt
 
+funcsList = (stat.stdev, stat.mean, stat.median)
+funcsID = ["SSD", "SMN", "SMD"]
 
 pathRoot = "/Volumes/marshallShare/ERACR/Yorkeys4/Experiment4/"
 pathSet = pathRoot + "Yorkeys_AGG_*/"  # + "eRACR29"
-foldersList = glob.glob(pathSet + "*GARBAGE")
-sortedFolders = sorted(foldersList)
-sortedFolders
+foldersList = sorted(glob.glob(pathSet + "*GARBAGE"))
 
-
-expPath = sortedFolders[2]
+expPath = foldersList[3]
 gPath = monet.listDirectoriesWithPathWithinAPath(expPath + "/")
 ePath = monet.listDirectoriesWithPathWithinAPath(gPath[0] + "/")
 aggregationDictionary = monet.autoGenerateGenotypesDictionary(
-        ["W", "H", "E", "R", "B"],
-        [
-            'WW', 'WH', 'WE', 'WR', 'WB', 'HH', 'HE', 'HR',
-            'HB', 'EE', 'ER', 'EB', 'RR', 'RB', 'BB'
-        ]
-    )
+    ["W", "H", "E", "R", "B"],
+    [
+        'WW', 'WH', 'WE', 'WR', 'WB', 'HH', 'HE', 'HR',
+        'HB', 'EE', 'ER', 'EB', 'RR', 'RB', 'BB'
+    ]
+)
 reps = monet.loadAndAggregateLandscapeDataRepetitions(
-    ePath, aggregationDictionary,
-    male=True, female=False
+    ePath, aggregationDictionary, male=True, female=False
 )
 repsN = monet.sumAggregatedLandscapeDataRepetitions(reps)
-lands = repsN['landscapes']
-
-(geneNumber, totalTime) = (
-    len(repsN['genotypes']),
-    len(repsN['landscapes'][0][0])
-)
-
+(genes, lands) = (repsN['genotypes'], repsN['landscapes'])
+(geneNumber, totalTime) = (len(genes), len(lands[0][0]))
 
 
 (hSD, hMn, hMd) = [np.zeros(shape=(totalTime, geneNumber)) for i in range(3)]
@@ -45,3 +38,17 @@ for time in range(totalTime):
         slice = [lanRep[0][time, gene] for lanRep in lands]
         (sSD, sMn, sMd) = (stat.stdev(slice), stat.mean(slice), stat.median(slice))
         (hSD[time][gene], hMn[time][gene], hMd[time][gene]) = (sSD, sMn, sMd)
+
+
+np.savetxt(
+    pathRoot + "SSD_" +  expPath.split('/')[-2] + ".csv",
+    hSD, fmt='%f', delimiter=',', newline='\n'
+)
+np.savetxt(
+    pathRoot + "SMN_" +  expPath.split('/')[-2] + ".csv",
+    hMn, fmt='%f', delimiter=',', newline='\n'
+)
+np.savetxt(
+    pathRoot + "SMD_" +  expPath.split('/')[-2] + ".csv",
+    hMd, fmt='%f', delimiter=',', newline='\n'
+)
