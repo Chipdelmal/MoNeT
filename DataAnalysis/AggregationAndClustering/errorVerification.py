ad66a19e5c785b0d11ce0910a2fdf06e3eb7fc8a
chipdelmal <chipdelmal@gmail.com>
2019-09-03 15:20:31 -0700

    Error plots are done!

--- DataAnalysis/AggregationAndClustering/errorVerification.py
+++ DataAnalysis/AggregationAndClustering/errorVerification.py
@@ -15,51 +15,79 @@ pathOut = pathRoot + "images/errors/"
     sorted(glob.glob(pathRoot + "SMN*")),
     sorted(glob.glob(pathRoot + funcsID[selStat] + "*"))
 )
-
+filesNum = len(filesListMean)
 # Style #######################################################################
 colors = ["#090446", "#f20060", "#59ff00", "#ff28d4", "#6898ff", "#c6d8ff"]
 cmaps = monet.generateAlphaColorMapFromColorArray(colors)
 styleS = {
     "width": 0, "alpha": .85, "dpi": 2*512, "legend": False,
     "aspect": .05, "colors": colors, "format": "png",
-    "xRange": [0, 9124], "yRange": [0, 50000]  # 590000]  # 2500]
+    "xRange": [0, 9124],
+    "yRange": [0, 50000]  # 590000]  # 2500]
 }
 
 
-fileIx = 2
-(fileM, fileS) = (filesListMean[fileIx], filesListStat[fileIx])
-nameID = fileM.split("/")[-1].split(".")[0]
-# Read Files ##################################################################
-(readDataM, readDataS) = (
-        np.genfromtxt(fileM, delimiter=','),
-        np.genfromtxt(fileS, delimiter=',')
+for fileIx in range(len(filesListMean)):
+    (fileM, fileS) = (filesListMean[fileIx], filesListStat[fileIx])
+    nameID = fileM.split("/")[-1].split(".")[0]
+    # Read Files ##############################################################
+    (readDataM, readDataS) = (
+            np.genfromtxt(fileM, delimiter=','),
+            np.genfromtxt(fileS, delimiter=',')
+        )
+    # Stack ###################################################################
+    # data = {"genotypes": ["W", "H", "E", "R", "B"], "population": readDataM}
+    # fig = monet.plotMeanGenotypeStack(data, styleS)
+    # monet.quickSaveFigure(
+    #     fig, pathOut + nameID + "_S.png",
+    #     format="png", dpi=styleS["dpi"]
+    # )
+    # Deviation ###############################################################
+    timeRange = range(len(readData[0:,0]))
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for i in range(len(data["genotypes"])):
+        plt.plot(
+            timeRange, readDataM[0:, i],
+            color=colors[i], alpha=styleS["alpha"]
+        )
+        max = readDataM[0:, i] + readDataS[0:, i]
+        min = readDataM[0:, i] - readDataS[0:, i]
+        ax.fill_between(timeRange, min, max, color=colors[i], alpha=0.1)
+    ax.set_aspect(aspect=styleS["aspect"])
+    ax.set_xlim(styleS["xRange"])
+    ax.set_ylim(styleS["yRange"])
+    monet.quickSaveFigure(
+        fig, pathOut + nameID + "_T.png",
+        format="png", dpi=styleS["dpi"]
     )
-# Stack #######################################################################
-# data = {"genotypes": ["W", "H", "E", "R", "B"], "population": readDataM}
-# fig = monet.plotMeanGenotypeStack(data, styleS)
-# monet.quickSaveFigure(
-#     fig, pathOut + nameID + "_S.png",
-#     format="png", dpi=styleS["dpi"]
-# )
-# Deviation ###################################################################
+
+
+# Full traces #################################################################
 timeRange = range(len(readData[0:,0]))
 fig = plt.figure()
 ax = fig.add_subplot(111)
-for i in range(len(data["genotypes"])):
-    plt.plot(
-        timeRange, readDataM[0:, i],
-        color=colors[i], alpha=styleS["alpha"]
-    )
-    plt.plot(
-        timeRange, readDataM[0:, i] + readDataS[0:, i],
-        color=colors[i], linewidth=.1, alpha=styleS["alpha"]
-    )
-    plt.plot(
-        timeRange, readDataM[0:, i] - readDataS[0:, i],
-        color=colors[i], linewidth=.1, alpha=styleS["alpha"]
-    )
-ax.set_aspect(aspect=styleS["aspect"])
+for fileIx in range(filesNum):
+    (fileM, fileS) = (filesListMean[fileIx], filesListStat[fileIx])
+    nameID = fileM.split("/")[-1].split(".")[0]
+    # Read Files ##############################################################
+    (readDataM, readDataS) = (
+            np.genfromtxt(fileM, delimiter=','),
+            np.genfromtxt(fileS, delimiter=',')
+        )
+    for i in range(len(data["genotypes"])):
+        plt.plot(
+            timeRange, readDataM[0:, i],
+            color=colors[i], alpha=1/(filesNum - 1) * fileIx,
+            linewidth=(.025 * fileIx)
+        )
+        # max = readDataM[0:, i] + readDataS[0:, i]
+        # min = readDataM[0:, i] - readDataS[0:, i]
+        # ax.fill_between(timeRange, min, max, color=colors[i], alpha=0.1)
+    ax.set_aspect(aspect=styleS["aspect"])
+    ax.set_xlim(styleS["xRange"])
+    ax.set_ylim(styleS["yRange"])
 monet.quickSaveFigure(
-    fig, pathOut + nameID + "_T.png",
+    fig, pathOut + "SMN_Yorkeys_AGG_1_Full.png",
     format="png", dpi=styleS["dpi"]
 )
