a113672a06847cb2b99c355d3d0a3433ab31ba08
chipdelmal <chipdelmal@gmail.com>
2019-10-01 15:57:51 -0700

    Added comments to code for readability.

--- Markov/network.py
+++ Markov/network.py
@@ -5,6 +5,11 @@ import numpy as np
 
 
 def normalizeMskMgrMat(migrMat, clandMskMat):
+    '''
+    Takes the migration matrix and the masking matrix and returns the
+        normalized masked migration matrix according to distance and
+        point-type.
+    '''
     normalized = np.empty((len(clandMskMat), len(clandMskMat)))
     mskdMigrMat = migrMat * clandMskMat
     for (i, row) in enumerate(mskdMigrMat):
