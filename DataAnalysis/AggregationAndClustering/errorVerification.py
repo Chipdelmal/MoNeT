a33fb79a753d9e8c393d893a9b70a63683fb5a02
chipdelmal <chipdelmal@gmail.com>
2019-10-24 14:36:17 -0700

    Various updates for YK and GV paper

--- DataAnalysis/AggregationAndClustering/errorVerification.py
+++ DataAnalysis/AggregationAndClustering/errorVerification.py
@@ -6,6 +6,12 @@ import numpy as np
 import MoNeT_MGDrivE as monet
 import matplotlib.pyplot as plt
 
+# Functions ###################################################################
+def scaleAspect(aspect, style):
+    xDiff = (style['xRange'][1] - style['xRange'][0])
+    yDiff = (style['yRange'][1] - style['yRange'][0])
+    return aspect * (xDiff / yDiff)
+# Filepaths ###################################################################
 
 (selStat, funcsID) = (0, ["SSD", "SMN", "SMD"])
 pathRoot = "/Volumes/marshallShare/ERACR/Yorkeys_MINI/Experiemnts/"
@@ -22,10 +28,12 @@ cmaps = monet.generateAlphaColorMapFromColorArray(colors)
 styleS = {
     "width": 0, "alpha": .85, "dpi": 2*512, "legend": False,
     "aspect": .03, "colors": colors, "format": "png",
-    "xRange": [0, 9124],
+    "xRange": [0, 2500],#9124],
     "yRange": [0, 50000]  # 590000]  # 2500]
 }
+styleS['aspect'] = scaleAspect(.2, styleS)
 
+# Main ########################################################################
 for fileIx in range(len(filesListMean)):
     (fileM, fileS) = (filesListMean[fileIx], filesListStat[fileIx])
     nameID = str(filesListMean[fileIx].split('/')[-1].split('.')[-2])
@@ -69,33 +77,33 @@ for fileIx in range(len(filesListMean)):
 
 
 # Full traces #################################################################
-# timeRange = range(len(readDataM[0:,0]))
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# for fileIx in range(filesNum):
-#     (fileM, fileS) = (filesListMean[fileIx], filesListStat[fileIx])
-#     nameID = fileM.split("/")[-1].split(".")[0]
-#     # Read Files ##############################################################
-#     (readDataM, readDataS) = (
-#             np.genfromtxt(fileM, delimiter=','),
-#             np.genfromtxt(fileS, delimiter=',')
-#         )
-#     for i in range(len(data["genotypes"])):
-#         plt.plot(
-#             timeRange, readDataM[0:, i],
-#             color=colors[i], alpha=1/(filesNum - 1) * fileIx,
-#             linewidth=(.025 * fileIx)
-#         )
-#         # max = readDataM[0:, i] + readDataS[0:, i]
-#         # min = readDataM[0:, i] - readDataS[0:, i]
-#         # ax.fill_between(timeRange, min, max, color=colors[i], alpha=0.1)
-#     ax.set_aspect(aspect=styleS["aspect"])
-#     ax.set_xlim(styleS["xRange"])
-#     ax.set_ylim(styleS["yRange"])
-# monet.quickSaveFigure(
-#     fig, pathOut + "SMN_Yorkeys_AGG_1_Full.png",
-#     format="png", dpi=styleS["dpi"]
-# )
+timeRange = range(len(readDataM[0:,0]))
+fig = plt.figure()
+ax = fig.add_subplot(111)
+for fileIx in range(filesNum):
+    (fileM, fileS) = (filesListMean[fileIx], filesListStat[fileIx])
+    nameID = fileM.split("/")[-1].split(".")[0]
+    # Read Files ##############################################################
+    (readDataM, readDataS) = (
+            np.genfromtxt(fileM, delimiter=','),
+            np.genfromtxt(fileS, delimiter=',')
+        )
+    for i in range(len(data["genotypes"])):
+        plt.plot(
+            timeRange, readDataM[0:, i],
+            color=colors[i], alpha=.75/(filesNum - 2) * fileIx,
+            linewidth=(.04 * fileIx)
+        )
+        # max = readDataM[0:, i] + readDataS[0:, i]
+        # min = readDataM[0:, i] - readDataS[0:, i]
+        # ax.fill_between(timeRange, min, max, color=colors[i], alpha=0.1)
+    ax.set_aspect(aspect=styleS["aspect"])
+    ax.set_xlim(styleS["xRange"])
+    ax.set_ylim(styleS["yRange"])
+monet.quickSaveFigure(
+    fig, pathOut + "SMN_Yorkeys_AGG_1_Full.png",
+    format="png", dpi=styleS["dpi"]
+)
 
 # ffmpeg -i %05d.jpg output.mp4
 # ffmpeg -ss 2.6 -t 1.3 -i output.mp4 -vf fps=15,scale=320:-1:flags=lanczos,palettegen palette.png
