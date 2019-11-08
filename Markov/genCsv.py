b195f9b00b79c672d014fc8afc726addb6d96d86
Yunwen Ji <jiyunwen@berkeley.edu>
2019-11-08 11:08:27 -0800

    Save the plot and types of landscape

--- Markov/genCsv.py
+++ Markov/genCsv.py
@@ -15,13 +15,23 @@ def genCSV(reps, n, heterogenity, landscapeProb):
     # create number of reps simulations
     for i in range(1, reps+1):
         for (j, zeroInflation, prob) in params:
-            singleMatrix = gS.genSingle(j, zeroInflation, prob)
+            singleMatrix, pointClasses, landscapePlot = gS.genSingle(j, zeroInflation, prob)
             # produce the name composed of probability
             s = ''
             probInNames = s.join([str(int(p*100)) for p in prob])
+            baseName = delimiter.join([str(j), probInNames, str(round(heterogenity[0]*10)), str(i)])
 
             # save the simulated data as csv files
-            filename = delimiter.join(['H', str(j), probInNames, str(round(heterogenity[0]*10)), str(i)])
+            filename = delimiter.join(['H', baseName])
+            filenameTypes = delimiter.join(['T', baseName])
+
+
             np.savetxt("kernels" + os.sep + filename + ".csv", singleMatrix, delimiter=",")
+            np.savetxt("kernels" + os.sep + filenameTypes + ".csv", singleMatrix, delimiter=",")
+
+            filenameLand = "kernels" + os.sep+'L_' + baseName + '.png'
+            fig = landscapePlot.get_figure()
+            fig.savefig(filenameLand)
+
 
 genCSV(reps, n, heterogenity, [[0.7, 0.2, 0.1]])
