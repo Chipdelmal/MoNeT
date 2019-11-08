b5c8949d50fdac10786a5e8f9e83c59034775e92
chipdelmal <chipdelmal@gmail.com>
2019-11-08 10:58:27 -0800

    Changing batch analyses to make CSV factorials consistent on drives.

--- DataAnalysis/ThresholdDependent/driveSelector.py
+++ DataAnalysis/ThresholdDependent/driveSelector.py
@@ -1,16 +1,21 @@
 import glob
 import numpy as np
+import MoNeT_MGDrivE as monet
 
 
 def driveGenesSelector(driveID):
-    if (driveID == 'T' or driveID == 'U'):
-        # Translocations and UDMel
+    if (driveID == 'T'):
+        # Translocations
         wildsList = [1, 1, 2, 2, 3, 3, 4, 5, 6, 1, 1, 2, 4, 4, 5, 7, 7, 8]
         homingList = [4, 5, 6, 7, 7, 8, 8, 9, 9, 2, 3, 3, 5, 6, 6, 8, 9, 9]
-    else:
+    elif (driveID == 'U'):
+        # UDMel
+        wildsList = [4, 5, 6, 7, 7, 8, 8, 9, 9, 2, 3, 3, 5, 6, 6, 8, 9, 9]
+        homingList = [1, 1, 2, 2, 3, 3, 4, 5, 6, 1, 1, 2, 4, 4, 5, 7, 7, 8]
+    elif (driveID == 'W'):
         # Wolbachia
-        wildsList = [1]
-        homingList = [0]
+        wildsList = [0]
+        homingList = [1]
     return (wildsList, homingList)
 
 
@@ -39,3 +44,57 @@ def compileFactorialCSVFromFiles(pathFiles, outPath):
         fileData = np.genfromtxt(file, skip_header=1, delimiter=",")
         np.savetxt(f, fileData, fmt='%2.5f', delimiter=",")
     f.close()
+
+
+def loadFolderAndWriteFactorialCSVInclude(
+    experimentString,
+    path,
+    aggregationDictionary,
+    ratiosDictionary,
+    male=True,
+    female=True,
+    dataType=float,
+    includePattern='*'
+):
+    """
+    Description:
+        * Wrapper function to perform the whole factorial parsing analysis on a
+            folder and write the resulting CSV to drive.
+    In:
+        * experimentString:
+        * path: Directory where the experiment is stored.
+        * aggregationDictionary: Dictionary containing the keys to aggregate
+            the genotypes (created with "generateAggregationDictionary")
+        * ratiosDictionary: "numerator", and "denominator" lists dictionary
+            containing the columns to use in each section of the ratio.
+    Out:
+        * NA
+    Notes:
+        * NA
+    """
+    # Read filenames
+    filenames = monet.readExperimentFilenames(path+experimentString)
+    # Filter out non-needed files
+    (mFiles, fFiles) = (filenames['male'], filenames['female'])
+    filenames['male'] = [i for i in mFiles if includePattern in i]
+    filenames['female'] = [i for i in mFiles if includePattern in i]
+    # Aggregate data
+    landscapeSumData = monet.sumLandscapePopulationsFromFiles(
+        filenames,
+        male=male, female=female,
+        dataType=dataType
+    )
+    aggregateData = monet.aggregateGenotypesInNode(
+        landscapeSumData,
+        aggregationDictionary
+    )
+    split = monet.splitExperimentString(experimentString)
+    monet.writeFactorialAnalysisCSV(
+        split["releasesNumber"],
+        int(split["coverage"])/1000.0,
+        path,
+        experimentString,
+        aggregateData,
+        ratiosDictionary
+    )
+    return None
