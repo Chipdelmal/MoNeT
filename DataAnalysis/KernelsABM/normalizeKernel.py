55f9ce353bdb793c0cef1a542d83c4d7e2023f37
chipdelmal <chipdelmal@gmail.com>
2019-11-21 15:41:10 -0800

    Normalize kernel is now a function ready for the Python pkg.

--- DataAnalysis/KernelsABM/normalizeKernel.py
+++ DataAnalysis/KernelsABM/normalizeKernel.py
@@ -3,15 +3,20 @@ import csv
 import numpy as np
 
 
+def normalizeKernel(kernelIn):
+    kernelOut = np.empty(kernelIn.shape)
+    for (i, row) in enumerate(kernel):
+        kernelOut[i] = row/np.sum(row)
+    return kernelOut
+
+
 (PATH, FILE) = (
         '/Volumes/marshallShare/Comoros_STP/Comoros/kernels/',
         'kernel_landcover_NEW'
     )
 
 kernelIn = np.loadtxt(PATH + FILE + '.csv', delimiter=',')
-kernelOut = np.empty(kernelIn.shape)
-for (i, row) in enumerate(kernel):
-    kernelOut[i] = row/np.sum(row)
+kernelOut = normalizeKernel(kernelIn)
 
 np.savetxt(
         PATH + 'kernels/' + FILE + '_NRM.csv',
