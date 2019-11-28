d09cb1bbc33fbace72b6eebd05c8e5888c8070a4
Maya Shen <mayashen@berkeley.edu>
2019-11-27 16:31:03 -0800

    yorkeys knob pop sampling script

--- GMLandscapeAnalysis/Sample_Distribution/sample_dist_yorkeysknob.py
+++ GMLandscapeAnalysis/Sample_Distribution/sample_dist_yorkeysknob.py
@@ -13,22 +13,25 @@ n_samp: int, number of samples to draw
 import os
 import subprocess
 import time
+import numpy as np
 import pandas as pd
 
 # Exponential or gamma?
 EXP = True
 
 # PARAMETERS TO EDIT
-n = 10 # number of nodes in the network
 popnode = 15 # average population per node
 cv = 0.8 # somewhat clustered compared to exponential
 p = 0.15 # fraction of empty nodes
 b = 90 # truncation upper bound
 n_samp = 1 # how many times to resample from ZITE/ZITG
+popconst = 15 # constant pop for second population
 
 # PATH TO EDIT
 folder = "simulations/"
 filepath = "/Users/mayashen/Desktop/MoNeT/GMLandscapeAnalysis/Sample_Distribution/"+folder
+
+poppath = "/Users/mayashen/Desktop/MoNeT/GMLandscapeAnalysis/Sample_Distribution/Yorkeys01_0001_I.csv"
 
 os.path.exists(filepath)
 if not os.path.exists(filepath):
@@ -36,6 +39,12 @@ if not os.path.exists(filepath):
 
 
 def main():
+    pops = pd.read_csv(poppath, header=0, index_col=0)
+    # number of nodes in the network
+    n = pops[pops['Cluster'] != 0].shape[0]
+    n_const = pops.shape[0] - n
+    pops = pops[['Latitude', 'Longitude']]
+
     if EXP:
         print("Sampling from truncated EXPONENTIAL distribution "+str(n_samp)+" times with "+str(n)+" nodes and size "+str(popnode)+" per node.")
         filename = "sample_exp_"+str(n).zfill(4)+"_"+str(popnode).zfill(4)+"_"+str(int(p*100)).zfill(3)+"_"+str(b).zfill(3)
@@ -43,17 +52,21 @@ def main():
         args = [str(n), str(popnode), str(p), str(b)]
     else:
         print("Sampling from truncated GAMMA distribution "+str(n_samp)+" times with "+str(n)+" nodes and size "+str(popnode)+" per node.")
-        filename = "sample_gamma_"+str(n).zfill(4)+"_"+str(popnode).zfill(4)+"_"+str(int(p*100)).zfill(3)+"_"+str(b).zfill(3)
+        filename = "sample_gamm a_"+str(n).zfill(4)+"_"+str(popnode).zfill(4)+"_"+str(int(p*100)).zfill(3)+"_"+str(b).zfill(3)
         commandline_args = ['Rscript', '--vanilla', 'sample_gamma.R']
         args = [str(n), str(popnode), str(cv), str(p), str(b)]
 
     FNULL = open(os.devnull, 'w')
     for i in range(n_samp):
+        # Call script to write population to csv
         csvfilepath = filepath+filename+"_"+str(i).zfill(3)+".csv"
         retcode = subprocess.call(commandline_args + [csvfilepath] + args, stdout=FNULL, stderr=subprocess.STDOUT)
-        pop_draws = pd.read_csv(csvfilepath, header=0)
-        pop_draws.columns = ['n']
-        pop_draws.to_csv(csvfilepath, index=False)
+        # Create csv with coordinates + second population
+        pop_draws_df = pd.read_csv(csvfilepath, header=0)
+        pop_draws = list(pop_draws_df.values.flatten()) + [popconst]*n_const
+        pops_copy = pops.copy()
+        pops_copy.insert(2, "n", pop_draws)
+        pops_copy.to_csv(csvfilepath)
     print("Process completed.")
 
 # RUN THIS
