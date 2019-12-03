b2295f8e9aa82443300f975c2e4a2966bc4b9c00
chipdelmal <chipdelmal@gmail.com>
2019-12-03 13:09:35 -0800

    Style updates in the SplitDrive routines.

--- DataAnalysis/SplitDrive_Suppression/main.py
+++ DataAnalysis/SplitDrive_Suppression/main.py
@@ -1,32 +1,34 @@
 import sys
-import os
+# import os
 import aux as aux
 import drive as drive
-import numpy as np
+# import numpy as np
 import datetime
 import MoNeT_MGDrivE as monet
 import matplotlib.pyplot as plt
 # import matplotlib.patches as mpatches
-# %matplotlib inline
 
-# print(sys.argv[2])
+
 if sys.argv[1] != "srv":
-    (ECO, PATH) = (sys.argv[2] == 'eco', '/Volumes/marshallShare/SplitDriveSup/')
+    (ECO, PATH) = (
+            sys.argv[2] == 'eco',
+            '/Volumes/marshallShare/SplitDriveSup/'
+        )
 else:
-    (ECO, PATH) = (sys.argv[2] == 'eco', '/RAID5/marshallShare/SplitDriveSup/')
+    (ECO, PATH) = (
+            sys.argv[2] == 'eco',
+            '/RAID5/marshallShare/SplitDriveSup/'
+        )
 # For testing
 (ECO, PATH) = (True, '/Volumes/marshallShare/SplitDriveSup/')
 ###############################################################################
 # Setup paths and analysis type
 ###############################################################################
 PATH_IMG = PATH + 'img/'
-(expsNum, folders) = (
-        6,
-        [
-            'ylinkedXShredder', 'autosomalXShredder',
-            'CRISPR','IIT','SIT','SplitDrive','fsRIDL', 'pgSIT'
-        ]
-    ) ##aux.getExperiments(PATH)
+folders = [
+        'ylinkedXShredder', 'autosomalXShredder', 'CRISPR',
+        'IIT', 'SIT', 'SplitDrive', 'fsRIDL', 'pgSIT'
+    ]
 (expType, style, path) = aux.selectAnalysisType(ECO, PATH_IMG)
 ###############################################################################
 # Iterate through folders
@@ -46,12 +48,12 @@ for dir in folders:
     print(aux.PAD + '* Processing Experiments [{0}]'.format(time) + aux.PAD)
     ###########################################################################
     num = len(pathExps)
-    aux.makeFolder(path + dir)
+    monet.makeFolder(path + dir)
     drv = drivePars.get(expType)
     for i in range(0, num, 1):
         pathSample = pathExps[i]
         experimentString = pathSample.split("/")[-1]
-        paths = sorted(monet.listDirectoriesWithPathWithinAPath(pathSample + "/"))
+        paths = monet.listDirectoriesWithPathWithinAPath(pathSample + "/")
         landscapeReps = monet.loadAndAggregateLandscapeDataRepetitions(
                 paths, drv, male=True, female=True
             )
@@ -65,16 +67,20 @@ for dir in folders:
             figsArray[j].get_axes()[0].set_ylim(0, style['yRange'][1])
             expOutStr = path + drivePars.get('folder') + '/' + experimentString
             monet.quickSaveFigure(
-                figsArray[j], expOutStr + "_N" + str(j) + ".png", dpi=style['dpi']
+                figsArray[j], expOutStr + "_N" + str(j) + ".png",
+                dpi=style['dpi']
             )
         plt.close('all')
-        print('Exported {0}/{1}: {2}'.format(str(i+1).rjust(4,'0'), num, expOutStr))
+        print(
+                'Exported {0}/{1}: {2}'.format(
+                    str(i+1).rjust(4,'0'), num, expOutStr
+                )
+            )
     # Export color palette
     drvNum = len(drv['genotypes'])
     (labels, colors) = (drv['genotypes'], style['colors'][0:drvNum])
     filename = path + drivePars.get('folder') + '/legend.png'
     aux.exportGeneLegend(labels, colors, filename, dpi=500)
-
 
     ###########################################################################
     time = str(datetime.datetime.now())
