f4a11292f47546a2d0a7e9bf7c170b9e14b35934
gillichu <gillichu@berkeley.edu>
2019-12-29 23:11:36 -0800

    Better comments

--- Dev/GillianChu/cluster_error.py
+++ Dev/GillianChu/cluster_error.py
@@ -3,10 +3,20 @@ import numpy as np
 import csv
 import MoNeT_MGDrivE as monet
 
-experiments_path = '/Volumes/marshallShare/ERACR/Yorkeys_MINI/Experiemnts'
-land_aggregated_path = '/Volumes/marshallShare/ERACR/Yorkeys_MINI/LandAggregated'
-#experiments_path = "/Users/gillian/Desktop/GillianDataset/Experiments"
-#land_aggregated_path = "/Users/gillian/Desktop/GillianDataset/LandAggregated"
+""" 
+The goal of this script is to take the difference between an aggregated time series vector, and the full resolution time 
+series vector. 
+"""
+
+"""
+*************************** Input Paths ************************************************************************************* 
+"""
+path_to_all_experiments = '/Volumes/marshallShare/ERACR/Yorkeys_MINI/Experiemnts'
+path_to_aggregated_landscapes = '/Volumes/marshallShare/ERACR/Yorkeys_MINI/LandAggregated'
+
+#path_to_all_experiments = "/Users/gillian/Desktop/GillianDataset/Experiments"
+#path_to_aggregated_landscapes = "/Users/gillian/Desktop/GillianDataset/LandAggregated"
+
 gender_mean = "F"
 maleToggle, femaleToggle = True, True
 
@@ -18,7 +28,8 @@ finish_ref = "/ANALYZED/E_0730_30_20_02_00020/"
 population_IDs = ["W", "H", "E", "R", "B"]
 aggregation_levels = ["C000002", "C000025", "C000250", "C001000", "C002195"]
 num_runs = 1
-# it computes for rall provided runs
+
+# it computes for all provided runs
 aggregationDictionary = monet.autoGenerateGenotypesDictionary(
     population_IDs,
     [
@@ -26,39 +37,51 @@ aggregationDictionary = monet.autoGenerateGenotypesDictionary(
         'HB', 'EE', 'ER', 'EB', 'RR', 'RB', 'BB'
     ]
 )
-# STEP 1 FUNCTIONS
 
-# Finds the paths to each aggregation level
-def getExperimentPaths(experiments_path):
+"""
+*************************** Functions *************************************************************************************** 
+"""
+def find_agg_to_listruns(path_to_all_experiments):
 	"""
-	Gives a list of aggregation_level/run_id folder names saved at the provided experiments_path.
+	Given the path to all experiments, builds a dictionary of experiment runs. Lookup by aggregation level. 
+
+	Input: 
+		path_to_all_experiments: should be the path of all the experiments
+	Output:
+		all_run_paths: a list of "aggregation_level/run_id" paths 
+		agg_level_dict: a dictionary with key: aggregation_level, value: a list of different experiment runs
 	"""
-	counts = dict()
-	run_paths = []
-	aggregation_levels = [dl for dl in os.listdir(experiments_path) if os.path.isdir(os.path.join(experiments_path, dl))]
+	agg_level_dict = dict()
+	all_run_paths = []
+	aggregation_levels = [dl for dl in os.listdir(path_to_all_experiments) if os.path.isdir(os.path.join(path_to_all_experiments, dl))]
 	for a in aggregation_levels:
-		agg_path = os.path.join(experiments_path, a)
-		counts[a] = []
+		agg_path = os.path.join(path_to_all_experiments, a)
+		agg_level_dict[a] = []
 
 		for run in os.listdir(agg_path):
-			run_paths.append(os.path.join(a, run))
-			counts[a] += [run]
-	return run_paths, counts
+			all_run_paths.append(os.path.join(a, run))
+			agg_level_dict[a] += [run]
+	return all_run_paths, agg_level_dict
 
 
-def getAggregation(run_path, land_aggregated_path):
+def find_node_to_cluster(run_path, path_to_aggregated_landscapes):
 	"""
-	run_path: the path specifying which level of aggregation, and which run ID
-	land_aggregated_path: the path to the LandAggregated folder, holding the cluster summaries
+	Reads in the experiment at run_path, and generates a mapping of each node to the cluster ID it's 
+	aggregated into. 
 
-	Returns a dictionary following the below format:
-	key: cluster ID
-	value: list of node IDs that were aggregated into this cluster
+	Input:
+		run_path: the path specifying which level of aggregation, and which run ID
+		path_to_aggregated_landscapes: the path to the LandAggregated folder, holding the cluster summaries
 
-	This is also saved into a csv file as a result of running this function.
+	Output: Returns a dictionary following the below format:
+		key: cluster ID
+		value: list of node IDs that were aggregated into this cluster
+ 
+	A side effect is a CSV file generated saving a mapping from cluster ID to node ID for a particular 
+	aggregation level. For an example, look for a file with "_cluster_summary_".
 	"""
 	agg_level, run = run_path.split('/')
-	agg_level_run = os.path.join(land_aggregated_path, run_path)
+	agg_level_run = os.path.join(path_to_aggregated_landscapes, run_path)
 	agg_level_edited = agg_level_run[:len(agg_level_run) - 1]
 	agg_level_edited += "I.csv"
 
@@ -77,7 +100,7 @@ def getAggregation(run_path, land_aggregated_path):
 
 	# Save this result into a csv file
 	with open(agg_level + "_cluster_summary_" + run[:len(run) - 2] + ".csv", mode="w") as cluster_summary:
-		result_writer = csv.writer(cluster_summary)
+		result_writer = csv.writer(cluster_summary) 
 		result_writer.writerow(["Cluster ID", "Node ID"]) # labels
 		for r in result:
 			result_writer.writerow(r)
@@ -97,7 +120,7 @@ def getAggregation(run_path, land_aggregated_path):
 # STEP 2 FUNCTIONS
 def process_node_id(node_id):
 	"""
-	Prepends node_id with 0's in string format
+	Prepends node_id with 0's in string format, so that it's 3 digits and can be used as part of the lookup path.
 	"""
 	node_id = str(node_id)
 	# print("Inside process_node: start", node_id)
@@ -106,13 +129,17 @@ def process_node_id(node_id):
 	# print("Inside process_node: end", node_id)
 	return node_id
 
-def getSum(key, run):
+def sum_nodes_in_cluster(key, cluster_dict):
 	"""
-	key tells the number of nodes, and the run ID
-	run is a dictionary where cluster ID maps to list of node IDs that cluster summarizes
+	Gets the summed output vector for the nodes aggregated into each cluster, and calls get_diff_agg_vs_full to calculate the 
+	difference betweeen this aggregation level's summed output vector, and the full resolution output vector. 
 
-	Output:
-	the summed output vector for each of the clusters
+	Input:
+		key: string that should be used as the key in the dict. Can be parsed for the number of nodes, and the run ID.
+		cluster_dict: is a dictionary where cluster ID maps to list of node IDs that cluster summarizes.
+
+	Output: None
+		Calls get_diff_agg_vs_full() writes the summed output difference vector for each of the clusters to the CSV.
 	"""
 	agg_level, run_id = key.split('/')
 	print(agg_level, run_id)
@@ -120,10 +147,10 @@ def getSum(key, run):
 	if not os.path.exists("agg_" + agg_level):
 		os.mkdir("agg_" + agg_level)
 
-	clusters = list(run.keys())
+	clusters = list(cluster_dict.keys())
 	for c in clusters:
 		print("Processing cluster ID ", c)
-		nodes_to_summarize = run[c]
+		nodes_to_summarize = cluster_dict[c]
 
 		if not os.path.exists("agg_" + agg_level + "/cluster_" + process_node_id(c)):
 			os.mkdir("agg_" + agg_level + "/cluster_" + process_node_id(c))
@@ -158,10 +185,10 @@ def getSum(key, run):
 		# take the difference between the sum and the clustered result
 		# male
 		male = str(agg_level) + "/" + str(run_id) + "/ANALYZED/E_0730_30_20_02_00020/M_Mean_Patch" + process_node_id(c) + ".csv"
-		exp_path_M = os.path.join(experiments_path, male)
+		exp_path_M = os.path.join(path_to_all_experiments, male)
 		# female
 		female = str(agg_level) + "/" + str(run_id) + "/ANALYZED/E_0730_30_20_02_00020/F_Mean_Patch" + process_node_id(c) + ".csv"
-		exp_path_F = os.path.join(experiments_path, female)
+		exp_path_F = os.path.join(path_to_all_experiments, female)
 
 		filenames2 = dict()
 		filenames2['male'] = [exp_path_M]
@@ -170,13 +197,22 @@ def getSum(key, run):
 		agg_sum = monet.sumLandscapePopulationsFromFiles(filenames2, male=maleToggle, female=femaleToggle)
 		aggGeno_sum = monet.aggregateGenotypesInNode(agg_sum, aggregationDictionary)
 
-		getDiff(aggGeno_sum, clusterGeno_sum, "agg_" + agg_level + "/cluster_" + process_node_id(c) + "/run_" + run_id + str(maleToggle) + str(femaleToggle))
+		# take the difference between the aggregated_sum vector and the full unaggregated sum vector, writes it to CSV
+		get_diff_agg_vs_full(aggGeno_sum, clusterGeno_sum, "agg_" + agg_level + "/cluster_" + process_node_id(c) + "/run_" + run_id + str(maleToggle) + str(femaleToggle))
 
 
-def getDiff(aggGeno_sum, clusterGeno_sum, name):
+def get_diff_cluster_vs_full(aggGeno_sum, clusterGeno_sum, name):
 	"""
 	Takes path to cluster_id run in Experiments/,
 	as well as the cluster_sum, which is the sum of all the nodes in that cluster_id.
+
+	Input: 
+		aggGeno_sum: dict representing the full resolution of nodes.
+		clusterGeno_sum: dict representing the aggregated clusters.
+		name: rseulting difference vector csv's name.
+
+	Output: None
+
 	"""
 	# print("AGG_GENO_RESULT: ", aggGeno_sum['population'].shape)
 	# print("CLUSTER_GENO_RESULT: ", clusterGeno_sum['population'].shape)
@@ -189,34 +225,47 @@ def getDiff(aggGeno_sum, clusterGeno_sum, name):
 		for r in result:
 			result_writer.writerow(r)
 
-def compareOnce():
-	# sample randomly over runs provided
-	print("Available keys are: ", counts.keys())
-	for key in counts.keys():
-		i = int(np.round(np.random.uniform(0, len(counts[key]) - 1)))
-		run_path = key + "/" + counts[key][i]
-		print("Running for key: ", key, " and run: ", counts[key][i])
-		aggData = getAggregation(run_path, land_aggregated_path)
-		cluster_run_sum = getSum(run_path, aggData)
+def compare_all_aggregations_once(agg_level_dict):
+	"""
+	Iterates over each aggregation level and samples randomly for an experiment run. 
+
+	Input: 
+		agg_level_dict: dictionary of agg_level to list of experimnt runs' paths. 
+
+	Output: None
+		A side effect of find_node_to_cluster() saves a CSV of each node mapped to the cluster ID in a particular aggregation level. 
+		A side effect of sum_nodes_in_cluster()->get_diff_agg_vs_full saves the difference vector into a CSV.
+	"""
+	# iterate over all of the aggregation levels
+	print("Available aggregation_levels to use as keys to agg_level_dict: ", agg_level_dict.keys())
+	for key in agg_level_dict.keys():
+		# sample randomly over runs provided
+		i = int(np.round(np.random.uniform(0, len(agg_level_dict[key]) - 1)))
+		# generate the run_path 
+		run_path = key + "/" + agg_level_dict[key][i]
+		print("Running for key: ", key, " and run: ", agg_level_dict[key][i])
+		# generate a dict of clusterIDs to nodeIDs aggregated into that cluster
+		aggData = find_node_to_cluster(run_path, path_to_aggregated_landscapes)
+		# sums an output difference vector for each cluster
+		cluster_run_sum = sum_nodes_in_cluster(run_path, aggData)
 
 
 ## MAIN
 
 ## STEP 1: Create mapping from aggregated to not aggregated in csv
-
-run_paths, counts = getExperimentPaths(experiments_path)
+all_run_paths, agg_level_dict = find_agg_to_listruns(path_to_all_experiments)
 
 # STEP 2: Sum all nodes in a cluster and generate difference
-
 for i in range(num_runs):
-	compareOnce()
+	# call compare_all_aggregations_once to iterate over each aggregation level and sample randomly
+	compare_all_aggregations_once(agg_level_dict)
 
 # run_dict = dict()
-# for run_path in run_paths:
-# 	run_dict[run_path] = getAggregation(run_path, land_aggregated_path)
-# 	cluster_run_sum = getSum(run_path, run_dict[run_path])
+# for run_path in all_run_paths:
+# 	run_dict[run_path] = find_node_to_cluster(run_path, path_to_aggregated_landscapes)
+# 	cluster_run_sum = sum_nodes_in_cluster(run_path, run_dict[run_path])
 
 # Previously used for testing, hardcoded
 # key = 'C000250/Yorkeys01_0000_A' # 250 nodes, run 0
 # run = run_dict[key] # dictionary for that run
-# cluster_run_sum = getSum(key, run)
+# cluster_run_sum = sum_nodes_in_cluster(key, run)
