8fe69c65c8b5a0ada85d66a317ab70168a28bff0
chipdelmal <chipdelmal@gmail.com>
2020-01-17 15:16:01 -0800

    Minor style changes for TD Traces

--- DataAnalysis/ThresholdDependent/Traces.py
+++ DataAnalysis/ThresholdDependent/Traces.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
 import MoNeT_MGDrivE as monet
 import auxT as aux
 import matplotlib.pyplot as plt
 plt.rcParams.update({'figure.max_open_warning': 0})
 
-
+(CRED, CEND) = ('\033[91m', '\033[0m')
 colors = ['#9f00cc', '#ec0b43', '#ff009d', '#94d4ff', '#232ed1']
 style = {
         "width": .1, "alpha": .1, "dpi": 500,
         "legend": True, "aspect": .5,
-        "xRange": [0, 3 * 365], "yRange": [0, 20000],
+        "xRange": [0, 3 * 365], "yRange": [0, 225000],
         "colors": ['#9f00cc', '#ec0b43', '#0038a8']
     }
-style['aspect'] = monet.scaleAspect(.04, style)
+style['aspect'] = monet.scaleAspect(.175, style)
+
+
+def setRange(ax, style):
+    ax.set_xlim(style['xRange'][0], style['xRange'][1])
+    ax.set_ylim(style['yRange'][0], style['yRange'][1])
+    return ax
 
 
 def driveGenesSelector(driveID):
@@ -38,18 +45,21 @@ aggregationDictionary = monet.generateAggregationDictionary(
         ["W", "H"], [[x - 1 for x in wildsList], [x - 1 for x in homingList]]
     )
 pathRoot = '/Volumes/marshallShare/ThresholdResub/factorialTRACES/udMel/GARBAGE/'
-exps = monet.listDirectoriesWithPathWithinAPath(pathRoot)
+exps = sorted(monet.listDirectoriesWithPathWithinAPath(pathRoot))
 expsNames = [i.split('/')[-1] for i in exps]
-for i in expsNames:
+expsNames.reverse()
+for i in expsNames[1:]:
     pathExt = i
     pathFull = pathRoot + pathExt + '/'
-    print(pathFull)
+    print(CRED + pathFull + CEND)
     paths = monet.listDirectoriesWithPathWithinAPath(pathFull)
     landscapeReps = monet.sumAggregatedLandscapeDataRepetitionsAlt(
             paths, aggregationDictionary,
-            male=False, female=True, dataType=float
+            male=True, female=True, dataType=float
         )
     figsArray = monet.plotLandscapeDataRepetitions(landscapeReps, style)
+    axTemp = figsArray[0].get_axes()[0]
+    axTemp = setRange(axTemp, style)
     figsArray[0].savefig(
             pathRoot + "img/" + pathExt + ".pdf",
             dpi=1024, facecolor='w',
