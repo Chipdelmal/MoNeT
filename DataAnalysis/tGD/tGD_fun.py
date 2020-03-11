a24dae2065f35ff93073ccc70f283ee1f1f89871
chipdelmal <chipdelmal@gmail.com>
2020-03-11 11:20:33 -0700

    Cleaning up the tGD script because it was a mess

---
+++ DataAnalysis/tGD/tGD_fun.py
@@ -0,0 +1,124 @@
+
+import numpy as np
+import tGD_aux as aux
+import tGD_plots as plots
+import MoNeT_MGDrivE as monet
+import matplotlib.pyplot as plt
+
+
+def getAggDataSSDay(pathsRoot, i, aggregationDictionary):
+    pathSample = pathsRoot[i] + "/"
+    experimentString = pathSample.split("/")[-2]
+    filenames = monet.readExperimentFilenames(pathSample)
+    landscapeSumData = monet.sumLandscapePopulationsFromFiles(
+        filenames, male=True, female=True, dataType=float
+    )
+    aggData = monet.aggregateGenotypesInNode(
+        landscapeSumData,
+        aggregationDictionary
+    )
+    ssDay = aux.reachedSteadtStateAtDay(aggData, .01)
+    return aggData, ssDay, experimentString
+
+
+def getLandscapeReps(i, pathRoot, pathExt, aggregationDictionary):
+    pathsRoot = monet.listDirectoriesWithPathWithinAPath(
+            pathRoot + pathExt + "ANALYZED/"
+        )
+    aggData, ssDay, _ = getAggDataSSDay(pathsRoot, i, aggregationDictionary)
+    ssDay = 0
+    #######################################################################
+    pathsRoot = monet.listDirectoriesWithPathWithinAPath(
+        pathRoot + pathExt + "GARBAGE/"
+    )
+    pathSample = pathsRoot[i]
+    experimentString = pathSample.split("/")[-1]
+    paths = monet.listDirectoriesWithPathWithinAPath(pathSample + "/")
+    landscapeReps = monet.loadAndAggregateLandscapeDataRepetitions(
+        paths, aggregationDictionary,
+        male=False, female=True, dataType=float
+    )
+    return landscapeReps, ssDay, experimentString
+
+
+def plotAndSaveLandscapeReps(
+            landscapeReps, ssDay, path, xRange, yRangeFixed, style
+        ):
+    figsArray = plots.plotLandscapeDataRepetitions(
+            landscapeReps,
+            style,
+            ssDay,
+            yRangeFixed
+        )
+    for i in range(0, len(figsArray)):
+        figsArray[i].get_axes()[0].set_xlim(0, xRange)
+        figsArray[i].get_axes()[0].set_ylim(0, yRangeFixed)
+        figsArray[i].get_axes()[0].set_aspect(.01)
+        monet.quickSaveFigure(
+            figsArray[i],
+            path,
+            dpi=750,
+            format="pdf"
+        )
+        plt.close()
+
+
+def getFFStrings(aggData, DRIVE):
+    if (DRIVE == 1 or DRIVE == 2):
+        groupingsList = [[2]]
+        groupingsListH = [[1]]
+        ratiosAtEnd = aux.getRatiosAtEnd(aggData, groupingsList, -1)
+        ratiosAtEndH = aux.getRatiosAtEnd(aggData, groupingsListH, -1)
+        ffString = "p(R): " + format(ratiosAtEnd[0], '.3f')
+        ffStringH = "p(H): " + format(ratiosAtEndH[0], '.3f')
+    else:
+        groupingsList = [[2], [3], [2, 3]]
+        groupingsListH = [[1], [4], [1, 4]]
+        ratiosAtEnd = aux.getRatiosAtEnd(aggData, groupingsList, -1)
+        ratiosAtEndH = aux.getRatiosAtEnd(aggData, groupingsListH, -1)
+        intersection = 4 * (ratiosAtEnd[0] * ratiosAtEnd[1])
+        intersectionH = 4 * (ratiosAtEndH[0] * ratiosAtEndH[1])
+        union = 2*ratiosAtEnd[0] + 2*ratiosAtEnd[1] - 4*(ratiosAtEnd[0] * ratiosAtEnd[1])
+        unionH = 2*ratiosAtEndH[0] + 2*ratiosAtEndH[1] - 4*(ratiosAtEndH[0] * ratiosAtEndH[1])
+        ffString = "p(R1): " + format(ratiosAtEnd[0]*2, '.3f') + ", p(R2): " + format(
+            ratiosAtEnd[1]*2, '.3f') + ", p(R1&R2): " + format(intersection,
+            '.3f') + ", p(R1|R2): " + format(union, '.3f')
+        ffStringH = "p(H): " + format(ratiosAtEndH[0]*2, '.3f') + ", p(G): " + format(
+            ratiosAtEndH[1]*2, '.3f') + ", p(H&G): " + format(intersectionH,
+            '.3f') + ", p(H|G): " + format(unionH, '.3f')
+    return ffString, ffStringH
+
+
+def adjustAggDataForDrive(aggData, DRIVE):
+    if not (DRIVE == 1 or DRIVE == 2):
+        aggData = {
+            "genotypes": aggData["genotypes"],
+            "population": aggData["population"]/2
+        }
+    return aggData
+
+
+def plotAndSaveStack(aggData, ssDay, ffString, ffStringH, path, xRange, yRange, styleS):
+    figB = plots.plotMeanGenotypeStack(aggData, styleS, ssDay, 2 * yRange)
+    figB.get_axes()[0].set_xlim(0, xRange)
+    figB.get_axes()[0].set_ylim(0, 2 * yRange)
+    figB.get_axes()[0].set_title(
+        "[tSS: " + str(ssDay) + "] :: [" + ffString + "] :: [" + ffStringH + "]",
+        fontsize=5
+    )
+    monet.quickSaveFigure(
+        figB,
+        path,
+        dpi=1024,
+        format="pdf"
+    )
+    plt.close()
+
+
+def getTimeToMin(aggData):
+    pop = [sum(row) for row in aggData['population']]
+    for time in range(len(pop)):
+        popMin = min(pop)
+        if np.isclose(pop[time], popMin, atol=.1):
+            break
+    return (time, popMin)
--- DataAnalysis/tGD/tgD_mainRev.py
+++ DataAnalysis/tGD/tgD_mainRev.py
@@ -5,8 +5,8 @@ import os
 import numpy as np
 import MoNeT_MGDrivE as monet
 import tgD_select as sel
-import tGD_plots as plots
 import tGD_aux as aux
+import tGD_fun as fun
 import matplotlib.pyplot as plt
 plt.rcParams.update({'figure.max_open_warning': 0})
 
@@ -23,6 +23,7 @@ def quickSaveFigure(
         pad_inches=0, frameon=None
     )
 
+
 ##############################################################################
 # Drive:
 #   1: CRISPR
@@ -33,12 +34,7 @@ def quickSaveFigure(
 #   6: tGDXCross
 ##############################################################################
 DRIVE = 3
-TRACES = False
-TRACE_ANIMATION = False
-STACK = True
-STACK_ANIMATION = False
-SUMMARIES_DATA = False
-TRACES_DATA = False
+(TRACES, STACK) = (True, False)
 FORMAT = ".pdf"
 ##############################################################################
 ##############################################################################
@@ -65,118 +61,7 @@ styleS = {
     "width": 0, "alpha": .85, "dpi": 1024, "legend": False, "aspect": .0075,
     "colors": colors, "xRange": [0, 600], "yRange": [0, 5000]
 }
-xRange = 1000
-yRangeFixed = 11000
-##############################################################################
-##############################################################################
-def getAggDataSSDay(pathsRoot, i):
-    pathSample = pathsRoot[i] + "/"
-    experimentString = pathSample.split("/")[-2]
-    filenames = monet.readExperimentFilenames(pathSample)
-    landscapeSumData = monet.sumLandscapePopulationsFromFiles(
-        filenames, male=True, female=True, dataType=float
-    )
-    aggData = monet.aggregateGenotypesInNode(
-        landscapeSumData,
-        aggregationDictionary
-    )
-    ssDay = aux.reachedSteadtStateAtDay(aggData, .01)
-    return aggData, ssDay, experimentString
-
-def getLandscapeReps(i):
-    pathsRoot = monet.listDirectoriesWithPathWithinAPath(
-            pathRoot + pathExt + "ANALYZED/"
-        )
-    aggData, ssDay, _ = getAggDataSSDay(pathsRoot, i)
-    #######################################################################
-    pathsRoot = monet.listDirectoriesWithPathWithinAPath(
-        pathRoot + pathExt + "GARBAGE/"
-    )
-    pathSample = pathsRoot[i]
-    experimentString = pathSample.split("/")[-1]
-    paths = monet.listDirectoriesWithPathWithinAPath(pathSample + "/")
-    landscapeReps = monet.loadAndAggregateLandscapeDataRepetitions(
-        paths, aggregationDictionary,
-        male=False, female=True, dataType=float
-    )
-    return landscapeReps, ssDay, experimentString
-
-
-def plotAndSaveLandscapeReps(landscapeReps, ssDay, path):
-    figsArray = plots.plotLandscapeDataRepetitions(
-            landscapeReps,
-            style,
-            ssDay,
-            yRangeFixed
-        )
-    for i in range(0, len(figsArray)):
-        figsArray[i].get_axes()[0].set_xlim(0, xRange)
-        figsArray[i].get_axes()[0].set_ylim(0, yRangeFixed)
-        monet.quickSaveFigure(
-            figsArray[i],
-            path,
-            dpi=750
-        )
-        plt.close()
-
-def getFFStrings(aggData):
-    if (DRIVE == 1 or DRIVE == 2):
-        groupingsList = [[2]]
-        groupingsListH = [[1]]
-        ratiosAtEnd = aux.getRatiosAtEnd(aggData, groupingsList, -1)
-        ratiosAtEndH = aux.getRatiosAtEnd(aggData, groupingsListH, -1)
-        ffString = "p(R): " + format(ratiosAtEnd[0], '.3f')
-        ffStringH = "p(H): " + format(ratiosAtEndH[0], '.3f')
-    else:
-        groupingsList = [[2], [3], [2, 3]]
-        groupingsListH = [[1], [4], [1, 4]]
-        ratiosAtEnd = aux.getRatiosAtEnd(aggData, groupingsList, -1)
-        ratiosAtEndH = aux.getRatiosAtEnd(aggData, groupingsListH, -1)
-        intersection = 4 * (ratiosAtEnd[0] * ratiosAtEnd[1])
-        intersectionH = 4 * (ratiosAtEndH[0] * ratiosAtEndH[1])
-        union = 2*ratiosAtEnd[0] + 2*ratiosAtEnd[1] - 4*(ratiosAtEnd[0] * ratiosAtEnd[1])
-        unionH = 2*ratiosAtEndH[0] + 2*ratiosAtEndH[1] - 4*(ratiosAtEndH[0] * ratiosAtEndH[1])
-        ffString = "p(R1): " + format(ratiosAtEnd[0]*2, '.3f') + ", p(R2): " + format(
-            ratiosAtEnd[1]*2, '.3f') + ", p(R1&R2): " + format(intersection,
-            '.3f') + ", p(R1|R2): " + format(union, '.3f')
-        ffStringH = "p(H): " + format(ratiosAtEndH[0]*2, '.3f') + ", p(G): " + format(
-            ratiosAtEndH[1]*2, '.3f') + ", p(H&G): " + format(intersectionH,
-            '.3f') + ", p(H|G): " + format(unionH, '.3f')
-    return ffString, ffStringH
-
-def adjustAggDataForDrive(aggData):
-    if not (DRIVE == 1 or DRIVE == 2):
-            aggData = {
-                "genotypes": aggData["genotypes"],
-                "population": aggData["population"]/2
-            }
-    return aggData
-
-def plotAndSaveStack(aggData, ssDay, ffString, ffStringH, path):
-    figB = plots.plotMeanGenotypeStack(aggData, styleS, ssDay, 2 * yRange)
-    figB.get_axes()[0].set_xlim(0, xRange)
-    figB.get_axes()[0].set_ylim(0, 2 * yRange)
-    figB.get_axes()[0].set_title(
-        "[tSS: " + str(ssDay) + "] :: [" + ffString + "] :: [" + ffStringH + "]",
-        fontsize=5
-    )
-    monet.quickSaveFigure(
-        figB,
-        path,
-        dpi=1024,
-        format="pdf"
-    )
-    plt.close()
-
-
-def getTimeToMin(aggData):
-    pop = [sum(row) for row in aggData['population']]
-    for time in range(len(pop)):
-        popMin = min(pop)
-        if np.isclose(pop[time], popMin, atol=.1):
-            break
-    return (time, popMin)
-
+(xRange, yRangeFixed) = (4 * 365, 22500)
 ##############################################################################
 # MAIN
 ##############################################################################
@@ -184,18 +69,39 @@ if STACK is True:
     pathsRoot = monet.listDirectoriesWithPathWithinAPath(
         pathRoot + pathExt + "ANALYZED/"
     )
-    for i in range(0, len(pathsRoot)):
+    expNum = len(pathsRoot)
+    for i in range(0, expNum):
         #####################################################################
-        aggData, ssDay, experimentString = getAggDataSSDay(pathsRoot, i)
-        (ssDay, popMin) = getTimeToMin(aggData)
-        print("[" + str(i) + "] " + experimentString + ": " + str(ssDay) + " @ " + str(popMin))
+        (aggData, ssDay, experimentString) = fun.getAggDataSSDay(
+                pathsRoot, i, aggregationDictionary
+            )
+        (ssDay, popMin) = fun.getTimeToMin(aggData)
+        print('* Exporting ({}/{})'.format(str(i+1).zfill(4), str(expNum).zfill(4)), end='\r')
         #####################################################################
-        ffString, ffStringH = getFFStrings(aggData)
-        aggData = adjustAggDataForDrive(aggData)
+        ffString, ffStringH = fun.getFFStrings(aggData, DRIVE)
+        aggData = fun.adjustAggDataForDrive(aggData)
         #####################################################################
-        # figA = plots.plotMeanGenotypeTrace(aggData, styleT, ssDay, 2 * yRange)
+        # figA = plots.plotMeanGenotypeTrace(aggData, styleT, ssDay, 2*yRange)
         # figA.get_axes()[0].set_xlim(0, xRange)
         # figA.get_axes()[0].set_ylim(0, 2 * yRange)
+        fun.plotAndSaveStack(
+                aggData, ssDay, ffString, ffStringH,
+                pathRoot + "/images/" + str(DRIVE).rjust(2, "0") + "S_" + experimentString + FORMAT,
+                xRange, yRange, styleS
+            )
 
-        plotAndSaveStack(aggData, ssDay, ffString, ffStringH, pathRoot + "/images/" + str(DRIVE).rjust(2, "0") + "S_" + experimentString + FORMAT)
+if TRACES is True:
+    pathsRoot = monet.listDirectoriesWithPathWithinAPath(
+        pathRoot + pathExt + "GARBAGE/"
+    )
+    expNum = len(pathsRoot)
+    ssDay = xRange
+    for i in range(0, expNum):
+        print('* Exporting ({}/{})'.format(str(i+1).zfill(4), str(expNum).zfill(4)), end='\r')
+        landscapeReps, ssDay, experimentString = fun.getLandscapeReps(i, pathRoot, pathExt, aggregationDictionary)
+        fun.plotAndSaveLandscapeReps(
+                landscapeReps, ssDay, pathRoot + "images/" + str(DRIVE).rjust(2, "0") + "R_" + experimentString + FORMAT,
+                xRange, yRangeFixed, style
+            )
+
 monet.exportGeneLegend(genes, colors, pathRoot + "/images/stacks/Palette" + FORMAT, 500)
