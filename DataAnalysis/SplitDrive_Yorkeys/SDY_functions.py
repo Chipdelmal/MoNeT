dc55da4274ac7dacc66c3a423f4e728106f7cbf5
chipdelmal <chipdelmal@gmail.com>
2020-03-26 08:02:56 -0700

    Modularizing functions for SDY

--- DataAnalysis/SplitDrive_Yorkeys/SDY_functions.py
+++ DataAnalysis/SplitDrive_Yorkeys/SDY_functions.py
@@ -1,9 +1,13 @@
 
-import MoNeT_MGDrivE as monet
 import numpy as np
+import SDY_aux as aux
+import MoNeT_MGDrivE as monet
 np.seterr(divide='ignore', invalid='ignore')
 
 
+###############################################################################
+# Error Calculations
+###############################################################################
 def rpd(signal, probe):
     num = probe - signal
     den = np.abs(probe) + np.abs(signal)
@@ -11,9 +15,59 @@ def rpd(signal, probe):
     return np.nan_to_num(err)
 
 
+###############################################################################
+# Paths
+###############################################################################
 def getValidationExperiments(path,setName):
     base = path + setName
     names = sorted(monet.listDirectoriesInPath(base+'/ANALYZED/'))
     pthsA = sorted(monet.listDirectoriesWithPathWithinAPath(base+'/ANALYZED/'))
     pthsB = sorted(monet.listDirectoriesWithPathWithinAPath(base+'/GARBAGE/'))
     return (list(zip(names, pthsA, pthsB)))
+
+
+def readDataPaths(aPath, gPath):
+    aFiles = monet.readExperimentFilenames(aPath)
+    gFiles = monet.listDirectoriesWithPathWithinAPath(gPath+'/')
+    return (aFiles, gFiles)
+
+
+def createFolders(foldersList):
+    for fldr in folderList:
+        monet.makeFolder(fldr)
+
+
+###############################################################################
+# Shaping Responses
+###############################################################################
+def loadSummedMeanResponse(aFiles, GDICT, MALE, FEMALE):
+    print(aux.CBBL, end='\r')
+    print('* Loading mean response...', end='\r')
+    landData = monet.loadLandscapeData(aFiles, male=MALE, female=FEMALE)
+    print('* Aggregating mean response...', end='\r')
+    aggDataM = monet.aggregateGenotypesInLandscape(landData, GDICT)
+    aggDataM['landscape'] = sum(aggDataM['landscape'])
+    print(aux.CEND, end='\r')
+    return aggDataM
+
+
+def loadSummedTracesResponse(gFiles, GDICT, MALE, FEMALE):
+    aggDataT = monet.sumAggregatedLandscapeDataRepetitionsAlt(
+        gFiles, GDICT, male=MALE, female=FEMALE
+    )
+    return aggDataT
+
+
+def calcResponses(aFiles, gFiles, GDICT, MALE, FEMALE):
+    (aggDataM, aggDataT) = (
+            loadSummedMeanResponse(aFiles, GDICT, MALE, FEMALE),
+            loadSummedTracesResponse(gFiles, GDICT, MALE, FEMALE)
+        )
+    return (aggDataM, aggDataT)
+
+
+def loadAndCalcResponse(expSet, GDICT, MALE, FEMALE):
+    (name, aPath, gPath) = expSet
+    (aFiles, gFiles) = readDataPaths(aPath, gPath)
+    (aggDtaM, aggDtaT) = calcResponses(aFiles, gFiles, GDICT, MALE, FEMALE)
+    return (name, aggDtaM, aggDtaT)
--- DataAnalysis/SplitDrive_Yorkeys/SDY_validation.py
+++ DataAnalysis/SplitDrive_Yorkeys/SDY_validation.py
@@ -14,6 +14,7 @@ plt.rcParams.update({'figure.max_open_warning': 0})
 ###############################################################################
 # Setting up colors and style
 ###############################################################################
+(MALE, FEMALE) = (True, True)
 COLORS = ["#090446", "#f20060", "#c6d8ff", "#7692ff", "#29339b", "#7fff3a"]
 STYLE = {
         "width": .1, "alpha": .15, "dpi": 2 * 300,
@@ -28,16 +29,12 @@ GDICT = monet.autoGenerateGenotypesDictionary(aux.GENES, aux.GENOTYPES)
 # Get paths and create output folders
 ###############################################################################
 # Input paths
-sig = fun.getValidationExperiments(PATH, SETS[0]) # (xpSN, xpSAP, xpSGP)
-prb = fun.getValidationExperiments(PATH, SETS[1]) # (xpPN, xpPAP, xpPGP)
+sig = fun.getValidationExperiments(PATH, SETS[0])
+prb = fun.getValidationExperiments(PATH, SETS[1])
 # Shallow validation
 (xpTest, xpNumb) = (len(sig[0]) == len(prb[0]), len(sig[0]))
-# for i in range(xpNumb):
-#     print('{}-{}::{}-{}'.format(xpSN[i], xpPN[i], xpSAP[i], xpPAP[i]))
-# Outputs paths
 (PATH_ERR, PATH_IMG) = (PATH + 'err/', PATH + 'img/')
-monet.makeFolder(PATH_ERR)
-monet.makeFolder(PATH_IMG)
+monet.makeFolder([PATH_ERR, PATH_IMG])
 ###############################################################################
 # Print terminal message
 ###############################################################################
@@ -47,21 +44,9 @@ print('{}* Experiments test passed: {}{}'.format(aux.CYEL, xpTest, aux.CEND))
 ###############################################################################
 # Main analyses
 ###############################################################################
-(i, dtSet) = (0, prb)
-# Loop starts here!
-(name, aPath, gPath) = dtSet[i]
-(aFiles, gFiles) = (
-        monet.readExperimentFilenames(aPath),
-        monet.listDirectoriesWithPathWithinAPath(gPath+'/')
-    )
-# Mean response
-print(aux.CBBL, end='\r')
-print('* Loading mean response...', end='\r')
-landData = monet.loadLandscapeData(aFiles, male=True, female=True)
-print('* Aggregating mean response...', end='\r')
-aggData = monet.aggregateGenotypesInLandscape(landData, GDICT)
-aggData['landscape'] = sum(aggData['landscape'])
-print(aux.CEND, end='\r')
+i = 0
+(nS, mS, tS) = fun.loadAndCalcResponse(sig[i], GDICT, MALE, FEMALE)
+(nP, mP, tP) = fun.loadAndCalcResponse(prb[i], GDICT, MALE, FEMALE)
 ###############################################################################
 # Print terminal message
 ###############################################################################
