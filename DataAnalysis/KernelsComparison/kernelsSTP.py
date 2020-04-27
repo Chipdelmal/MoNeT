23f4d2cbc31b142f73f3ad39546841fc86068d78
tomasleon <tomas.leon@berkeley.edu>
2020-04-27 09:15:01 -0700

    change runtime and drop burn-in

--- DataAnalysis/KernelsComparison/kernelsSTP.py
+++ DataAnalysis/KernelsComparison/kernelsSTP.py
@@ -28,7 +28,7 @@ mpl.rcParams['axes.linewidth'] = .4
 (thresholds, SSPOP, MALE, FEMALE) = (
         [.1, .25, .5, .75, .9], 10000, True, True
     )
-(xRange, driveID, burn) = (1095, 'LDR', 150)
+(xRange, driveID, burn) = (4*365, 'LDR', 0)
 ###############################################################################
 # Setting up the experiment analyses
 ###############################################################################
