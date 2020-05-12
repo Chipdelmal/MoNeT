ba39b31b683ca28e6231f1d5bee06536b0cc138d
chipdelmal <chipdelmal@gmail.com>
2020-05-12 13:30:20 -0700

    STP Preprocess to pkl files running on dsk

--- DataAnalysis/UCIPanmictic/uciSTP_simPreprocess.py
+++ DataAnalysis/UCIPanmictic/uciSTP_simPreprocess.py
@@ -5,6 +5,7 @@
 # import sys
 # import csv
 import datetime
+import pickle as pkl
 import uciPan_aux as aux
 import uciPan_fun as fun
 # import uciPan_plot as plot
@@ -13,17 +14,10 @@ import uciSTP_indices as ix
 import MoNeT_MGDrivE as monet
 # import matplotlib.pyplot as plt
 
-
-# (ROOT, LAND, DRIVE_ID, SETTING) = (
-#         "RAID5", 'Yoosook/' + sys.argv[2],
-#         'LDR', sys.argv[3]
-#     )
-
-(USR, ROOT, LAND, DRIVE_ID, SETTING, STP) = (
-        'dsk', '/home/chipdelmal/Desktop/Panmictic',
-        'tParams', 'LDR', 'island', False
+USR = 'dsk'
+(LAND, DRIVE_ID, SET, STP, AOI, MF) = (
+        'tParams', 'LDR', 'island', False, 'HLT', (True, True)
     )
-(FACT, PLOT, MF) = (True, True, (True, True))
 (thresholds, REL_STRT) = ([.05, .10, .25, .50, .75], 1)
 drvPars = drv.driveSelector(DRIVE_ID)
 (STYLE, DRV, NOI) = (
@@ -36,37 +30,56 @@ drvPars = drv.driveSelector(DRIVE_ID)
 ###############################################################################
 # Select form server/desktop
 if USR == 'srv':
-    PATH_ROOT = '/{}/marshallShare/UCI/{}/{}/'.format(ROOT, LAND, SETTING)
+    PATH_ROOT = '/RAID5/marshallShare/UCI/Yoosook/{}/'.format(LAND, SET)
 else:
-    PATH_ROOT = '{}/{}/{}/'.format(ROOT, LAND, SETTING)
+    PATH_ROOT = '/home/chipdelmal/Desktop/Panmictic/{}/{}/'.format(LAND, SET)
 # Setting paths
 (PATH_IMG, PATH_DATA) = (
         '{}img/'.format(PATH_ROOT),
         '{}out/{}/'.format(PATH_ROOT, DRIVE_ID)
     )
+PATH_OUT = PATH_DATA + '/PREPROCESS/'
+monet.makeFolder(PATH_OUT)
 # Print terminal info and create folder
 tS = datetime.datetime.now()
 fun.printExpTerminal(tS, PATH_ROOT, PATH_IMG, PATH_DATA)
 ###############################################################################
 # Setting up paths and directories
 ###############################################################################
-gIx = drvPars['HLT']['genotypes'].index('Other')
+gIx = drvPars[AOI]['genotypes'].index('Other')
 (expDirsMean, expDirsTrac) = fun.getExpPaths(PATH_DATA)
 expNum = len(expDirsMean)
 ###############################################################################
 # Analyze data
 ###############################################################################
-exIx = 0
-# Setup paths -------------------------------------------------------------
-print('* Analyzing ({}/{})'.format(str(exIx + 1), str(expNum)), end='\r')
-(pathMean, pathTraces) = (expDirsMean[exIx], expDirsTrac[exIx])
-expName = pathMean.split('/')[-1]
-(dirsMean, dirsTraces) = (
-        pathMean, fun.listDirectoriesWithPathWithinAPath(pathTraces)
-    )
-files = monet.readExperimentFilenames(pathMean)
-filesList = [fun.filterFilesByIndex(files, ix) for ix in NOI]
-# Load data ----------------------------------------------------------------
-sumData = monet.sumLandscapePopulationsFromFiles(files, MF[0], MF[1])
-aggData = monet.loadAndAggregateLandscapeData(files, DRV, MF[0], MF[1])
-geneSpaTemp = monet.getGenotypeArraysFromLandscape(aggData)
+for exIx in range(0, expNum):
+    # Setup paths -------------------------------------------------------------
+    print('* Analyzing ({}/{})'.format(str(exIx + 1), str(expNum)), end='\r')
+    (pathMean, pathTraces) = (expDirsMean[exIx], expDirsTrac[exIx])
+    expName = pathMean.split('/')[-1]
+    (dirsMean, dirsTraces) = (
+            pathMean, fun.listDirectoriesWithPathWithinAPath(pathTraces)
+        )
+    files = monet.readExperimentFilenames(pathMean)
+    filesList = [fun.filterFilesByIndex(files, ix) for ix in NOI]
+    # Load data ---------------------------------------------------------------
+    sumData = monet.sumLandscapePopulationsFromFiles(files, MF[0], MF[1])
+    sumAgg = monet.aggregateGenotypesInNode(sumData, DRV)
+    aggData = monet.loadAndAggregateLandscapeData(files, DRV, MF[0], MF[1])
+    geneSpaTemp = monet.getGenotypeArraysFromLandscape(aggData)
+    landReps = monet.loadAndAggregateLandscapeDataRepetitions(
+            dirsTraces, DRV, MF[0], MF[1]
+        )
+    # PLACE OTHER PROCESSING OPERATIONS HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    preData = {
+            'sum': sumAgg, 'agg': aggData,
+            'spa': geneSpaTemp, 'rep': landReps
+        }
+    # Dump to serialized file -------------------------------------------------
+    fout = open('{}/{}_{}.pkl'.format(PATH_OUT, expName, AOI), 'wb')
+    pkl.dump(preData, fout)
+    fout.close()
+tE = datetime.datetime.now()
+print(aux.PADL)
+print('Finished [{}]'.format(tE-tS))
+print(aux.PAD)
