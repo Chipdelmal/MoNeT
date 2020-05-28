0d825cac391803461ca49eef038aa7e49b097d34
chipdelmal <chipdelmal@gmail.com>
2020-05-28 11:16:36 -0700

    SDY Plots being converted to prepost format

--- DataAnalysis/SplitDrive_Yorkeys/SDY_postDynamicsPlots.py
+++ DataAnalysis/SplitDrive_Yorkeys/SDY_postDynamicsPlots.py
@@ -5,6 +5,7 @@ import glob
 import datetime
 import numpy as np
 import SDY_ix as ix
+from glob import glob
 import SDY_aux as aux
 import SDY_functions as fun
 import MoNeT_MGDrivE as monet
@@ -13,5 +14,22 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'figure.max_open_warning': 0})
 
 
-(SET, TRA, HEA) = ('Aggregated', True, True)
-PATH = '/media/chipdelmal/cache/Sims/SplitDrive_Yorkeys/geoProof'
+(SET, TRA, HEA, EXT) = ('Aggregated', True, True, '.lzma')
+PATH = '/media/chipdelmal/cache/Sims/SplitDrive_Yorkeys/geoProof/'
+pathPre = '{}pre/{}/'.format(PATH, SET)
+###############################################################################
+# Setup paths
+###############################################################################
+(COLORS, CMAPS) = (aux.COLORS, aux.CMAPS)
+STYLE = {
+        "width": .1, "alpha": .15, "dpi": 2 * 300,
+        "legend": True, "aspect": .5, "colors": COLORS,
+        "xRange": [0, 1825], "yRange": [0, 150000]
+    }
+STYLE['aspect'] = monet.scaleAspect(.2, STYLE)
+###############################################################################
+# Load preprocessed files
+###############################################################################
+typTag = ('sum', 'spa')
+fLists = [sorted(glob(pathPre+'*'+tp+EXT)) for tp in typTag]
+(sumFList, spaFList) = fLists
