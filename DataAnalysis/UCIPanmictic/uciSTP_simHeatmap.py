f7735be5f92e13b62192333dac68686236be1c16
chipdelmal <chipdelmal@gmail.com>
2020-06-11 09:23:26 -0700

    STP Fixing heatmaps to be consistent with previous iterations

--- DataAnalysis/UCIPanmictic/uciSTP_simHeatmap.py
+++ DataAnalysis/UCIPanmictic/uciSTP_simHeatmap.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # https://stackoverflow.com/questions/23913151/log-log-lmplot-with-seaborn
 # https://seaborn.pydata.org/generated/seaborn.heatmap.html
+# https://matplotlib.org/3.2.1/gallery/images_contours_and_fields/irregulardatagrid.html
 # https://matplotlib.org/3.2.1/gallery/images_contours_and_fields/contourf_demo.html#sphx-glr-gallery-images-contours-and-fields-contourf-demo-py
 
 import math
@@ -28,6 +29,7 @@ header = ['ratio', 'releases', 'fitness', 'sv', 'group']
 header.extend(thr)
 drvPars = drv.driveSelector(DRV)
 ci = QNT[2]
+months = list(range(0, 12*4, 6))
 ###############################################################################
 # Setting up paths and directories
 ###############################################################################
@@ -69,25 +71,24 @@ for group in [0]:
                 )
             piv.to_csv(PATH_DATA+fName+'.csv')
             # Plot
-            (x, y, z) = (df['releases'], df['ratio'], df[level])
+            (x, y, z) = (df['ratio'], df['releases'], df[level])
             (x, y, z) = (
-                    np.array([int(i) for i in x]),
-                    np.array([float(i/1000000) for i in y]),
+                    np.array([float(i/1000000) for i in x]),
+                    np.array([int(i) for i in y]),
                     np.array([float(i) for i in z])
                 )
-            (grid_x, grid_y) = np.mgrid[0:a:100j, 0:b:200j]
-            grid = griddata((x, y), z, (grid_x, grid_y), method='nearest')
+            (a, b) = (max(x), max(y))
             fig, ax = plt.subplots()
-            ax.tricontourf(x, y, z, levels=10, cmap="RdBu_r")
+            ax.tricontourf(
+                    x, y, z,
+                    levels=[4*i for i in months],
+                    cmap="PuBu"
+                )
             ax.set(xscale="log", yscale="linear")
-            xmin, xmax = ax.get_xbound()
-            ymin, ymax = ax.get_ybound()
-            data_aspect = (
-                math.log(ymax)-math.log(ymin))/(math.log(xmax)-math.log(xmin)
-            )
-            # ax.set_aspect(1./data_aspect)
-            fig_ratio=1
-            fig.set_size_inches(fig.get_size_inches()[0], fig_ratio*fig.get_size_inches()[0])
+            fig.set_size_inches(
+                    fig.get_size_inches()[0],
+                    1*fig.get_size_inches()[0]
+                )
             plt.savefig(
                     PATH_IMG+fName+'.png',
                     dpi=400, facecolor=None, edgecolor='w',
@@ -95,6 +96,8 @@ for group in [0]:
                     transparent=True, bbox_inches='tight', pad_inches=.01
                 )
             plt.close('all')
+
+
 
 ###############################################################################
 # Heatmap
@@ -105,20 +108,27 @@ for group in [0]:
 #         np.array([float(i/1000000) for i in y]),
 #         np.array([float(i) for i in z])
 #     )
-# (a, b) = (max(x), max(y))
-(grid_x, grid_y) = np.mgrid[0:a:100j, 0:b:200j]
-grid = griddata((x, y), z, (grid_x, grid_y), method='nearest')
-# ax = plt.subplot(222)
-# plt.imshow(grid.T, extent=(0, a, 0, b), origin='lower')
-# ax.set_aspect(52/1)
-# ax.set(xscale="linear", yscale="linear")
-#
-#
-# ax = plt.subplot(222)
-# ax.tricontourf(x, y, z, levels=20, linewidths=0.5, cmap="RdBu_r")
-# ax.set(xscale="log", yscale="log")
+#(ngridx, ngridy) = (1000, 1000)
+# (grid_x, grid_y) = np.mgrid[0:a:100j, 0:b:200j]
+# grid = griddata((x, y), z, (grid_x, grid_y), method='nearest')
+# fig, ax = plt.subplots()
+# ax.tricontourf(x, y, z, levels=10, cmap="RdBu_r")
+# ax.set(xscale="log", yscale="linear")
 # xmin, xmax = ax.get_xbound()
 # ymin, ymax = ax.get_ybound()
-# data_aspect = (math.log(ymax)-math.log(ymin))/(math.log(xmax)-math.log(xmin))
-# ax.set_aspect(1./data_aspect)
-# plt.savefig(PATH_DATA)
+# data_aspect = (
+#     math.log(ymax)-math.log(ymin))/(math.log(xmax)-math.log(xmin)
+#     )
+# # ax.set_aspect(1./data_aspect)
+# fig_ratio=1
+# fig.set_size_inches(fig.get_size_inches()[0], fig_ratio*fig.get_size_inches()[0])
+#
+#
+#
+# (xi, yi) = (np.linspace(0, a, ngridx), np.linspace(0, b, ngridy))
+# fig, ax = plt.subplots()
+# zi = griddata((x, y), z, (xi[None, :], yi[:, None]), method='linear')
+# ax.contour(xi, yi, zi, levels=14, linewidths=1, colors='k')
+# ax.set(xscale="log", yscale="linear")
+# cntr1 = ax.contourf(xi, yi, zi, levels=14, cmap="RdBu_r")
+# fig.set_size_inches(fig.get_size_inches()[0], fig_ratio*fig.get_size_inches()[0])
