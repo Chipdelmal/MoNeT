7238c165393f35009916c8a8edba43759ea0e1a5
zhangpriscilla <45432629+zhangpriscilla@users.noreply.github.com>
2020-07-01 17:36:55 -0700

    Update Compression.py

--- Dev/PriscillaZhang/Compression.py
+++ Dev/PriscillaZhang/Compression.py
@@ -8,11 +8,11 @@ import Constants as constants
 import directories as directory
 import Compression_Algorithms as all_algs
 
-# If an output directory doesn't exist, make one
+"""Create a new directory if one doesn't already exist """
 if not os.path.exists(directory.output_directory):
     os.mkdir(directory.output_directory)
 
-#Get the initial file sizes to later calculate compression ratio
+"""Import the data from the directory & prepare it for compression"""
 for filename in os.listdir(directory.input_directory):
     data = pd.read_csv(directory.input_directory + '\\' + filename)
     constants.initial_filesize.append(os.path.getsize(directory.input_directory + '\\' + filename))
@@ -20,18 +20,13 @@ for filename in os.listdir(directory.input_directory):
     for col in data.columns:
         headers.append(col)
     headers_list = list(data.columns)[1:]
-
-    """Create a Dictionary for each csv file
-       First element is the header (genotype)
-       Second element are the counts stored in a np array
-    """
     data_dict = {}
     for header in headers_list:
         dic_val = np.array(data[header])
         data_dict[header] = dic_val
     constants.filename2dict[filename[:-4]] = data_dict
 
-"""Begin Compression Experiment"""
+"""Conducting the Compression Experiment"""
 for alg in all_algs.all_compression_algs:
     type_alg = constants.alg2name[alg]
     end = constants.alg2end[constants.alg2name[alg]]
@@ -40,26 +35,23 @@ for alg in all_algs.all_compression_algs:
         times = newfunc.getCompressionTimes(alg, dict, type_alg, filename+end, constants.all_data, directory.output_directory)
         constants.all_data[type_alg + '_compressedData_Time'].append(times[0])
         constants.all_data[type_alg + '_compression_time'].append(times[1])
+for filename in constants.filename2dict.keys():
+    recorded_time = newfunc.getZipCompressionTime(filename)
+    constants.all_data['zip_compression_time'].append(recorded_time)
+constants.zipfolder.close()
 
-# Get the compressed file sizes to calculate compression ratio later
+"""Get the compressed file sizes """
 for file in os.listdir(directory.output_directory):
     if file.endswith("bz2"):
         constants.all_data['bz2_compressed_filesize'].append(os.path.getsize(directory.output_directory + '\\' + file))
     if file.endswith(".gz"):
         constants.all_data['gzip_compressed_filesize'].append(os.path.getsize(directory.output_directory + '\\' + file))
     if file.endswith(".xz"):
-        constants.all_data['lzma_compressed_filesize'].append(os.path.getsize(directory.output_directory + '\\' + file ))
-
-""" ZIP Compression"""
+        constants.all_data['lzma_compressed_filesize'].append(os.path.getsize(
+        directory.output_directory + '\\' + file ))
 for filename in constants.filename2dict.keys():
-    recorded_time = newfunc.getZipCompressionTime(filename)
-    constants.all_data['zip_compression_time'].append(recorded_time)
-constants.zipfolder.close()
-
-"""Get the file compressed zip file sizes """
-for filename in constants.filename2dict.keys():
-    k = constants.zipfolder.getinfo(filename)
-    constants.all_data['zip_compressed_filesize'].append(k.compress_size)
+    fileinfo = constants.zipfolder.getinfo(filename)
+    constants.all_data['zip_compressed_filesize'].append(fileinfo.compress_size)
 
 """Get initial file sizes """
 for filename in os.listdir(directory.input_directory):
