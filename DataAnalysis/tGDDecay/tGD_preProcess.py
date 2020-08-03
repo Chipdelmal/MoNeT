c0aae9e168bd22223388e828ae10847cc2792b79
chipdelmal <chipdelmal@gmail.com>
2020-08-03 11:14:43 -0700

    tGD Loops over experiments

--- DataAnalysis/tGDDecay/tGD_preProcess.py
+++ DataAnalysis/tGDDecay/tGD_preProcess.py
@@ -6,40 +6,41 @@ import tGD_aux as aux
 import tGD_gene as drv
 import tGD_fun as fun
 from datetime import datetime
-# import MoNeT_MGDrivE as monet
 from joblib import Parallel, delayed
 
 ###############################################################################
 # Drives: LinkedDrive, splitDrive, tGD
 ###############################################################################
-(USR, DRV, EXP, AOI) = (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+(USR, DRV, AOI) = (sys.argv[1], sys.argv[2], sys.argv[3])
 (FMT, OVW, MF, JOB) = ('bz2', True, (False, True), 8)
 (SUM, AGG, SPA, REP, SRP) = (True, False, False, True, True)
+EXP = (000, 001, 005, 010, 100)
 ###############################################################################
 # Setting up paths and style
 ###############################################################################
-(PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT) = aux.selectPath(USR, DRV, EXP)
-drive = drv.driveSelector(DRV)
-(DVP, NOI) = (drive.get('gDict'), [[0], [1]])
-# Time and head ---------------------------------------------------------------
-tS = datetime.now()
-fun.printExperimentHead(PT_ROT, PT_IMG, PT_PRE, tS, 'Preprocess ' + AOI)
-###############################################################################
-# Load folders
-###############################################################################
-(expDirsMean, expDirsTrac) = fun.getExpPaths(PT_DTA)
-(expNum, nodeDigits) = (len(expDirsMean), len(str(NOI))+1)
-outNames = fun.splitExpNames(PT_OUT)
-outExpNames = set(outNames)
-###############################################################################
-# Analyze data
-###############################################################################
-Parallel(n_jobs=JOB)(
-        delayed(fun.preProcess)(
-                exIx, expNum, expDirsMean, expDirsTrac, DVP,
-                analysisOI=AOI, prePath=PT_PRE, nodesAggLst=NOI,
-                outExpNames=outExpNames, fNameFmt='{}/{}-{}_', OVW=OVW,
-                MF=MF, cmpr=FMT, nodeDigits=nodeDigits,
-                SUM=SUM, AGG=AGG, SPA=SPA, REP=REP, SRP=SRP
-            ) for exIx in range(0, expNum)
-    )
+for exp in EXP:
+    (PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT) = aux.selectPath(USR, DRV, exp)
+    drive = drv.driveSelector(DRV)
+    (DVP, NOI) = (drive.get('gDict'), [[0], [1]])
+    # Time and head -----------------------------------------------------------
+    tS = datetime.now()
+    fun.printExperimentHead(PT_ROT, PT_IMG, PT_PRE, tS, 'Preprocess ' + AOI)
+    ###########################################################################
+    # Load folders
+    ###########################################################################
+    (expDirsMean, expDirsTrac) = fun.getExpPaths(PT_DTA)
+    (expNum, nodeDigits) = (len(expDirsMean), len(str(NOI))+1)
+    outNames = fun.splitExpNames(PT_OUT)
+    outExpNames = set(outNames)
+    ###########################################################################
+    # Analyze data
+    ###########################################################################
+    Parallel(n_jobs=JOB)(
+            delayed(fun.preProcess)(
+                    exIx, expNum, expDirsMean, expDirsTrac, DVP,
+                    analysisOI=AOI, prePath=PT_PRE, nodesAggLst=NOI,
+                    outExpNames=outExpNames, fNameFmt='{}/{}-{}_', OVW=OVW,
+                    MF=MF, cmpr=FMT, nodeDigits=nodeDigits,
+                    SUM=SUM, AGG=AGG, SPA=SPA, REP=REP, SRP=SRP
+                ) for exIx in range(0, expNum)
+        )
