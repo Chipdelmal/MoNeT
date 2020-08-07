cda9e938dd6473f6fc3713269b22a7221a2a0014
chipdelmal <chipdelmal@gmail.com>
2020-08-07 13:12:31 -0700

    tGD Coded a script to download experiment images

---
+++ DataAnalysis/tGDDecay/tGD_dwdImg.py
@@ -0,0 +1,22 @@
+import sys
+import subprocess
+
+
+(drive, download) = (sys.argv[1], sys.argv[2])
+# (drive, download) = ('linkedDrive', 'img')
+
+# Experiments names (folders)
+EXPS = ('000', '001', '005', '010', '100')
+# Experiments paths
+(LAB_BASE, DSK_BASE) = (
+        'lab:/RAID5/marshallShare/tGD/fullSweep/',
+        '/media/chipdelmal/cache/Sims/tGD/fS/'
+    )
+# Download loop
+for (i, exp) in enumerate(EXPS):
+    (fm, to) = [pth+drive+'/'+exp+'/' for pth in (LAB_BASE, DSK_BASE)]
+    cmd = 'scp -rp '+fm+download+' '+to
+    print('Downloading {} part {}/{}... '.format(drive, i+1, len(EXPS)))
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    p.wait()
+print('Finished!')
