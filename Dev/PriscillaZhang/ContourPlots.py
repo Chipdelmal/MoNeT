01f8b802692e93141b08ec5592f1ab5fcf37e25f
zhangpriscilla <45432629+zhangpriscilla@users.noreply.github.com>
2020-08-07 12:47:06 -0700

    Update ContourPlots.py

--- Dev/PriscillaZhang/ContourPlots.py
+++ Dev/PriscillaZhang/ContourPlots.py
@@ -1,17 +1,6 @@
-import numpy as np
-import pandas as pd
-import csv
-import os
 import glob
-import matplotlib.pyplot as plt
-import seaborn as sns
-from matplotlib import cm
-from scipy.interpolate import griddata
-import sys
-import ContourPlots_Variables as variables
+from joblib import Parallel, delayed
 import ContourPlots_directories as directories
 import ContourPlots_functions as functions
-from joblib import Parallel, delayed
-
 
 Parallel(n_jobs=4)(delayed(functions.read_generate)(pathname) for pathname in glob.glob(directories.path))
