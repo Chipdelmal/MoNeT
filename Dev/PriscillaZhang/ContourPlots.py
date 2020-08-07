71b6394a070a79edcc1e7fcbbaf0f2ab98a8d57e
zhangpriscilla <45432629+zhangpriscilla@users.noreply.github.com>
2020-08-07 12:09:36 -0700

    Update

--- Dev/PriscillaZhang/ContourPlots.py
+++ Dev/PriscillaZhang/ContourPlots.py
@@ -8,14 +8,14 @@ import seaborn as sns
 from matplotlib import cm
 from scipy.interpolate import griddata
 import sys
-import ContourPlots_FilterVariables as filter_variables
+import ContourPlots_Variables as variables
 import ContourPlots_directories as directories
+import ContourPlots_functions as functions
 from joblib import Parallel, delayed
 
-headers = ['ratio', 'releases', 'resistance', 'fitness', 'sv', 'group', .05, .10, .25, .50, .75]
 
-filter_values = filter_variables.filter_values
-threshold = filter_variables.threshold
+filter_values = variables.filter_values
+threshold = variables.threshold
 
 def generate_plot(dataframe, threshold, filter_dict, title):
     for key in filter_dict:
@@ -34,8 +34,8 @@ def generate_plot(dataframe, threshold, filter_dict, title):
         (xi, yi) = (np.linspace(xmin, xmax, 500), np.linspace(ymin, ymax, 500))
         zi = griddata((x,y), z, (xi[None,:], yi[:, None]), method='nearest')
         fig, ax = plt.subplots()
-        ax.contour(xi, yi, zi, levels=filter_variables.mapLevels, linewidths=.5, colors='k')
-        heatmap = ax.contourf(xi, yi, zi, levels=filter_variables.mapLevels, extend='max', cmap=plt.cm.Purples)
+        ax.contour(xi, yi, zi, levels=variables.mapLevels, linewidths=.5, colors='k')
+        heatmap = ax.contourf(xi, yi, zi, levels=variables.mapLevels, extend='max', cmap=plt.cm.Purples)
         ax.set(xscale='log')
         ax.set_xlabel('Standing Variation')
         ax.set_ylabel('Fitness Cost')
@@ -43,19 +43,6 @@ def generate_plot(dataframe, threshold, filter_dict, title):
         plt.xlim(1E-6, 1E-2)
         plt.ylim(ymin, ymax)
         cbar = plt.colorbar(heatmap)
-        plt.show()
+        plt.savefig(directories.plots_path  + '/' + title)
 
-#Open up the csv files and concatenate the dataframes
-# for pathname in glob.glob(directories.path):
-#     filename = pathname.split("\\")[-1][:-4]
-#     df = pd.read_csv(pathname)
-#     df.columns = headers
-#     generate_plot(df, threshold, filter_values, filename)
-
-def read_generate(pathname):
-    df = pd.read_csv(pathname)
-    df.columns = headers
-    filename = pathname.split("\\")[-1][:-4]
-    generate_plot(df, threshold, filter_values, filename)
-
-Parallel(n_jobs=2)(delayed(read_generate)(pathname) for pathname in glob.glob(directories.path))
+Parallel(n_jobs=4)(delayed(functions.read_generate)(pathname) for pathname in glob.glob(directories.path))
--- Dev/PriscillaZhang/ContourPlots_directories.py
+++ Dev/PriscillaZhang/ContourPlots_directories.py
@@ -1,2 +1,3 @@
 path = r"C:\Users\prisc\Desktop\Marshall Lab\Marshall Lab Data\Contour Plot Data\ultrafine_data\*.csv"
 csv_path = r"C:\Users\prisc\Desktop\Marshall Lab\Marshall Lab Data\Contour Plot Data\ultrafine_data\01_HLT_05-WOP.csv"
+plots_path = r"C:\Users\prisc\Documents\GitHub\MoNeT2\Dev\PriscillaZhang\ContourPlots"
