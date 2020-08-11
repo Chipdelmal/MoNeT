f3175d163c053855d1f00e07ae727a3f36e9dc14
chipdelmal <chipdelmal@gmail.com>
2020-08-11 12:21:26 -0700

    tGD Adding the group ID to the pstProcess files

--- DataAnalysis/tGDDecay/tGD_pstProcess.py
+++ DataAnalysis/tGDDecay/tGD_pstProcess.py
@@ -11,8 +11,8 @@ import compress_pickle as pkl
 # from joblib import Parallel, delayed
 
 
-# (USR, DRV, AOI) = ('dsk', 'linkedDrive', 'HLT')
-(USR, DRV, AOI) = (sys.argv[1], sys.argv[2], sys.argv[3])
+(USR, DRV, AOI) = ('dsk', 'linkedDrive', 'HLT')
+# (USR, DRV, AOI) = (sys.argv[1], sys.argv[2], sys.argv[3])
 (FMT, SKP, MF, QNT, OVW) = ('bz', False, (False, True), .10, True)
 (SUM, AGG, SPA, REP, SRP) = (True, False, False, True, True)
 (thr, gIx) = ([.05, .10, .50], 1)
@@ -21,7 +21,7 @@ for EXP in EXPS:
     ###########################################################################
     # Setting up paths and style
     ###########################################################################
-    header = ['ratio', 'releases', 'resistance', 'fitness', 'sv', 'group']
+    header = ['hnf', 'cac', 'frc', 'hrt', 'ren', 'res', 'grp']
     header.extend(thr)
     (PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT) = aux.selectPath(USR, DRV, EXP)
     tS = datetime.now()
@@ -43,7 +43,7 @@ for EXP in EXPS:
         fName = PT_OUT+str(rnIt).zfill(2)+'_'+AOI+'_'+str(int(QNT*100)).zfill(2)
         print('{}* [{}/{}] {}{}'.format(
                 monet.CWHT,
-                str(rnIt+1).zfill(3), str(len(ren)).zfill(3),
+                str(rnIt).zfill(3), str(len(ren)-1).zfill(3),
                 fName.split('/')[-1], monet.CEND
             ))
         fNum = len(pbFiles)
@@ -62,12 +62,12 @@ for EXP in EXPS:
                              srpPrb, mnRef, ssRef, thr, gIx, quantile=QNT
                          )
                  ]
-            xpId = fun.getXpId(pFile, [1, 2, 3, 4, 5, 6])
+            xpId = fun.getXpId(pFile, [1, 2, 3, 4, 5, 6, 8])
             wopL[pIx] = xpId+wop
             mnCuts[pIx] = xpId+tti
             mxCuts[pIx] = xpId+tto
             tsCuts[pIx] = xpId+tts
-        monet.writeListToCSV(fName+'-WOP.csv', wopL)
-        monet.writeListToCSV(fName+'-TTI.csv', mnCuts)
-        monet.writeListToCSV(fName+'-TTO.csv', mxCuts)
-        monet.writeListToCSV(fName+'-TTS.csv', tsCuts)
+        monet.writeListToCSV(fName+'-WOP.csv', wopL, header=header+thr)
+        monet.writeListToCSV(fName+'-TTI.csv', mnCuts, header=header+thr)
+        monet.writeListToCSV(fName+'-TTO.csv', mxCuts, header=header+thr)
+        monet.writeListToCSV(fName+'-TTS.csv', tsCuts, header=header+'sd')
