0ef5cd950ce279e294cd21d2becb74331693c0db
chipdelmal <chipdelmal@gmail.com>
2020-09-10 11:04:44 -0700

    tGD srv sync

--- DataAnalysis/tGDDecay/tGD_preProcess.py
+++ DataAnalysis/tGDDecay/tGD_preProcess.py
@@ -14,7 +14,7 @@ from joblib import Parallel, delayed
 ###############################################################################
 (USR, DRV, AOI) = (sys.argv[1], sys.argv[2], sys.argv[3])
 (FMT, OVW, MF, JOB) = ('bz2', True, (False, True), 1)
-(SUM, AGG, SPA, REP, SRP) = (True, False, False, True, True)
+(SUM, AGG, SPA, REP, SRP) = (True, False, False, True, False)
 EXP = ('000', ) #, '001', '005', '010', '100')
 ###############################################################################
 # Setting up paths and style
