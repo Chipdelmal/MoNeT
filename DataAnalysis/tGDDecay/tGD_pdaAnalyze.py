ed90962e6d70886842890650cefa4028e637d0a7
Ameek Bindra <ameekbindra@berkeley.edu>
2020-09-17 16:58:59 -0400

    Updated tGD_pdaAnalyze.py

--- DataAnalysis/tGDDecay/tGD_pdaAnalyze.py
+++ DataAnalysis/tGDDecay/tGD_pdaAnalyze.py
@@ -1,16 +1,15 @@
-
 import numpy as np
 import operator as op
 import tGD_dataProcess as da
 import matplotlib.pyplot as plt
 
-USR = 'chipdelmal'
+USR = 'ameek'
 (thi, tho, thw, qnt) = (.35, .75, .5, .9)
 # Setup path -----------------------------------------------------------------
 if USR == 'chipdelmal':
     pth = '/home/chipdelmal/Documents/WorkSims/tGD/figure2/tGD/000/POSTPROCESS/'
 elif USR == 'ameek':
-    pth = '/PATH_TO_FILE/'
+    pth = '/Volumes/GoogleDrive/.shortcut-targets-by-id/1e56Nrp5mAxkyQ0C2zgBXCJ1YMFa2QNXg/AmeekBindra/tGD/000/POSTPROCESS/'
 # Load file ------------------------------------------------------------------
 fName = 'E_005_075_005_090_02_0100-HLT_00_rto.npy'
 repsRatios = np.load(pth+fName)
@@ -34,9 +33,13 @@ wop = np.sum(thwBool, axis=1)
         np.nanquantile(tto, qnt),
         np.nanquantile(wop, 1-qnt)
     )
-
-(fig, ax) = plt.subplots(nrows=3, ncols=1)
-ax[0].imshow(repsRatios, cmap='Purples_r')
-ax[1].imshow(thIBool, cmap='Purples_r')
-ax[2].imshow(thOBool, cmap='Purples_r')
+# Plotting---------------------------------------------------------------------
+(fig, ax) = plt.subplots(nrows=1, ncols=1)
+ax.imshow(repsRatios, cmap='Purples_r')
+# add TTI----------------------------------------------------------------------
+[plt.axvline(i, color = 'white', alpha = 75, linewidth = 0.5) for i in tti]
+# add TTO----------------------------------------------------------------------
+[plt.axvline(j, alpha = 75, linewidth = 0.5) for j in tto]
+# Save the figure--------------------------------------------------------------
+plt.savefig("heatmap.png", bbox_inches = 'tight', pad_inches = 0)
 # monet.quickSaveFigure(fig, './test.png')
