16fe5fd173516a15f0b85989e1621406d136de6e
chipdelmal <chipdelmal@gmail.com>
2020-09-23 13:44:12 -0700

    STP pstTraces is working now

--- DataAnalysis/STP/STP_pstTraces.py
+++ DataAnalysis/STP/STP_pstTraces.py
@@ -3,9 +3,11 @@
 
 import sys
 import numpy as np
+import pandas as pd
 from glob import glob
 import STP_aux as aux
 import STP_gene as drv
+import STP_plots as plot
 import STP_functions as fun
 import STP_dataProcess as da
 from datetime import datetime
@@ -34,3 +36,40 @@ STYLE = {
 STYLE['aspect'] = monet.scaleAspect(1, STYLE)
 tS = datetime.now()
 aux.printExperimentHead(PT_ROT, PT_IMG, PT_OUT, tS, 'PstTraces')
+###########################################################################
+# Load postprocessed files
+###########################################################################
+pstPat = PT_MTR+AOI+'_{}_'+QNT+'_qnt.csv'
+pstFiles = [pstPat.format(i) for i in ('TTI', 'TTO', 'WOP', 'MNX', 'RAP')]
+(dfTTI, dfTTO, dfWOP, dfMNX, _) = [pd.read_csv(i) for i in pstFiles]
+###########################################################################
+# Load preprocessed files lists
+###########################################################################
+repFiles = glob(PT_PRE+'*'+AOI+'*'+'srp'+'*')
+if FZ:
+    repFiles = fun.getFilteredFiles(
+            PT_PRE+'*_00_*'+AOI+'*'+'srp'+'*',
+            PT_PRE+'*'+AOI+'*'+'srp'+'*'
+        )
+else:
+    repFiles = glob(PT_PRE+'*'+AOI+'*'+'srp'+'*')
+###########################################################################
+# Iterate through experiments
+###########################################################################
+(fNum, digs) = monet.lenAndDigits(repFiles)
+fmtStr = '{}+ File: {}/{}'
+for (i, repFile) in enumerate(repFiles):
+    padi = str(i+1).zfill(digs)
+    print(fmtStr.format(monet.CBBL, padi, fNum, monet.CEND), end='\r')
+    (repDta, xpid) = (
+            pkl.load(repFile),
+            fun.getXpId(repFile, (1, 2, 3, 4, 5, 7))
+        )
+    xpRow = [da.filterDFWithID(i, xpid) for i in (dfTTI, dfTTO, dfWOP, dfMNX)]
+    (tti, tto, wop) = [float(row[THS]) for row in xpRow[:3]]
+    (mnf, mnd) = (float(xpRow[3]['min']), float(xpRow[3]['minx']))
+    pop = repDta['landscapes'][0][0][-1]
+    plot.exportTracesPlot(
+            repDta, repFile.split('/')[-1][:-6]+str(QNT), STYLE, PT_IMG,
+            vLines=[tti, tto, mnd], hLines=[mnf*pop]
+        )
