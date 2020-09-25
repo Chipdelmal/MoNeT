8cca998f68698eda70c37d59024f1fa6b6ba2dcc
chipdelmal <chipdelmal@gmail.com>
2020-09-25 12:14:00 -0700

    tGD Minor style updates for traces

--- DataAnalysis/tGDDecay/tGD_pstTraces.py
+++ DataAnalysis/tGDDecay/tGD_pstTraces.py
@@ -27,7 +27,7 @@ for EXP in EXPS:
     drive = drv.driveSelector(DRV, AOI)
     (CLR, YRAN) = (drive.get('colors'), (0, drive.get('yRange')))
     STYLE = {
-            "width": .5, "alpha": .15, "dpi": 500, "legend": True,
+            "width": .3, "alpha": .15, "dpi": 500, "legend": True,
             "aspect": .25, "colors": CLR, "xRange": [0, (365*5)/3],
             "yRange": YRAN
         }
