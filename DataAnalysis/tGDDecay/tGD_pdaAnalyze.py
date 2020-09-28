7187bf507d79b03eea1d142de94e2c9f744d48d3
chipdelmal <chipdelmal@gmail.com>
2020-09-28 09:25:22 -0700

    tGD changes for Ameek's work!

--- DataAnalysis/tGDDecay/tGD_pdaAnalyze.py
+++ DataAnalysis/tGDDecay/tGD_pdaAnalyze.py
@@ -1,66 +1,48 @@
 import numpy as np
-import operator as op
-import tGD_dataProcess as da
-import matplotlib.pyplot as plt
+import tGD_aux as aux
+from glob import glob
 import compress_pickle as pkl
+import matplotlib.pyplot as plt
+import MoNeT_MGDrivE as monet
 
-USR = 'ameek'
-(thi, tho, thw, QNT) = (.35, .75, .5, '90')
+USR = 'chipdelmal'
+(thi, tho, QNT) = (.5, .5, '90')
+(USR, DRV, exp, AOI) = ('dsk', 'tGD', '000', 'TRS')
 # Setup path -----------------------------------------------------------------
-if USR == 'chipdelmal':
-    pth = '/media/hdd/WorkExperiments/tGD/figure2/tGD/000/POSTPROCESS/'
-elif USR == 'ameek':
-    pth = '/Volumes/GoogleDrive/.shortcut-targets-by-id/1e56Nrp5mAxkyQ0C2zgBXCJ1YMFa2QNXg/AmeekBindra/tGD/000/POSTPROCESS/'
+# TO DO: Make sure your code now runs with this function. You'll probably have
+#           to have a look at tGD_aux.py in line 52 to setup your path correctly.
+(PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(USR, DRV, exp)
+PT_IMG = PT_IMG+'xRay/'
+monet.makeFolder(PT_IMG)
 # Load file ------------------------------------------------------------------
-fName = 'E_005_075_005_090_02_0100-HLT_00_rto.npy'
-repsRatios = np.load(pth+fName)
-# Thresholds -----------------------------------------------------------------
-# (reps, days) = repsRatios.shape
-# (thIBool, thOBool) = (
-#         da.compRatioToThreshold(repsRatios, thi, op.lt),
-#         da.compRatioToThreshold(repsRatios, tho, op.gt)
-#     )
-# # Time to first introgression ------------------------------------------------
-# tti = np.argmax(thIBool == 1, axis=1)
-# tto = np.subtract(days, np.argmin(np.flip(thOBool), axis=1))
-# # Min and max ----------------------------------------------------------------
-# (repsMin, repsMax) = (repsRatios.min(axis=1), repsRatios.max(axis=1))
-# # Window of protection -------------------------------------------------------
-# thwBool = da.compRatioToThreshold(repsRatios, thw, op.lt)
-# wop = np.sum(thwBool, axis=1)
-# # Quantiles ------------------------------------------------------------------
-# (ttiQ, ttoQ, wopQ) = (
-#         np.nanquantile(tti, qnt),
-#         np.nanquantile(tto, qnt),
-#         np.nanquantile(wop, 1-qnt)
-#     )
-
+# TO DO: We need to iterate over this fNames list of files, repeating the plot
+#           for each one (this will become a for loop)
+fNames = glob(PT_OUT+'*{}*.npy'.format(AOI))
+i = 1
+fName = fNames[i]
+repsRatios = np.load(fName)
 # fName
 f_list = fName.replace('-', '_').split('_')[1:9]
 del f_list[6]
 f_tuple = tuple(list(map(int, f_list)))
-
 # Load the TTI and TTO from the MLR data files in the SUMMARY folder
-ttiR = pkl.load('/Volumes/GoogleDrive/.shortcut-targets-by-id/1e56Nrp5mAxkyQ0C2zgBXCJ1YMFa2QNXg/AmeekBindra/tGD/000/SUMMARY/HLT_TTI_{}_mlr.bz'.format(QNT))
+# TO DO : Updated these to take the path from above. Check that they are still
+#           working correctly.
+ttiR = pkl.load(PT_MTR+'{}_TTI_{}_mlr.bz'.format(AOI, QNT))
 ttiR.keys()
-tti = ttiR[f_tuple][int(QNT)] # These ID numbers come from the fName
-
-ttoR = pkl.load('/Volumes/GoogleDrive/.shortcut-targets-by-id/1e56Nrp5mAxkyQ0C2zgBXCJ1YMFa2QNXg/AmeekBindra/tGD/000/SUMMARY/HLT_TTO_{}_mlr.bz'.format(QNT))
-ttoR.keys()
-tto = ttoR[f_tuple][int(QNT)]
-# ameek's scratch
-#print(ttiR.keys())
-#ttiR[] = {ttiR}
-#tti = ttiR[(5, 75, 5, 90, 2, 100, 0)][int(QNT)]
-#tto =
-
+tti = ttiR[f_tuple][int(thi*100)] # These ID numbers come from the fName
+ttoR = pkl.load(PT_MTR+'{}_TTO_{}_mlr.bz'.format(AOI, QNT))
+tto = ttoR[f_tuple][int(tho*100)]
 # Plotting---------------------------------------------------------------------
 (fig, ax) = plt.subplots(nrows=1, ncols=1)
 ax.imshow(repsRatios, cmap='Purples_r')
+# Style notes: In python you usually don't leave space in-between '=' when
+#               its for passing arguments to a function (only for assignment)
 # add TTI----------------------------------------------------------------------
-[plt.axvline(i, color = 'pink', alpha = 75, linewidth = 0.5) for i in tti]
+[plt.axvline(i, color='#3DFE70', alpha=.75, linewidth=0.2) for i in tti]
 # add TTO----------------------------------------------------------------------
-[plt.axvline(j, alpha = 75, linewidth = 0.5) for j in tto]
+[plt.axvline(j, color='#3DFE70', alpha=.75, linewidth=0.2) for j in tto]
 # Save the figure--------------------------------------------------------------
-plt.savefig("test_heatmap.png", bbox_inches = 'tight', pad_inches = 0)
+outName = fName.split('/')[-1].split('.')[0][:-4]
+plt.savefig(PT_IMG+outName+'.png', bbox_inches='tight', pad_inches=0, dpi=500)
 # monet.quickSaveFigure(fig, './test.png')
