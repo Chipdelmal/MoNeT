2d11e4586a4348afe571f12fd674ae13dda6b8f6
chipdelmal <chipdelmal@gmail.com>
2020-10-07 12:54:54 -0700

    tGD Moved pdaAnalyze to xray

--- DataAnalysis/tGDDecay/tGD_pstXRay.py
+++ DataAnalysis/tGDDecay/tGD_pstXRay.py
@@ -1,39 +1,64 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 
+import sys
 import numpy as np
-from glob import glob
 import tGD_aux as aux
-import tGD_fun as fun
-import tGD_gene as drv
-import MoNeT_MGDrivE as monet
+from glob import glob
+import compress_pickle as pkl
 import matplotlib.pyplot as plt
+import MoNeT_MGDrivE as monet
+from datetime import datetime
+# import re
 
 
-(USR, DRV, AOI) = ('dsk', 'tGD', 'HLT')
-(gIx, hIx) = (1, 0)
+(thi, tho, QNT) = (.1, .1, '90')
+(USR, DRV, AOI) = (sys.argv[1], sys.argv[2], sys.argv[3])
+# (USR, DRV, AOI) = ('dsk', 'tGD', 'HLT')
+X_RAN = [0, 5*365/3]
+EXPS = ('000', )
 
-EXP = ('000', )  # '001', '005', '010', '100')
-
-# This will become a for-loop
-exp = EXP[0]
-# Selects the working directories
-(PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT) = aux.selectPath(USR, DRV, exp)
-outputFolder = PT_IMG + 'xray/'
-monet.makeFolder(outputFolder)
-# Gets the paths of the numpy arrays
-filepaths = sorted(glob(PT_OUT+'E_*'))
-outputFolder
-
-# Load the numpy array (should become a for-loop)
-fPath = filepaths[10]
-repsRatios = np.load(fPath)
-# Setup the plot (should use the hIx color)
-(fig, ax) = plt.subplots(nrows=1, ncols=1)
-ax.imshow(repsRatios, cmap='Purples_r')
-
-
-
-#################### COLOR THING ###########################################
-drive = drv.driveSelector(DRV, AOI)
-drive['colors']
+for exp in EXPS:
+    # Select path ------------------------------------------------------------
+    (PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_OUT, PT_MTR) = aux.selectPath(USR, DRV, exp)
+    PT_IMG = PT_IMG+'xRay/'
+    monet.makeFolder(PT_IMG)
+    tS = datetime.now()
+    aux.printExperimentHead(PT_ROT, PT_IMG, PT_MTR, tS, 'X-Ray '+AOI)
+    # Load file --------------------------------------------------------------
+    fNames = glob(PT_OUT+'*{}*.npy'.format(AOI))
+    xpNumS = str(len(fNames)).zfill(4)
+    for (i, fName) in enumerate(fNames):
+        xpNumCS = str(i + 1).zfill(4)
+        print('* Exporting {}/{}'.format(xpNumCS, xpNumS), end='\r')
+        # Name formatting -> tuple--------------------------------------------
+        repsRatios = np.load(fName)
+        fList = fName.split('/')[-1].split('-')[0].split('_')[1:]
+        fList.append(fName.split('/')[-1].split('-')[1].split('_')[1])
+        fKeys = tuple(list(map(int, fList)))
+        # fList = re.split(r'[a-zA-Z_/.-]+', fName)[9:16]
+        # AOI = re.split(r'[0-9_./-]+', fName)[18]
+        # Select cmap --------------------------------------------------------
+        (scalers, HD_DEP, IND_RAN, palette) = aux.selectDepVars('TTI', AOI)
+        cmap = palette.reversed()
+        # load TTI and TTO ---------------------------------------------------
+        ttiR = pkl.load(PT_MTR+'{}_TTI_{}_mlr.bz'.format(AOI, QNT))
+        tti = ttiR[fKeys][int(thi*100)]
+        ttoR = pkl.load(PT_MTR+'{}_TTO_{}_mlr.bz'.format(AOI, QNT))
+        tto = ttoR[fKeys][int(tho*100)]
+        # Plotting-------------------------------------------------------------
+        (fig, ax) = plt.subplots(nrows=1, ncols=1)
+        ax.imshow(repsRatios, cmap=cmap)
+        # add TTI-------------------------------------------------------------
+        [plt.axvline(i, color='#f8f7ff', alpha=.65, lw=0.175, ls='-.') for i in tti]
+        # add TTO-------------------------------------------------------------
+        [plt.axvline(j, color='cyan', alpha=.75, lw=0.2, ls='dotted') for j in tto]
+        # Save the figure------------------------------------------------------
+        outName = fName.split('/')[-1].split('.')[0][:-4]
+        plt.xlim(X_RAN)
+        ax.axes.xaxis.set_ticklabels([])
+        ax.axes.yaxis.set_ticklabels([])
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
+        ax.xaxis.set_tick_params(size=0)
+        ax.yaxis.set_tick_params(size=0)
+        plt.savefig(PT_IMG+outName + '.png', bbox_inches='tight', pad_inches=0.01, dpi=500)
+        plt.close("all")
