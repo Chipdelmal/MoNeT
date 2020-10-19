f07521427d34382fd0d845678ec3db5e51a42020
chipdelmal <chipdelmal@gmail.com>
2020-10-19 15:32:30 -0700

    SDY Stacked QNT terminal print

--- DataAnalysis/SplitDrive_Yorkeys/SDY_qntVConcat.py
+++ DataAnalysis/SplitDrive_Yorkeys/SDY_qntVConcat.py
@@ -3,6 +3,7 @@ import sys
 import cv2
 from glob import glob
 import SDY_aux as aux
+from datetime import datetime
 import MoNeT_MGDrivE as monet
 
 
@@ -16,13 +17,17 @@ QNT = ['50', '75', '90']
 (PT_ROT, PT_IMG, PT_DTA, PT_PRE, PT_PST, PT_MTR) = aux.selectPath(USR, EXP)
 (PT_IMG_I, PT_IMG_O) = (PT_IMG + 'pstTraces/', PT_IMG + 'stacksQNT/')
 monet.makeFolder(PT_IMG_O)
+tS = datetime.now()
+aux.printExperimentHead(PT_ROT, PT_IMG_I, PT_IMG_O, tS, 'PostProcess ' + AOI)
 # Get files -------------------------------------------------------------------
 imgLists = [glob(PT_IMG_I + '*HLT*' + i + '*') for i in QNT]
 imgTuples = list(zip(*[sorted(i) for i in imgLists]))
 # #############################################################################
 # Iterate through images
 # #############################################################################
-for tp in imgTuples:
+fmt = 'Processing {}/' + str(len(imgTuples))
+for (i, tp) in enumerate(imgTuples):
+    print(fmt.format(str(i + 1).zfill(4)), end='\r')
     fName = tp[0].split('/')[-1][:-7]
     imgArray = [cv2.imread(i) for i in tp]
     vertical = cv2.vconcat(imgArray)
