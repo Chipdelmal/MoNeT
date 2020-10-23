162de68723c91b9d7e11c3c299c620269a33c1aa
chipdelmal <chipdelmal@gmail.com>
2020-10-22 19:50:53 -0700

    STP More updates for the map

--- DataAnalysis/STP/STP_map.py
+++ DataAnalysis/STP/STP_map.py
@@ -3,6 +3,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import math
 import glob
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -28,13 +29,15 @@ COLORS = [
     ]
 
 
-PTH_pts = '/media/hdd/WorkExperiments/STP/stp_all_sites_v5.csv'
-pts = pd.read_csv(PTH_pts)
+PTH_pts = '/media/hdd/WorkExperiments/STP/'
+pts = pd.read_csv(PTH_pts+'stp_all_sites_v5.csv')
 
+
+# #############################################################################
+# Sao Tome
+# #############################################################################
 (minLat, minLong) = (-0.045, 6.4)
-(maxLat, maxLong) = (1.75, 7.56)
-
-
+(maxLat, maxLong) = (.5, 6.8)
 fig = plt.figure(figsize=(5, 5))
 ax = fig.add_subplot(111, label="1")
 mH = Basemap(
@@ -51,10 +54,56 @@ mL = Basemap(
     llcrnrlon=minLong, urcrnrlon=maxLong,
     lat_ts=20, resolution='i', ax=ax
 )
-mL.drawcoastlines(color=COLORS[4], linewidth=6, zorder=0)
+mL.drawcoastlines(color=COLORS[4], linewidth=10, zorder=0)
+ax.tick_params(
+    axis='both', which='both',
+    bottom=True, top=False, left=True, right=False,
+    labelbottom=True, labelleft=True
+)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+ax.spines["left"].set_visible(False)
+mH.scatter(
+    list(pts['lon']), list(pts['lat']), latlon=True,
+    alpha=.2, marker='.', s=[10 * math.log(i) for i in list(pts['pop'])],
+    color='#E048B8', zorder=3
+)
+fun.quickSaveFig(PTH_pts + 'ST.png', fig, dpi=750)
+# #############################################################################
+# Principe
+# #############################################################################
+(minLat, minLong) = (1.5, 7.275)
+(maxLat, maxLong) = (1.75, 7.5)
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(111, label="1")
+mH = Basemap(
+    projection='merc',
+    llcrnrlat=minLat, urcrnrlat=maxLat,
+    llcrnrlon=minLong, urcrnrlon=maxLong,
+    lat_ts=20, resolution='h', ax=ax
+)
+mH.drawcoastlines(color=COLORS[0], linewidth=2, zorder=1)
+mH.drawcoastlines(color=COLORS[3], linewidth=.25, zorder=2)
+mL = Basemap(
+    projection='merc',
+    llcrnrlat=minLat, urcrnrlat=maxLat,
+    llcrnrlon=minLong, urcrnrlon=maxLong,
+    lat_ts=20, resolution='i', ax=ax
+)
+mL.drawcoastlines(color=COLORS[4], linewidth=10, zorder=0)
 ax.tick_params(
     axis='both', which='both',
     bottom=False, top=False, left=False, right=False,
-    labelbottom=False, labelleft=False
+    labelbottom=True, labelleft=True
 )
-fun.quickSaveFig('map.png', fig, dpi=1500)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+ax.spines["left"].set_visible(False)
+mH.scatter(
+    list(pts['lon']), list(pts['lat']), latlon=True,
+    alpha=.2, marker='.', s=[10 * math.log(i) for i in list(pts['pop'])],
+    color='#E048B8', zorder=3
+)
+fun.quickSaveFig(PTH_pts + 'PP.png', fig, dpi=750)
