f7ca21584db7741640fc851f2ee8d3fa60f98025
chipdelmal <chipdelmal@gmail.com>
2020-11-09 17:32:02 -0800

    STP Minor improvements to RF

--- DataAnalysis/STP/STP_classifier.py
+++ DataAnalysis/STP/STP_classifier.py
@@ -6,22 +6,32 @@ import numpy as np
 from os import path
 import pandas as pd
 from scipy import stats
-import MoNeT_MGDrivE as monet
-import STP_dataAnalysis as da
 from sklearn import tree
 from sklearn import metrics
+from joblib import dump, load
+import MoNeT_MGDrivE as monet
+import STP_dataAnalysis as da
+import matplotlib.pyplot as plt
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 
 
-
-(MTR, ERR, OVW, THS, QNT) = ('WOP', False, False, '0.5', '50')
+(MTR, ERR, OVW, THS, QNT) = ('WOP', False, True, '0.1', '50')
 ID_MTR = 'HLT_{}_{}_qnt.csv'.format(MTR, QNT)
 FEATS = ['i_sex', 'i_rer', 'i_ren', 'i_rsg', 'i_fic', 'i_gsv', 'i_grp']
 SCA = 100000000
-OPRAN = ((0, 1*365), (1*365, 3*365), (3*365, 100*365))
+# Classifier Variables --------------------------------------------------------
+(OPRAN, TV_SPLT) = (
+    ((0, 1), (1, 2), (2, 5), (5, 100)),
+    .3
+)
+(modelFeats, classNames) = (
+    ['i_rer', 'i_ren', 'i_rsg', 'i_fic', 'i_gsv'],
+    ['None', 'Low', 'Mid', 'High']
+)
 ###############################################################################
 # Create directories structure
 ###############################################################################
@@ -29,17 +39,17 @@ PT_ROT = '/media/hdd/WorkExperiments/STP/PAN/sim/'
 PT_OUT = PT_ROT + 'SUMMARY/'
 PT_DTA = '{}{}_{}'.format(PT_OUT, 'Full', ID_MTR)
 PT_IMG = PT_ROT + 'img/'
+PT_MOD = PT_ROT + 'MODELS/'
 monet.makeFolder(PT_OUT)
+monet.makeFolder(PT_MOD)
 ###############################################################################
 # Read and clean datasets
 ###############################################################################
 dfRC = da.rescaleDataset(pd.read_csv(PT_DTA), SCA)
 data = dfRC.drop(dfRC.columns[0], axis=1)
 HEADER = list(dfRC.columns)
-(LBLS, FEAT_LVLS) = (
-    sorted(list(set(HEADER) - set(FEATS))),
-    [set(dfRC[i].unique()) for i in FEATS]
-)
+LBLS = sorted(list(set(HEADER) - set(FEATS))),
+FEATS_LVLS = {i: list(data[i].unique()) for i in FEATS}
 ###############################################################################
 # Preprocess
 ###############################################################################
@@ -53,20 +63,16 @@ dataFiltered = data[fltr]
 dataFiltered.corr(method='spearman')[THS]
 # sns.pairplot(dataFiltered)
 # Calculate Operational Ranges ------------------------------------------------
-groupMtr = np.asarray(dataFiltered[THS])
-groupBools = [[i[0] <= feat < i[1] for i in OPRAN] for feat in groupMtr]
+grpMtr = np.asarray(dataFiltered[THS])
+groupBools = [[i[0]*365 <= feat < i[1]*365 for i in OPRAN] for feat in grpMtr]
 groupIx = [i.index(True) for i in groupBools]
 # Features/Labels Separate ----------------------------------------------------
-(modelFeats, classNames) = (
-    ['i_rer', 'i_ren', 'i_rsg', 'i_fic', 'i_gsv'],
-    ['None', 'Low', 'High']
-)
 (features, labels) = (dataFiltered[modelFeats], groupIx)
 ###############################################################################
 # Split and preprocess dataset
 ###############################################################################
 (X_train, X_test, y_train, y_test) = train_test_split(
-    features, labels, test_size=0.3
+    features, labels, test_size=TV_SPLT
 )
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
@@ -74,21 +80,32 @@ X_test = sc.transform(X_test)
 ###############################################################################
 # Train
 ###############################################################################
-rf = RandomForestClassifier(n_estimators=100, random_state=42, max_depth=5)
-rf.fit(X_train, y_train)
+strMod = PT_MOD+'RandomForest.joblib'
+if (not path.exists(strMod)) or (OVW):
+    print("Training...")
+    rf = RandomForestClassifier(n_estimators=100, random_state=42, max_depth=8)
+    rf.fit(X_train, y_train)
+    dump(rf, strMod)
+else:
+    rf = load(strMod)
 y_pred = rf.predict(X_test)
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
-[print(i) for i in zip(modelFeats, rf.feature_importances_)]
-tree_in_forest = rf.estimators_[0]
-tree.plot_tree(
-    rf.estimators_[0], filled=True,
-    class_names=classNames, feature_names=modelFeats
-)
 ###############################################################################
 # Test
 ###############################################################################
-rf.predict(sc.transform([[.1, 1, 0, 0, 0]])) 
+FEATS_LVLS
+inProbe = [3.65716e-09, 3, 0, 0, 0]
+classNames[rf.predict(sc.transform([inProbe]))[0]]
+###############################################################################
+# Test
+###############################################################################
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+[print(i) for i in zip(modelFeats, rf.feature_importances_)]
 plot_confusion_matrix(
     rf, X_test, y_test,
-    display_labels=classNames
+    display_labels=classNames, cmap=plt.cm.Blues
 )
+# tree_in_forest = rf.estimators_[0]
+# tree.plot_tree(
+#     rf.estimators_[0], filled=True,
+#     class_names=classNames, feature_names=modelFeats
+# )
