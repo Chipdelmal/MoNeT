236aa28ba8da23e454d970977e148f83da5d58ce
chipdelmal <chipdelmal@gmail.com>
2021-03-04 14:07:56 -0800

    PYF Media added

--- DataAnalysis/tGDDecay/tGD_preProcess.sh
+++ DataAnalysis/tGDDecay/tGD_preProcess.sh
@@ -4,4 +4,4 @@ python tGD_preProcess.py $1 $2 ECO
 python tGD_preProcess.py $1 $2 HLT
 python tGD_preProcess.py $1 $2 TRS
 python tGD_preProcess.py $1 $2 WLD
-python tGD_preProcess.py $1 $2 CAS
+python tGD_preProcess.py $1 $2 CST
