32b0136088a0579d136d3696a87b0d00fabf999f
chipdelmal <chipdelmal@gmail.com>
2021-03-18 11:04:20 -0700

    tGD Updating aux paths for video preProcess

--- DataAnalysis/tGDDecay/tGD_aux.py
+++ DataAnalysis/tGDDecay/tGD_aux.py
@@ -47,16 +47,12 @@ def selectPath(USR, DRV, EXP):
         PATH_ROOT = '/RAID5/marshallShare/tGD/figure3/{}/{}/'.format(DRV, EXP)
     elif USR == 'srv2':
         PATH_ROOT = '/RAID5/marshallShare/tGD/figure2/{}/{}/'.format(DRV, EXP)
+    elif USR == 'srv3':
+        PATH_ROOT = '/RAID5/marshallShare/tGD/video/{}/{}/'.format(DRV, EXP)
     elif USR == 'dsk2':
         PATH_ROOT = '/home/chipdelmal/Documents/WorkSims/tGD/figure2/{}/{}/'.format(DRV, EXP)
     elif USR == 'dsk':
         PATH_ROOT = '/home/chipdelmal/Documents/WorkSims/tGD/figure3/{}/{}/'.format(DRV, EXP)
-    elif USR == 'PZ':
-        PATH_ROOT = '/PATH_TO_FOLDER/tGD/figure2/{}/{}/'.format(DRV, EXP)
-    elif USR == 'ameek':
-        PATH_ROOT = '/Volumes/GoogleDrive/.shortcut-targets-by-id/1e56Nrp5mAxkyQ0C2zgBXCJ1YMFa2QNXg/AmeekBindra/{}/{}/'.format(DRV, EXP)
-    elif USR == 'ashley':
-        PATH_ROOT = '/PATH_TO_DATA_tGD_FOLDER/{}/{}/'.format(DRV, EXP)
     else:
         PATH_ROOT = '/home/chipdelmal/Documents/WorkSims/tGD/figure2/{}/{}/'.format(DRV, EXP)
     # monet.makeFolder('{}/'.format(PATH_ROOT))
